00a486df6e81a1cc406e79f7f122f7280805192a
markgray <mark.t.gray@gmail.com>
2022-08-09 07:00:42 -0400

    Added comments in ComposeTutorial

--- ComposeTutorial/app/src/main/java/com/example/android/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/android/composetutorial/MainActivity.kt
@@ -22,6 +22,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -78,7 +81,15 @@ data class Message(val author: String, val body: String)
  * and [Modifier.border] to add a border of 1.5.dp of the `secondaryVariant` color of
  * [MaterialTheme.colors] (a dark teal). This is followed by a [Spacer] of 8.dp, and a [Column] with
  * a [Modifier.clickable] `modifier` argument whose lambda toggles the value of the remembered
- * [Boolean] variable `isExpanded` when it is clicked.
+ * [Boolean] variable `isExpanded` when it is clicked. This [Column] holds a [Text] which displays
+ * the [Message.author] field or our [msg] parameter followed by a [Spacer] of 4.dp and a [Surface]
+ * which holds a [Text] which displays the [Message.body] field or our [msg] parameter. The `color`
+ * of the [Surface] is `surfaceColor` which animates between the `primary` color and the `surface`
+ * color of [MaterialTheme.colors] depending on the value of `isExpanded`, and the `modifier` of the
+ * [Surface] uses [Modifier.animateContentSize] to change the [Surface] size gradually when its child
+ * [Text] Composable changes size. The [Text] sets the value of its `maxLines` parameter to 1 if
+ * `isExpanded` is `false` (displays only the first line of the [Message.body] of [msg]) or to
+ * [Int.MAX_VALUE] if it is `true` (displays the entire [Message.body] of [msg]).
  *
  * @param msg the [Message] we are to display.
  */
@@ -98,7 +109,7 @@ fun MessageCard(msg: Message) {
         // We keep track if the message is expanded or not in this variable
         var isExpanded by remember { mutableStateOf(false) }
         // surfaceColor will be updated gradually from one color to the other
-        val surfaceColor by animateColorAsState(
+        val surfaceColor: Color by animateColorAsState(
             if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
         )
 
@@ -135,6 +146,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
+/**
+ * This Composable displays the fake data found in its [List] of [Message] argument [messages] in a
+ * [LazyColumn] using [MessageCard] objects for each [Message].
+ *
+ * @param messages the [List] of [Message] objects we should display in a [LazyColumn].
+ */
 @Composable
 fun Conversation(messages: List<Message>) {
     LazyColumn {
@@ -144,6 +161,10 @@ fun Conversation(messages: List<Message>) {
     }
 }
 
+/**
+ * This is just a [ComposeTutorialTheme] wrapped preview of our [Conversation] Composable using the
+ * sample data found in[SampleData.conversationSample],
+ */
 @Preview
 @Composable
 fun PreviewConversation() {
@@ -152,6 +173,13 @@ fun PreviewConversation() {
     }
 }
 
+/**
+ * This is two previews of a [ComposeTutorialTheme] wrapped [MessageCard] Composable of a hard coded
+ * [Message]. The preview named "Light Mode" uses the default `LightColorPalette` [lightColors] colors
+ * of our [ComposeTutorialTheme] while the preview named "Dark Mode" preview sets `uiMode` to
+ * [Configuration.UI_MODE_NIGHT_YES] and this causes the `DarkColorPalette` [darkColors] colors of
+ * our [ComposeTutorialTheme] to be used.
+ */
 @Preview(name = "Light Mode")
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
