e60918cf062c4adfd479b4f3f83e033fa7498f2e
markgray <mark.t.gray@gmail.com>
2022-09-13 06:17:08 -0400

    Added comments in LayoutsCodelab

--- LayoutsCodelab/app/src/main/java/com/codelab/layouts/Lists.kt
+++ LayoutsCodelab/app/src/main/java/com/codelab/layouts/Lists.kt
@@ -17,6 +17,7 @@
 package com.codelab.layouts
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -33,62 +34,111 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.launch
 
+/**
+ * An example [Column] whose children consist of 100 [Text] Composables displaying the text "Item #n"
+ * where `n` is the index of the item in the [Column]. The `style` configuration for the text is the
+ * `subtitle1` [TextStyle] from [MaterialTheme.typography] (`subtitle1` is the largest subtitle, and
+ * is typically reserved for medium-emphasis text that is shorter in length). The default value from
+ * [MaterialTheme] is: Font: Roboto, Weight: Normal, Size: 16px, and Letter spacing: 0.15px. Note:
+ * this [Column] is not scroll-able.
+ */
 @Composable
 fun SimpleColumn() {
     Column {
         repeat(100) {
-            Text("Item #$it", style = MaterialTheme.typography.subtitle1)
+            Text(text = "Item #$it", style = MaterialTheme.typography.subtitle1)
         }
     }
 }
 
+/**
+ * This is a Preview of the [SimpleColumn] Composable.
+ */
 @Preview
 @Composable
 fun SimpleColumnPreview() {
     SimpleColumn()
 }
 
+/**
+ * An example of a scroll-able [Column] whose children consist of 100 [Text] Composables displaying
+ * the text "Item #n" where `n` is the index of the item in the [Column]. The `style` configuration
+ * for the text is the `subtitle1` [TextStyle] from [MaterialTheme.typography] (`subtitle1` is the
+ * largest subtitle, and is typically reserved for medium-emphasis text that is shorter in length).
+ * The default value from [MaterialTheme] is: Font: Roboto, Weight: Normal, Size: 16px, and Letter
+ * spacing: 0.15px. We initialize our [ScrollState] variable `val scrollState` using the method
+ * [rememberScrollState] (we will save the scrolling position of our [Column] in this state). Then
+ * we pass a [Modifier.verticalScroll] that uses `scrollState` as its `state` argument when we
+ * invoke our [Column] Composable.
+ */
 @Composable
 fun SimpleList() {
     // We save the scrolling position with this state
-    val scrollState = rememberScrollState()
+    val scrollState: ScrollState = rememberScrollState()
 
-    Column(Modifier.verticalScroll(scrollState)) {
+    Column(modifier = Modifier.verticalScroll(state = scrollState)) {
         repeat(100) {
-            Text("Item #$it", style = MaterialTheme.typography.subtitle1)
+            Text(text = "Item #$it", style = MaterialTheme.typography.subtitle1)
         }
     }
 }
 
+/**
+ * The Preview of our [SimpleList] Composable
+ */
 @Preview
 @Composable
 fun SimpleListPreview() {
     SimpleList()
 }
 
+/**
+ * An example of a scroll-able [LazyColumn] whose children consist of 100 [Text] Composables displaying
+ * the text "Item #n" where `n` is the index of the item in the [LazyColumn]. The `style` configuration
+ * for the text is the `subtitle1` [TextStyle] from [MaterialTheme.typography] (`subtitle1` is the
+ * largest subtitle, and is typically reserved for medium-emphasis text that is shorter in length).
+ * The default value from [MaterialTheme] is: Font: Roboto, Weight: Normal, Size: 16px, and Letter
+ * spacing: 0.15px. We initialize our [ScrollState] variable `val scrollState` using the method
+ * [rememberScrollState] (we will save the scrolling position of our [Column] in this state). Then
+ * we use `scrollState` as the `state` argument of [LazyColumn], and use 100 as the `count` argument
+ * to `items` to have it create the 100 [Text] Composables that are to be the children of the
+ * [LazyColumn].
+ */
 @Composable
 fun LazyList() {
     // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
 
     LazyColumn(state = scrollState) {
-        items(100) {
-            Text("Item #$it", style = MaterialTheme.typography.subtitle1)
+        items(count = 100) {
+            Text(text = "Item #$it", style = MaterialTheme.typography.subtitle1)
         }
     }
 }
 
+/**
+ * The Preview of the [LazyList] Composable.
+ */
 @Preview
 @Composable
 fun LazyListPreview() {
     LazyList()
 }
 
+/**
+ * This Composable consists of a [Row] that holds a 50.dp by 50.dp [Image], a 10.dp [Spacer] and a
+ * [Text] displaying the `text` "Item #$index" where "$index" refers to our [Int] parameter [index].
+ * This Composable is used for each of the 100 `items` in the [LazyColumn] of [ImageList].
+ *
+ * @param index the number we should display in our [Text], in our case it will be the index of the
+ * [ImageListItem] in the [LazyColumn] of [ImageList].
+ */
 @Composable
 fun ImageListItem(index: Int) {
     Row(verticalAlignment = Alignment.CenterVertically) {
@@ -100,7 +150,7 @@ fun ImageListItem(index: Int) {
             modifier = Modifier.size(50.dp)
         )
         Spacer(Modifier.width(10.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
