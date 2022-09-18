518584801e0e99bbbe4f96be34dda8ff8825e23f
markgray <mark.t.gray@gmail.com>
2022-09-18 06:30:07 -0400

    Added comments in LayoutsCodelab

--- LayoutsCodelab/app/src/main/java/com/codelab/layouts/TwoTexts.kt
+++ LayoutsCodelab/app/src/main/java/com/codelab/layouts/TwoTexts.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -36,11 +37,32 @@ import com.codelab.layouts.ui.LayoutsCodelabTheme
 
 /**
  * A Composable that consists of a [Row] root Composable holding a [Text], a 1.dp wide [Divider],
- * and a second [Text].
+ * and a second [Text]. For its `modifier` argument the [Row] adds a [Modifier.height] to our
+ * [modifier] parameter which declares the preferred height of its content to be the same as the min
+ * intrinsic height of the content. The first [Text] uses as its `modifier` argument the
+ * `Modifier.weight` [Modifier] of `RowScope` to set its weight to 1f (the other [Text] does so too
+ * so they split evenly the space left after the [Divider] in their middle is measured), then adds
+ * a [Modifier.padding] to add 4.dp padding to its start, and a [Modifier.wrapContentWidth] of
+ * [Alignment.Start] which aligns its text to the start of the [Text]. The `text` argument of the
+ * first [Text] is our [String] parameter [text1]. The next widget in the [Row] is a [Divider] whose
+ * `color` argument sets the color of the dividing line to [Color.Black], and whose `modifier`
+ * argument uses [Modifier.fillMaxHeight] to have it fill the height of the incoming measurement
+ * constraint, to which is added a [Modifier.width] to set its width to 1.dp. Then comes the second
+ * [Text] which uses as its `modifier` argument the `Modifier.weight` [Modifier] of `RowScope` to
+ * set its weight to 1f (the other [Text] does so too so they split evenly the space left after the
+ * [Divider] in their middle is measured), then adds a [Modifier.padding] to add 4.dp padding to its
+ * end, and a [Modifier.wrapContentWidth] of [Alignment.End] which aligns its text to the end of the
+ * [Text].  The `text` argument of the second [Text] is our [String] parameter [text2].
+ *
+ * @param modifier a [Modifier] instance which allows our caller to modify our appearance or behavior.
+ * Our caller passes no value so the empty, default, or starter [Modifier] that contains no elements
+ * is used.
+ * @param text1 the text that the first [Text] in our [Row] should display.
+ * @param text2 the text that the second [Text] in our [Row] should display.
  */
 @Composable
 fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
-    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+    Row(modifier = modifier.height(intrinsicSize = IntrinsicSize.Min)) {
         Text(
             modifier = Modifier
                 .weight(1f)
@@ -48,17 +70,23 @@ fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
                 .wrapContentWidth(Alignment.Start),
             text = text1
         )
-        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(width = 1.dp))
         Text(
             modifier = Modifier
-                .weight(1f)
+                .weight(weight = 1f)
                 .padding(end = 4.dp)
-                .wrapContentWidth(Alignment.End),
+                .wrapContentWidth(align = Alignment.End),
             text = text2
         )
     }
 }
 
+/**
+ * The Preview of our [TwoTexts] Composable. Wrapped in our [LayoutsCodelabTheme] custom [MaterialTheme]
+ * is a [Surface] whose `content` is a [TwoTexts] Composable whose `text1` argument is "Hi" and whose
+ * `text2` argument is "there". (The "Hi" will be displayed by the first [Text] in the [Row] of [TwoTexts]
+ * and the "there" will be displayed by the second [Text].
+ */
 @Preview
 @Composable
 fun TwoTextsPreview() {
