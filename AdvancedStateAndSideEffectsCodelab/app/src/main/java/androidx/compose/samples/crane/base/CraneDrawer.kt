8fa5d1e46487172c19952cfe029061f9e504a871
markgray <mark.t.gray@gmail.com>
2022-09-21 07:33:08 -0400

    Added comments in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/CraneDrawer.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/CraneDrawer.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.samples.crane.R
@@ -32,13 +33,39 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.samples.crane.home.CraneHome
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 
+/**
+ * The list of titles for entries in our [CraneDrawer].
+ */
 private val screens = listOf("Find Trips", "My Trips", "Saved Trips", "Price Alerts", "My Account")
 
+/**
+ * This is the content of the Drawer sheet that can be pulled from the left side (right for RTL) of
+ * the [Scaffold] in our [CraneHome] Composable. Our root Composable is a [Column] whose `modifier`
+ * argument adds a [Modifier.fillMaxSize] to our [modifier] parameter to have its content fill its
+ * incoming measurement constraints, and adds a [Modifier.padding] to that to add 24.dp padding to
+ * the `start` and 48.dp to the `top` of the [Column]. The content of the [Column] is an [Image]
+ * whose `painter` argument is the [Painter] created by [painterResource] for the drawable whose
+ * resource ID is [R.drawable.ic_crane_drawer] (a stylized line drawing of a crane), and this is
+ * followed by a [Text] widget for each of the strings in the [List] of [String] field [screens],
+ * with the text drawn using the `h4` [TextStyle] of [MaterialTheme.typography] (this is defined
+ * by our [CraneTheme] to be the [Font] whose resource ID is [R.font.raleway_semibold] (the file
+ * raleway_semibold.ttf) with a `fontSize` of 34.sp, and a `fontWeight` of [FontWeight.W600]. There
+ * is a 24.dp high [Spacer] above each of the [Text] widgets in the [Column].
+ *
+ * @param modifier a [Modifier] that our caller can use to modify our appearance or behavior. Our
+ * caller does not pass one so the empty, default, or starter [Modifier] that contains no elements
+ * is used instead.
+ */
 @Composable
 fun CraneDrawer(modifier: Modifier = Modifier) {
     Column(
-        modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(start = 24.dp, top = 48.dp)
     ) {
@@ -47,12 +74,15 @@ fun CraneDrawer(modifier: Modifier = Modifier) {
             contentDescription = stringResource(R.string.cd_drawer)
         )
         for (screen in screens) {
-            Spacer(Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(height = 24.dp))
             Text(text = screen, style = MaterialTheme.typography.h4)
         }
     }
 }
 
+/**
+ * This is the Preview of our [CraneDrawer] Composable wrapped in our [CraneTheme] custom [MaterialTheme].
+ */
 @Preview
 @Composable
 fun CraneDrawerPreview() {
--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
@@ -17,6 +17,7 @@
 package androidx.compose.samples.crane.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.BackdropScaffold
 import androidx.compose.material.BackdropValue
 import androidx.compose.material.ExperimentalMaterialApi
@@ -38,7 +39,6 @@ import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.accompanist.insets.statusBarsPadding
 import kotlinx.coroutines.launch
 
 typealias OnExploreItemClicked = (ExploreModel) -> Unit
