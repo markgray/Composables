e6881790af2230e27d8fc6e071247c2aabc40245
markgray <mark.t.gray@gmail.com>
2022-09-22 06:11:11 -0400

    Added comments in AccessibilityCodelab

--- AccessibilityCodelab/app/src/main/java/com/example/jetnews/data/posts/PostsRepository.kt
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/data/posts/PostsRepository.kt
@@ -16,41 +16,76 @@
 
 package com.example.jetnews.data.posts
 
+import com.example.jetnews.data.interests.TopicSelection
 import com.example.jetnews.data.posts.impl.posts
 import com.example.jetnews.model.Post
+import com.example.jetnews.ui.home.HomeScreen
 import com.example.jetnews.utils.addOrRemove
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.sync.Mutex
 
 /**
  * Simplified implementation of PostsRepository that returns a hardcoded list of
  * posts with resources synchronously.
  */
+@Suppress("MemberVisibilityCanBePrivate")
 class PostsRepository {
-    // for now, keep the favorites in memory
+    /**
+     * This [MutableStateFlow] will emit its value whenever it changes. [observeFavorites] returns
+     * a reference to this private field as a [Flow] which a Composable could collect using the
+     * `collectAsState` method which Collects values from the Flow and represents its latest value
+     * via State (but no Composable in this app uses [observeFavorites]). It is updated by the
+     * [toggleFavorite] method, but [toggleFavorite] is not called anywhere.
+     */
     private val favorites = MutableStateFlow<Set<String>>(setOf())
 
     /**
-     * Get a specific JetNews post.
+     * Get a specific JetNews post. It searches the [List] of [Post] field [posts] using the [find]
+     * exension method for a [Post] whose [Post.id] property is equal to our [String] parameter
+     * [postId] and the first element it finds, or `null` if no such element was found. It is called
+     * by the `ArticleScreen` Composable (see the file ui/article/ArticleScreen.kt)
+     *
+     * @param postId the [Post.id] string we are to search for in the [List] of [Post] field [posts].
+     * @return the first [Post] in the [List] of [Post] field [posts] whose [Post.id] property matches
+     * our parameter [postId] or `null` if no such [Post] was found.
      */
     fun getPost(postId: String?): Post? {
         return posts.find { it.id == postId }
     }
 
     /**
-     * Get JetNews posts.
+     * Get JetNews posts. Public read-only access to our [List] of [Post] field [posts]. It is called
+     * by the [HomeScreen] Composable where it is used as its `posts` argument.
+     *
+     * @return a reference to our [List] of [Post] field [posts].
      */
     fun getPosts(): List<Post> {
         return posts
     }
 
     /**
-     * Observe the current favorites
+     * Observe the current favorites. This method returns a read-only reference to our [MutableStateFlow]
+     * field [favorites] which a Composable could collect from using the `collectAsState` method which
+     * Collects values from the [Flow] and represents its latest value via State (but no Composable
+     * in this app calls [observeFavorites]).
+     *
+     * @return a read-only reference to our private [MutableStateFlow] field [favorites]
      */
     fun observeFavorites(): Flow<Set<String>> = favorites
 
     /**
-     * Toggle a postId to be a favorite or not.
+     * Toggle a [postId] to be a favorite or not. It retrieves the [MutableSet] of [TopicSelection]
+     * from the [MutableStateFlow.value] of [favorites] to initialize the variable `val set`, then
+     * uses the [MutableSet.addOrRemove] extension function to add [postId] to the `set` if it is
+     * not already there or remove it if it is there. Finally it sets the value of [favorites] to
+     * `set` (this will cause [favorites] to emit its new value to anyone collecting it). This
+     * method is not called by anyone, but if it was it probably should be a suspend function with
+     * its use of [favorites] protected by a [Mutex] like `toggleTopicSelection` does in
+     * `InterestsRepository` (see the file data/interests/InterestsRepository.kt).
+     *
+     * @param postId the [String] whose presence in the [Set] of [String] held in the [MutableStateFlow]
+     * field [favorites] needs to be toggled.
      */
     fun toggleFavorite(postId: String) {
         val set = favorites.value.toMutableSet()
