b958c169fb15a3efe14f518bc8bf46490b097edb
markgray <mark.t.gray@gmail.com>
2022-09-24 09:08:41 -0400

    Added comments and a Preview in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -29,23 +29,52 @@ import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.samples.crane.R
+import androidx.compose.samples.crane.base.EditableUserInputState.Companion.Saver
+import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.compose.samples.crane.ui.captionTextStyle
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * This class is the "saveable" `state` used by [CraneEditableUserInput]. It holds a [MutableState]
- * of [String] in its `var text` field whose reads and writes are observed by Compose when it is
- * "remembered" using [rememberSaveable] which is done for a new class instance by the Composable
- * method [rememberEditableUserInputState]. The [Saver] in our companion object is used to [Bundle]
- * up the class for the call to [rememberSaveable].
+ * of [String] in its `var text` field whose reads and writes are observed by Compose when this
+ * class is "remembered" using [rememberSaveable] which is done for a new class instance by the
+ * Composable method [rememberEditableUserInputState]. The [Saver] in our companion object is used
+ * by [rememberSaveable] to [Bundle] up the class to save using the saved instance state mechanism.
+ *
+ * @param hint the "hint" that will be displayed to the user when the Composable is first composed.
+ * This will be the same value as the `initialText` parameter when the class is constructed and our
+ * [Boolean] property [isHint] will return `true` until our [text] field is modified.
+ * @param initialText the initial value to be passed to [mutableStateOf] when it creates our [String]
+ * field [text]. This will be the same value as the `hint` parameter when the class is constructed
+ * and our [Boolean] property [isHint] will return `true` until our [text] field is modified.
  */
 class EditableUserInputState(private val hint: String, initialText: String) {
+    /**
+     * This [MutableState] contains the current text that should be displayed by the [BasicTextField]
+     * as the input [String] shown in its text field (its `value` argument, and it is updated by its
+     * `onValueChange` lambda argument). Note that it starts our equal to the [hint] field when the
+     * [EditableUserInputState] is constructed by [rememberEditableUserInputState].
+     */
     var text: String by mutableStateOf(value = initialText)
 
+    /**
+     * Returns `true` if our [text] field is still equal to our [hint] field.
+     */
     val isHint: Boolean
         get() = text == hint
 
     companion object {
+        /**
+         * The [Saver] for our [EditableUserInputState] class. It uses the [listSaver] method to
+         * create a `ListSaver` whose `save` lambda returns a [List] of [String] containing the
+         * [hint] field and the [text] field of the [EditableUserInputState] it is saving, and its
+         * `restore` lambda returns a new instance of [EditableUserInputState] whose `hint` argument
+         * is the [String] at index 0 in the [List] of [String] passed the lambda and whose
+         * `initialText` argument is the [String] at index 1 in the [List] of [String] passed the
+         * lambda.
+         */
         val Saver: Saver<EditableUserInputState, *> = listSaver(
             save = { listOf(it.hint, it.text) },
             restore = {
@@ -58,12 +87,30 @@ class EditableUserInputState(private val hint: String, initialText: String) {
     }
 }
 
+/**
+ * Constructs a new instance of [EditableUserInputState] whose `hint` and `initialText` arguments
+ * are the same as our [String] parameter [hint] and uses it as the `init` argument when it calls
+ * [rememberSaveable]. The `inputs` argument of [rememberSaveable] is our [hint] parameter (when
+ * it changes it will cause the state to reset and `init` to be rerun) and the `saver` argument is
+ * the [EditableUserInputState.Saver] method. The call to [rememberSaveable] will cause compose to
+ * "remember" the instance of [EditableUserInputState] created by `init` across recompositions and
+ * the stored value will survive the activity or process recreation using the saved instance state
+ * mechanism.
+ */
 @Composable
 fun rememberEditableUserInputState(hint: String): EditableUserInputState =
-    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
+    rememberSaveable(hint, saver = Saver) {
         EditableUserInputState(hint = hint, initialText = hint)
     }
 
+/**
+ * This is a wrapper around [CraneBaseUserInput] which uses a [BasicTextField] as the `content`
+ * lambda of [CraneBaseUserInput]. In addition the `tintIcon` and `showCaption` arguments of the
+ * [CraneBaseUserInput] are controlled by lambdas which return `true` if the user has changed the
+ * text content of the [BasicTextField] from the original `hint` that our [EditableUserInputState]
+ * parameter [state] was constructed with (the [EditableUserInputState.isHint] property returns
+ * `false`).
+ */
 @Composable
 fun CraneEditableUserInput(
     state: EditableUserInputState = rememberEditableUserInputState(hint = ""),
@@ -86,6 +133,19 @@ fun CraneEditableUserInput(
                 MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
             },
             cursorBrush = SolidColor(LocalContentColor.current)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CraneEditableUserInputPreview() {
+    val editableUserInputState = rememberEditableUserInputState(hint = "Choose Destination")
+    CraneTheme {
+        CraneEditableUserInput(
+            state = editableUserInputState,
+            caption = "To",
+            vectorImageId = R.drawable.ic_plane
         )
     }
 }
