70dff7dce2d796aad19b16442446b4b96b7bcb39
markgray <mark.t.gray@gmail.com>
2022-09-27 08:50:41 -0400

    Added comments in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
@@ -54,12 +54,14 @@ import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.samples.crane.details.DetailsActivity
 import androidx.compose.samples.crane.details.launchDetailsActivity
 import androidx.compose.samples.crane.home.CraneScreen
+import androidx.compose.samples.crane.home.MainActivity
 import androidx.compose.samples.crane.home.MainViewModel
 import androidx.compose.samples.crane.home.OnExploreItemClicked
 import androidx.compose.samples.crane.ui.BottomSheetShape
 import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.compose.samples.crane.ui.crane_caption
 import androidx.compose.samples.crane.ui.crane_divider_color
+import androidx.compose.samples.crane.ui.craneTypography
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -68,6 +70,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
@@ -111,7 +114,7 @@ import kotlinx.coroutines.launch
  * argument our `listState` [LazyListState] variable. [ExploreList] will then display each of the
  * [ExploreModel]'s in the [List] of [ExploreModel]'s in a [LazyColumn] using an [ExploreItem] to
  * display each individual [ExploreModel]. It will use the [LazyListState] in our variable `listState`
- * as the `state` of the [LazyColumn] which will allow us to use `listState` to to observe and control
+ * as the `state` of the [LazyColumn] which will allow us to use `listState` to observe and control
  * scrolling from the [Box]. Next we initialize our [Boolean] variable `val showButton` by using the
  * [remember] method to remember a derived [State] which returns `true` if the `listState` method
  * [LazyListState.firstVisibleItemIndex] indicates that the index of the first item that is visible
@@ -202,6 +205,36 @@ fun ExploreSection(
     }
 }
 
+/**
+ * This Composable uses a [LazyColumn] to display [ExploreItem] Composables which render the info in
+ * each of the [ExploreModel] objects in our [List] of [ExploreModel] parameter [exploreList]. Our
+ * root Composable is our [LazyColumn], whose `modifier` argument is our [Modifier] parameter [modifier]
+ * and whose `state` argument is our [LazyListState] parameter [listState]. The `content` of the
+ * [LazyColumn] is an [items] whose `items` argument is our [exploreList] parameter. It loops through
+ * all of the [ExploreModel] objects in that [List] feeding them as `exploreItem` to its `itemContent`
+ * which consists of a [Column] whose `modifier` argument is a `LazyItemScope` `Modifier.fillParentMaxWidth`
+ * which has the [Column] fill the maximum width of its parent's measurement constraints. The [Column]
+ * holds an [ExploreItem] whose `modifier` argument is a `LazyItemScope` `Modifier.fillParentMaxWidth`,
+ * whose `item` argument is the `exploreItem` [ExploreModel] it is to render, and whose `onItemClicked`
+ * is our [OnExploreItemClicked] parameter [onItemClicked]. Below the [ExploreItem] in the [Column]
+ * is a [Divider] whose `color` is [crane_divider_color] (which is defined to be [Color.LightGray] in
+ * the file ui/CraneTheme.kt). Below all of the [items] is a `LazyListScope` `item` holding a [Spacer]
+ * whose `modifier` is a [Modifier.windowInsetsBottomHeight] which sets the height to that of the
+ * `statusBars` [WindowInsets] at the bottom of the screen.
+ *
+ * @param exploreList the [List] of [ExploreModel] objects we are to display in our [LazyColumn].
+ * @param onItemClicked an [OnExploreItemClicked] lambda that the [ExploreItem]'s in our [LazyColumn]
+ * should call with the [ExploreModel] they hold when it is clicked. It is passed down a series of
+ * Composable parents from the `onCreate` override of [MainActivity] and is a lambda that calls the
+ * [launchDetailsActivity] method to have it launch the [DetailsActivity] to display the [ExploreModel]
+ * that was clicked.
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or behavior.
+ * Our caller does not pass us one so the empty, default, or starter [Modifier] that contains no elements
+ * is used instead.
+ * @param listState a [LazyListState] that our caller can use to observe and control scrolling of our
+ * [LazyColumn]. Our caller [ExploreSection] uses it to observe when the first visible item is past
+ * the first item of the [List], and to scroll to item 0 when its [FloatingActionButton] is clicked.
+ */
 @Composable
 private fun ExploreList(
     exploreList: List<ExploreModel>,
@@ -210,7 +243,7 @@ private fun ExploreList(
     listState: LazyListState = rememberLazyListState()
 ) {
     LazyColumn(modifier = modifier, state = listState) {
-        items(items = exploreList) { exploreItem ->
+        items(items = exploreList) { exploreItem: ExploreModel ->
             Column(modifier = Modifier.fillParentMaxWidth()) {
                 ExploreItem(
                     modifier = Modifier.fillParentMaxWidth(),
@@ -226,6 +259,39 @@ private fun ExploreList(
     }
 }
 
+/**
+ * Displays the information contained in its [ExploreModel] parameter [item], and calls its parameter
+ * [onItemClicked] with that [item] if it is clicked. Our root Composable is a [Row] whose `modifier`
+ * argument adds to our [Modifier] parameter [modifier] a [Modifier.clickable] that calls our
+ * [OnExploreItemClicked] parameter [onItemClicked] with our [ExploreModel] parameter [item] and adds
+ * a [Modifier.padding] whose `top` padding is 12.dp, and whose `bottom` padding is 12.sp on to that
+ * as well. The `content` of the [Row] is:
+ *  - An [ExploreImageContainer] (which is just a wrapper around a [Surface] whose size is 60.dp by
+ *  60.dp, and whose shape is a [RoundedCornerShape] of 4.dp) whose `content` is a [Box] which holds
+ *  an [Image] downloaded from the URL in the [ExploreModel.imageUrl] field of [item] or an [Image]
+ *  of the drawable resource ID [R.drawable.ic_crane_logo] while the photo is being loaded (ie. when
+ *  the [AsyncImagePainter.state] of the [AsyncImagePainter] variable `val painter` is
+ *  [AsyncImagePainter.State.Loading])
+ *  - A [Spacer] whose width is 24.dp
+ *  - A [Column] whose top entry is a [Text] displaying the [City.nameToDisplay] field of the
+ *  [ExploreModel.city] field of [item] using the [TextStyle] `h6` of [MaterialTheme.typography] (which
+ *  is the [FontFamily] `craneFontFamily` for [FontWeight.W400] (the [Font] with resource ID
+ *  [R.font.raleway_regular] and `fontSize` 20.sp). Below this is a [Spacer] that is 8.dp hight, followed
+ *  by another [Text] displaying the [ExploreModel.description] field of [item] using a copy of the
+ *  [TextStyle] `caption` of [MaterialTheme.typography] whose `color` is modified to be [crane_caption]
+ *  ([Color.DarkGray]). The base [TextStyle] of `caption` defined by [craneTypography] is the [FontFamily]
+ *  `craneFontFamily` for [FontWeight.W500] (the [Font] with resource ID [R.font.raleway_medium] and
+ *  `fontSize` 12.sp).
+ *
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance or behavior.
+ * Our caller calls us with a `LazyItemScope` `Modifier.fillParentMaxWidth` to have us fill the maximum
+ * width of our parent's measurement constraints.
+ * @param item the [ExploreModel] whose information we should display.
+ * @param onItemClicked an [OnExploreItemClicked] we should call with our [ExploreModel] parameter
+ * [item] as its argument. If you trace back up the Composable tree you will find that it is a lambda
+ * created in the `onCreate` override of [MainActivity] that calls the [launchDetailsActivity] method
+ * to have it launch the [DetailsActivity] to display the [ExploreModel] that was clicked.
+ */
 @Composable
 private fun ExploreItem(
     modifier: Modifier = Modifier,
@@ -263,13 +329,13 @@ private fun ExploreItem(
                 }
             }
         }
-        Spacer(Modifier.width(24.dp))
+        Spacer(modifier = Modifier.width(24.dp))
         Column {
             Text(
                 text = item.city.nameToDisplay,
                 style = MaterialTheme.typography.h6
             )
-            Spacer(Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(height = 8.dp))
             Text(
                 text = item.description,
                 style = MaterialTheme.typography.caption.copy(color = crane_caption)
@@ -278,9 +344,18 @@ private fun ExploreItem(
     }
 }
 
+/**
+ * This Composable is a wrapper around a [Surface] whose size is 60.dp by 60.dp, and whose shape is
+ * a [RoundedCornerShape] of 4.dp
+ *
+ * @param content the Composable `content` for our [Surface],
+ */
 @Composable
 private fun ExploreImageContainer(content: @Composable () -> Unit) {
-    Surface(Modifier.size(width = 60.dp, height = 60.dp), RoundedCornerShape(4.dp)) {
+    Surface(
+        modifier = Modifier.size(width = 60.dp, height = 60.dp),
+        shape = RoundedCornerShape(4.dp)
+    ) {
         content()
     }
 }
