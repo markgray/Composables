ec620784b12a51b93ebcef67f2afb7bcff53ad04
markgray <mark.t.gray@gmail.com>
2022-10-06 08:30:54 -0400

    Added comments in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/CraneHome.kt
@@ -16,12 +16,16 @@
 
 package androidx.compose.samples.crane.home
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.BackdropScaffold
 import androidx.compose.material.BackdropValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -36,31 +40,83 @@ import androidx.compose.samples.crane.base.CraneTabBar
 import androidx.compose.samples.crane.base.CraneTabs
 import androidx.compose.samples.crane.base.ExploreSection
 import androidx.compose.samples.crane.data.ExploreModel
+import androidx.compose.samples.crane.details.launchDetailsActivity
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
+/**
+ * This `typealias` for a function which takes an [ExploreModel] and returns [Unit] probably exists
+ * because [OnExploreItemClicked] is easier to type, but then again it does make its purpose clearer.
+ */
 typealias OnExploreItemClicked = (ExploreModel) -> Unit
 
+/**
+ * This `enum` is used to identify which [Tab] in the [TabRow] of [CraneTabs] has been selected by
+ * the user. It is then used to determine which Composable is rendered and where the data it uses
+ * comes from (see the `frontLayerContent` argument of the [BackdropScaffold] in [CraneHomeContent]
+ * for the different Composables and datasets used depending on which [Tab] has been selected).
+ */
 enum class CraneScreen {
-    Fly, Sleep, Eat
+    /**
+     * When the [Tab] using this `enum` value as its index is clicked the user is able to find out
+     * flight information for the various cities he can fly to.
+     */
+    Fly,
+
+    /**
+     * When the [Tab] using this `enum` value as its index is clicked the user is able to find out
+     * hotel information for the various cities.
+     */
+    Sleep,
+
+    /**
+     * When the [Tab] using this `enum` value as its index is clicked the user is able to find out
+     * restaurant information for the various cities.
+     */
+    Eat
 }
 
+/**
+ * This Composable is called by the Composable `MainScreen` main screen of [MainActivity] after it
+ * briefly shows [LandingScreen] when starting up, and its children produce the UI of [MainActivity].
+ * Its root Composable is a [Scaffold] whose `scaffoldState` argument is our remembered [ScaffoldState]
+ * variable, whose `modifier` argument is a [Modifier.statusBarsPadding] (adds padding to accommodate
+ * the status bars insets), and whose `drawerContent` argument is a [CraneDrawer] Composable. In the
+ * `content` of the [Scaffold] we first remember our [CoroutineScope] variable `val scope` then call
+ * the [CraneHomeContent] Composable with its `modifier` argument our [Modifier] parameter [modifier]
+ * with a [Modifier.padding] of the [PaddingValues] the [Scaffold] passes its `content` in the `padding`
+ * variable, with our [OnExploreItemClicked] parameter [onExploreItemClicked] as its `onExploreItemClicked`
+ * argument (which traces back to the `onCreate` override of [MainActivity] and is a lambda which will call
+ * the method [launchDetailsActivity] with the [ExploreModel] that was clicked), and as the `openDrawer`
+ * argument of [CraneHomeContent] we pass a lambda which launches a coroutine using the `scope`
+ * [CoroutineScope] which calls the `open` method of the [ScaffoldState.drawerState] of our [ScaffoldState]
+ * variable `scaffoldState`.
+ *
+ * @param onExploreItemClicked the [OnExploreItemClicked] method that should be called with the
+ * [ExploreModel] that was clicked. In our case this traces back to the `onCreate` override of
+ * [MainActivity] and is a lambda which will call the method [launchDetailsActivity] with the
+ * [ExploreModel] that was clicked
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. Our caller ([MainScreen]) does not pass any so the empty, default, or starter [Modifier]
+ * that contains no elements is used instead.
+ */
 @Composable
 fun CraneHome(
     onExploreItemClicked: OnExploreItemClicked,
     modifier: Modifier = Modifier,
 ) {
-    val scaffoldState = rememberScaffoldState()
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
     Scaffold(
         scaffoldState = scaffoldState,
         modifier = Modifier.statusBarsPadding(),
         drawerContent = {
             CraneDrawer()
         }
-    ) { padding ->
-        val scope = rememberCoroutineScope()
+    ) { padding: PaddingValues ->
+        val scope: CoroutineScope = rememberCoroutineScope()
         CraneHomeContent(
             modifier = modifier.padding(padding),
             onExploreItemClicked = onExploreItemClicked,
@@ -74,6 +130,9 @@ fun CraneHome(
     }
 }
 
+/**
+ * This Composable is called to be the `content` of the [Scaffold] of [CraneHome].
+ */
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CraneHomeContent(
@@ -93,13 +152,17 @@ fun CraneHomeContent(
         scaffoldState = rememberBackdropScaffoldState(BackdropValue.Revealed),
         frontLayerScrimColor = Color.Unspecified,
         appBar = {
-            HomeTabBar(openDrawer, tabSelected, onTabSelected = { tabSelected = it })
+            HomeTabBar(
+                openDrawer = openDrawer,
+                tabSelected = tabSelected,
+                onTabSelected = { tabSelected = it }
+            )
         },
         backLayerContent = {
             SearchContent(
-                tabSelected,
-                viewModel,
-                onPeopleChanged
+                tabSelected = tabSelected,
+                viewModel = viewModel,
+                onPeopleChanged = onPeopleChanged
             )
         },
         frontLayerContent = {
