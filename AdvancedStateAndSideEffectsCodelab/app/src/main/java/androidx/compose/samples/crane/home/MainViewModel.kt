f7121a51b89f0c34e1912fadaee3b536bfd75cdd
markgray <mark.t.gray@gmail.com>
2022-10-11 09:11:19 -0400

    Added comments in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -16,13 +16,20 @@
 
 package androidx.compose.samples.crane.home
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.samples.crane.base.ExploreSection
 import androidx.compose.samples.crane.data.DestinationsRepository
 import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.samples.crane.di.DefaultDispatcher
+import androidx.compose.samples.crane.di.DispatchersModule
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.internal.lifecycle.HiltViewModelFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -30,18 +37,68 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import kotlin.random.Random
 
+/**
+ * The maximum number of people that can travel
+ */
 const val MAX_PEOPLE = 4
 
+/**
+ * This is the [ViewModel] used by [MainActivity]. It is injected as the default value of the
+ * `viewModel` parameter of [CraneHomeContent] using the [viewModel] method. The `HiltViewModel`
+ * annotation identifies a [ViewModel] for construction injection. The [ViewModel] annotated with
+ * HiltViewModel will be available for creation by the [HiltViewModelFactory] and can be retrieved
+ * by default in an `Activity` or `Fragment` annotated with `AndroidEntryPoint`. The `HiltViewModel`
+ * containing a constructor annotated with `Inject` will have its dependencies defined in the
+ * constructor parameters injected by Dagger's Hilt. The generated java class
+ * `MainViewModel_Factory.java` is used to provide instances of [MainViewModel].
+ *
+ * @param destinationsRepository the [DestinationsRepository] singleton injected by Hilt that we
+ * should use in order to use the three [List] of [ExploreModel]'s it provides:
+ * [DestinationsRepository.destinations], [DestinationsRepository.hotels] and
+ * [DestinationsRepository.restaurants].
+ * @param defaultDispatcher the [CoroutineDispatcher] that is injected by Hilt for the qualifier
+ * `@DefaultDispatcher`, which is the [Dispatchers.Default] that is returned by the method
+ * [DispatchersModule.provideDefaultDispatcher].
+ */
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val destinationsRepository: DestinationsRepository,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
+    /**
+     * The [List] of [ExploreModel] which is used as the dataset for the `frontLayerContent` of the
+     * [BackdropScaffold] of [CraneHomeContent] when the [CraneScreen.Sleep] tab of [CraneHomeContent]
+     * is selected (file home/CraneHome.kt). A when statement chooses which of three [ExploreSection]
+     * Composables calls is executed, and [ExploreSection] then has its `ExploreList` Composable
+     * display it in its [LazyColumn].
+     */
     val hotels: List<ExploreModel> = destinationsRepository.hotels
+
+    /**
+     * The [List] of [ExploreModel] which is used as the dataset for the `frontLayerContent` of the
+     * [BackdropScaffold] of [CraneHomeContent] when the [CraneScreen.Eat] tab of [CraneHomeContent]
+     * is selected (file home/CraneHome.kt). A when statement chooses which of three [ExploreSection]
+     * Composables calls is executed, and [ExploreSection] then has its `ExploreList` Composable
+     * display it in its [LazyColumn].
+     */
     val restaurants: List<ExploreModel> = destinationsRepository.restaurants
 
+    /**
+     *
+     */
     private val _suggestedDestinations = MutableStateFlow<List<ExploreModel>>(emptyList())
+
+    /**
+     * Public read-only access to our [MutableStateFlow] of [List] of [ExploreModel] field
+     * [_suggestedDestinations]. [CraneHomeContent] uses the `collectAsState` extension function
+     * on this property to initialize its [List] of [ExploreModel] variable `suggestedDestinations`,
+     * which is then used as the dataset for the `frontLayerContent` of the [BackdropScaffold] of
+     * [CraneHomeContent] when the [CraneScreen.Fly] tab of [CraneHomeContent] is selected (file
+     * home/CraneHome.kt). A when statement chooses which of three [ExploreSection] Composables
+     * calls is executed, and [ExploreSection] then has its `ExploreList` Composable display it in
+     * its [LazyColumn].
+     */
     val suggestedDestinations: StateFlow<List<ExploreModel>>
         get() = _suggestedDestinations
 
@@ -54,9 +111,9 @@ class MainViewModel @Inject constructor(
             if (people > MAX_PEOPLE) {
                 _suggestedDestinations.value = emptyList()
             } else {
-                val newDestinations = withContext(defaultDispatcher) {
+                val newDestinations: List<ExploreModel> = withContext(defaultDispatcher) {
                     destinationsRepository.destinations
-                        .shuffled(Random(people * (1..100).shuffled().first()))
+                        .shuffled(random = Random(people * (1..100).shuffled().first()))
                 }
                 _suggestedDestinations.value = newDestinations
             }
@@ -65,7 +122,7 @@ class MainViewModel @Inject constructor(
 
     fun toDestinationChanged(newDestination: String) {
         viewModelScope.launch {
-            val newDestinations = withContext(defaultDispatcher) {
+            val newDestinations: List<ExploreModel> = withContext(defaultDispatcher) {
                 destinationsRepository.destinations
                     .filter { it.city.nameToDisplay.contains(newDestination) }
             }
