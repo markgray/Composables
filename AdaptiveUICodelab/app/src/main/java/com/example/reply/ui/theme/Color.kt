7ba275294c262de33797d564dd59088ae1d4e66c
markgray <mark.t.gray@gmail.com>
2022-10-27 09:08:58 -0400

    Added comments in AdaptiveUICodelab

--- AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/theme/Color.kt
+++ AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/theme/Color.kt
@@ -16,62 +16,349 @@
 
 package com.example.reply.ui.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
 
 // Generate them via theme builder
 // https://material-foundation.github.io/material-theme-builder/#/custom
 
-val replyLightPrimary = Color(0xFF825500)
-val replyLightOnPrimary = Color(0xFFFFFFFF)
-val replyLightPrimaryContainer = Color(0xFFFFDDAE)
-val replyLightOnPrimaryContainer = Color(0xFF2A1800)
-val replyLightSecondary = Color(0xFF6F5B40)
-val replyLightOnSecondary = Color(0xFFFFFFFF)
-val replyLightSecondaryContainer = Color(0xFFFADEBC)
-val replyLightOnSecondaryContainer = Color(0xFF271904)
-val replyLightTertiary = Color(0xFF516440)
-val replyLightOnTertiary = Color(0xFFFFFFFF)
-val replyLightTertiaryContainer = Color(0xFFD3EABC)
-val replyLightOnTertiaryContainer = Color(0xFF102004)
-val replyLightError = Color(0xFFBA1B1B)
-val replyLightErrorContainer = Color(0xFFFFDAD4)
-val replyLightOnError = Color(0xFFFFFFFF)
-val replyLightOnErrorContainer = Color(0xFF410001)
-val replyLightBackground = Color(0xFFFCFCFC)
-val replyLightOnBackground = Color(0xFF1F1B16)
-val replyLightSurface = Color(0xFFFCFCFC)
-val replyLightOnSurface = Color(0xFF1F1B16)
-val replyLightSurfaceVariant = Color(0xFFF0E0CF)
-val replyLightOnSurfaceVariant = Color(0xFF4F4539)
-val replyLightOutline = Color(0xFF817567)
-val replyLightInverseOnSurface = Color(0xFFF9EFE6)
-val replyLightInverseSurface = Color(0xFF34302A)
-val replyLightPrimaryInverse = Color(0xFFFFB945)
+/**
+ * Used as the [ColorScheme.primary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The primary color is the color displayed most frequently across your app’s screens and
+ * components.
+ */
+val replyLightPrimary: Color = Color(0xFF825500)
+
+/**
+ * Used as the [ColorScheme.onPrimary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. It is the Color used for text and icons displayed on top of the primary color.
+ */
+val replyLightOnPrimary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.primaryContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The preferred tonal color of containers.
+ */
+val replyLightPrimaryContainer: Color = Color(0xFFFFDDAE)
+
+/**
+ * Used as the [ColorScheme.onPrimaryContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The color (and state variants) that should be used for content on
+ * top of [ColorScheme.primaryContainer].
+ */
+val replyLightOnPrimaryContainer: Color = Color(0xFF2A1800)
+
+/**
+ * Used as the [ColorScheme.secondary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The secondary color provides more ways to accent and distinguish your product. Secondary
+ * colors are best for:
+ *  - Floating action buttons
+ *  - Selection controls, like checkboxes and radio buttons
+ *  - Highlighting selected text
+ *  - Links and headlines
+ */
+val replyLightSecondary: Color = Color(0xFF6F5B40)
+
+/**
+ * Used as the [ColorScheme.onSecondary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Color used for text and icons displayed on top of the secondary color.
+ */
+val replyLightOnSecondary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.secondaryContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. A tonal color to be used in containers.
+ */
+val replyLightSecondaryContainer: Color = Color(0xFFFADEBC)
+
+/**
+ * Used as the [ColorScheme.onSecondaryContainer] color of the `replyLightColorScheme` custom
+ * `lightColorScheme` that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered
+ * to be in 'light theme'. The color (and state variants) that should be used for content on
+ * top of [ColorScheme.secondaryContainer].
+ */
+val replyLightOnSecondaryContainer: Color = Color(0xFF271904)
+
+/**
+ * Used as the [ColorScheme.tertiary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The tertiary color that can be used to balance primary and secondary colors, or bring
+ * heightened attention to an element such as an input field.
+ */
+val replyLightTertiary: Color = Color(0xFF516440)
+
+/**
+ * Used as the [ColorScheme.onTertiary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Color used for text and icons displayed on top of the tertiary color.
+ */
+val replyLightOnTertiary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.tertiaryContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. A tonal color to be used in containers.
+ */
+val replyLightTertiaryContainer: Color = Color(0xFFD3EABC)
+
+/**
+ * Used as the [ColorScheme.onTertiaryContainer] color of the `replyLightColorScheme` custom
+ * `lightColorScheme` that our [ReplyTheme] custom [MaterialTheme] uses when the system is
+ * considered to be in 'light theme'. The color (and state variants) that should be used for content
+ * on top of [ColorScheme.tertiaryContainer]
+ */
+val replyLightOnTertiaryContainer: Color = Color(0xFF102004)
+
+/**
+ * Used as the [ColorScheme.error] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The error color is used to indicate errors in components, such as invalid text in a text
+ * field.
+ */
+val replyLightError: Color = Color(0xFFBA1B1B)
+
+/**
+ * Used as the [ColorScheme.errorContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The preferred tonal color of error containers.
+ */
+val replyLightErrorContainer: Color = Color(0xFFFFDAD4)
+
+/**
+ * Used as the [ColorScheme.onError] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The Color used for text and icons displayed on top of the error color.
+ */
+val replyLightOnError: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.onErrorContainer] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The color (and state variants) that should be used for content on
+ * top of [ColorScheme.errorContainer].
+ */
+val replyLightOnErrorContainer: Color = Color(0xFF410001)
+
+/**
+ * Used as the [ColorScheme.background] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The background color that appears behind scrollable content.
+ */
+val replyLightBackground: Color = Color(0xFFFCFCFC)
+
+/**
+ * Used as the [ColorScheme.onBackground] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Color used for text and icons displayed on top of the background color.
+ */
+val replyLightOnBackground: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.surface] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. The surface color that affect surfaces of components, such as cards, sheets, and menus.
+ */
+val replyLightSurface: Color = Color(0xFFFCFCFC)
+
+/**
+ * Used as the [ColorScheme.onSurface] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Color used for text and icons displayed on top of the surface color.
+ */
+val replyLightOnSurface: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.surfaceVariant] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Another option for a color with similar uses of [ColorScheme.surface].
+ */
+val replyLightSurfaceVariant: Color = Color(0xFFF0E0CF)
+
+/**
+ * Used as the [ColorScheme.onSurfaceVariant] color of the `replyLightColorScheme` custom
+ * `lightColorScheme` that our [ReplyTheme] custom [MaterialTheme] uses when the system is
+ * considered to be in 'light theme'. The color (and state variants) that can be used for content on top of
+ * [ColorScheme.surfaceVariant].
+ */
+val replyLightOnSurfaceVariant: Color = Color(0xFF4F4539)
+
+/**
+ * Used as the [ColorScheme.outline] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Subtle color used for boundaries. Outline color role adds contrast for accessibility
+ * purposes.
+ */
+val replyLightOutline: Color = Color(0xFF817567)
+
+/**
+ * Used as the [ColorScheme.inverseOnSurface] color of the `replyLightColorScheme` custom
+ * `lightColorScheme` that our [ReplyTheme] custom [MaterialTheme] uses when the system is
+ * considered to be in 'light theme'. A color that contrasts well with [ColorScheme.inverseSurface].
+ * Useful for content that sits on top of containers that are [ColorScheme.inverseSurface].
+ */
+val replyLightInverseOnSurface: Color = Color(0xFFF9EFE6)
+
+/**
+ * Used as the [ColorScheme.inverseSurface] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. A color that contrasts sharply with [ColorScheme.surface]. Useful for surfaces that sit on
+ * top of other surfaces with [ColorScheme.surface] color.
+ */
+val replyLightInverseSurface: Color = Color(0xFF34302A)
+
+/**
+ * Used as the [ColorScheme.inversePrimary] color of the `replyLightColorScheme` custom `lightColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'light
+ * theme'. Color to be used as a "primary" color in places where the inverse color scheme is needed,
+ * such as the button on a SnackBar.
+ */
+val replyLightPrimaryInverse: Color = Color(0xFFFFB945)
 
 
-val replyDarkPrimary = Color(0xFFFFB945)
-val replyDarkOnPrimary = Color(0xFF452B00)
-val replyDarkPrimaryContainer = Color(0xFF624000)
-val replyDarkOnPrimaryContainer = Color(0xFFFFDDAE)
-val replyDarkSecondary = Color(0xFFDDC3A2)
-val replyDarkOnSecondary = Color(0xFF3E2E16)
-val replyDarkSecondaryContainer = Color(0xFF56442B)
-val replyDarkOnSecondaryContainer = Color(0xFFFADEBC)
-val replyDarkTertiary = Color(0xFFB8CEA2)
-val replyDarkOnTertiary = Color(0xFF243516)
-val replyDarkTertiaryContainer = Color(0xFF3A4C2B)
-val replyDarkOnTertiaryContainer = Color(0xFFD3EABC)
-val replyDarkError = Color(0xFFFFB4A9)
-val replyDarkErrorContainer = Color(0xFF930006)
-val replyDarkOnError = Color(0xFF680003)
-val replyDarkOnErrorContainer = Color(0xFFFFDAD4)
-val replyDarkBackground = Color(0xFF1F1B16)
-val replyDarkOnBackground = Color(0xFFEAE1D9)
-val replyDarkSurface = Color(0xFF1F1B16)
-val replyDarkOnSurface = Color(0xFFEAE1D9)
-val replyDarkSurfaceVariant = Color(0xFF4F4539)
-val replyDarkOnSurfaceVariant = Color(0xFFD3C4B4)
-val replyDarkOutline = Color(0xFF9C8F80)
-val replyDarkInverseOnSurface = Color(0xFF1F1B16)
-val replyDarkInverseSurface = Color(0xFFEAE1D9)
-val replyDarkPrimaryInverse = Color(0xFF825500)
+/**
+ * Used as the [ColorScheme.primary] color of the `replyDarkColorScheme` custom `darkColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'dark
+ * theme'. The primary color is the color displayed most frequently across your app’s screens and
+ * components.
+ */
+val replyDarkPrimary: Color = Color(0xFFFFB945)
+
+/**
+ * Used as the [ColorScheme.onPrimary] color of the `replyDarkColorScheme` custom `darkColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'dark
+ * theme'. Color used for text and icons displayed on top of the primary color.
+ */
+val replyDarkOnPrimary: Color = Color(0xFF452B00)
+
+/**
+ * Used as the [ColorScheme.primaryContainer] color of the `replyDarkColorScheme` custom `darkColorScheme`
+ * that our [ReplyTheme] custom [MaterialTheme] uses when the system is considered to be in 'dark
+ * theme'. The preferred tonal color of containers.
+ */
+val replyDarkPrimaryContainer: Color = Color(0xFF624000)
+
+/**
+ * Used as the [ColorScheme.onPrimaryContainer] color of the `replyDarkColorScheme` custom
+ * `darkColorScheme` that our [ReplyTheme] custom [MaterialTheme] uses when the system is
+ * considered to be in 'dark theme'. The color (and state variants) that should be used for
+ * content on top of [ColorScheme.primaryContainer].
+ */
+val replyDarkOnPrimaryContainer: Color = Color(0xFFFFDDAE)
+
+/**
+ *
+ */
+val replyDarkSecondary: Color = Color(0xFFDDC3A2)
+
+/**
+ *
+ */
+val replyDarkOnSecondary: Color = Color(0xFF3E2E16)
+
+/**
+ *
+ */
+val replyDarkSecondaryContainer: Color = Color(0xFF56442B)
+
+/**
+ *
+ */
+val replyDarkOnSecondaryContainer: Color = Color(0xFFFADEBC)
+
+/**
+ *
+ */
+val replyDarkTertiary: Color = Color(0xFFB8CEA2)
+
+/**
+ *
+ */
+val replyDarkOnTertiary: Color = Color(0xFF243516)
+
+/**
+ *
+ */
+val replyDarkTertiaryContainer: Color = Color(0xFF3A4C2B)
+
+/**
+ *
+ */
+val replyDarkOnTertiaryContainer: Color = Color(0xFFD3EABC)
+
+/**
+ *
+ */
+val replyDarkError: Color = Color(0xFFFFB4A9)
+
+/**
+ *
+ */
+val replyDarkErrorContainer: Color = Color(0xFF930006)
+
+/**
+ *
+ */
+val replyDarkOnError: Color = Color(0xFF680003)
+
+/**
+ *
+ */
+val replyDarkOnErrorContainer: Color = Color(0xFFFFDAD4)
+
+/**
+ *
+ */
+val replyDarkBackground: Color = Color(0xFF1F1B16)
+
+/**
+ *
+ */
+val replyDarkOnBackground: Color = Color(0xFFEAE1D9)
+
+/**
+ *
+ */
+val replyDarkSurface: Color = Color(0xFF1F1B16)
+
+/**
+ *
+ */
+val replyDarkOnSurface: Color = Color(0xFFEAE1D9)
+
+/**
+ *
+ */
+val replyDarkSurfaceVariant: Color = Color(0xFF4F4539)
+
+/**
+ *
+ */
+val replyDarkOnSurfaceVariant: Color = Color(0xFFD3C4B4)
+
+/**
+ *
+ */
+val replyDarkOutline: Color = Color(0xFF9C8F80)
+
+/**
+ *
+ */
+val replyDarkInverseOnSurface: Color = Color(0xFF1F1B16)
+
+/**
+ *
+ */
+val replyDarkInverseSurface: Color = Color(0xFFEAE1D9)
+
+/**
+ *
+ */
+val replyDarkPrimaryInverse: Color = Color(0xFF825500)
