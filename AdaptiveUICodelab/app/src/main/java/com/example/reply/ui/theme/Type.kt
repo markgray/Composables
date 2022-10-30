f817fc1b59db8ebb8c2a6a218739e2722f265d37
markgray <mark.t.gray@gmail.com>
2022-10-30 08:59:20 -0400

    Added comments in AdaptiveUICodelab

--- AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/theme/Type.kt
+++ AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/theme/Type.kt
@@ -25,72 +25,169 @@ import androidx.compose.ui.unit.sp
  * Material 3 typography
  */
 val replyTypography: Typography = Typography(
+    /**
+     * `headlineLarge` is the largest headline, reserved for short, important text or numerals. For
+     * headlines, you can choose an expressive font, such as a display, handwritten, or script style.
+     * These unconventional font designs have details and intricacy that help attract the eye.
+     *  - `fontWeight` = [FontWeight.SemiBold],
+     *  - `fontSize` = 32.sp,
+     *  - `lineHeight` = 40.sp,
+     *  - `letterSpacing` = 0.sp
+     */
     headlineLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
+    /**
+     * headlineMedium is the second largest headline, reserved for short, important text or numerals.
+     * For headlines, you can choose an expressive font, such as a display, handwritten, or script
+     * style. These unconventional font designs have details and intricacy that help attract the eye.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 28.sp,
+     *  - lineHeight = 36.sp,
+     *  - letterSpacing = 0.sp
+     */
     headlineMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
+    /**
+     * headlineSmall is the smallest headline, reserved for short, important text or numerals. For
+     * headlines, you can choose an expressive font, such as a display, handwritten, or script style.
+     * These unconventional font designs have details and intricacy that help attract the eye.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 24.sp,
+     *  - lineHeight = 32.sp,
+     *  - letterSpacing = 0.sp
+     */
     headlineSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
+    /**
+     * titleLarge is the largest title, and is typically reserved for medium-emphasis text that is
+     * shorter in length. Serif or sans serif typefaces work well for subtitles.
+     */
     titleLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    /**
+     * titleMedium is the second largest title, and is typically reserved for medium-emphasis text
+     * that is shorter in length. Serif or sans serif typefaces work well for subtitles.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 16.sp,
+     *  - lineHeight = 24.sp,
+     *  - letterSpacing = 0.15.sp
+     */
     titleMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
+    /**
+     * titleSmall is the smallest title, and is typically reserved for medium-emphasis text that is
+     * shorter in length. Serif or sans serif typefaces work well for subtitles.
+     *  - fontWeight = FontWeight.Bold,
+     *  - fontSize = 14.sp,
+     *  - lineHeight = 20.sp,
+     *  - letterSpacing = 0.1.sp
+     */
     titleSmall = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
+    /**
+     * bodyLarge is the largest body, and is typically used for long-form writing as it works well
+     * for small text sizes. For longer sections of text, a serif or sans serif typeface is recommended.
+     *  - fontWeight = FontWeight.Normal,
+     *  - fontSize = 16.sp,
+     *  - lineHeight = 24.sp,
+     *  - letterSpacing = 0.15.sp
+     */
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
+    /**
+     * bodyMedium is the second largest body, and is typically used for long-form writing as it
+     * works well for small text sizes. For longer sections of text, a serif or sans serif typeface
+     * is recommended.
+     *  - fontWeight = FontWeight.Medium,
+     *  - fontSize = 14.sp,
+     *  - lineHeight = 20.sp,
+     *  - letterSpacing = 0.25.sp
+     */
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
+    /**
+     * bodySmall is the smallest body, and is typically used for long-form writing as it works well
+     * for small text sizes. For longer sections of text, a serif or sans serif typeface is recommended.
+     *  - fontWeight = FontWeight.Bold,
+     *  - fontSize = 12.sp,
+     *  - lineHeight = 16.sp,
+     *  - letterSpacing = 0.4.sp
+     */
     bodySmall = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
+    /**
+     * labelLarge text is a call to action used in different types of buttons (such as text, outlined
+     * and contained buttons) and in tabs, dialogs, and cards. Button text is typically sans serif,
+     * using all caps text.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 14.sp,
+     *  - lineHeight = 20.sp,
+     *  - letterSpacing = 0.1.sp
+     */
     labelLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
+    /**
+     * labelMedium is one of the smallest font sizes. It is used sparingly to annotate imagery or to
+     * introduce a headline.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 12.sp,
+     *  - lineHeight = 16.sp,
+     *  - letterSpacing = 0.5.sp
+     */
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
+    /**
+     * labelSmall is one of the smallest font sizes. It is used sparingly to annotate imagery or to
+     * introduce a headline.
+     *  - fontWeight = FontWeight.SemiBold,
+     *  - fontSize = 11.sp,
+     *  - lineHeight = 16.sp,
+     *  - letterSpacing = 0.5.sp
+     */
     labelSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
         fontSize = 11.sp,
--- AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
+++ AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -18,6 +18,8 @@ package com.example.reply.ui.utils
 
 import android.graphics.Rect
 import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowLayoutInfo
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
@@ -25,18 +27,57 @@ import kotlin.contracts.contract
  * Information about the posture of the device
  */
 sealed interface DevicePosture {
+    /**
+     * The default [DevicePosture] when both [isBookPosture] and [isSeparating] return `false` for
+     * the [FoldingFeature] that is found in the [WindowLayoutInfo] that the [WindowInfoTracker]
+     * emits for the device we are running on (ie. it a normal device without a fold or the ability
+     * to be separated).
+     */
     object NormalPosture : DevicePosture
 
+    /**
+     * To be this type of [DevicePosture] the [FoldingFeature] that is found in the [WindowLayoutInfo]
+     * that the [WindowInfoTracker] emits for the device we are running on has the [FoldingFeature.state]
+     * property [FoldingFeature.State.HALF_OPENED] and the [FoldingFeature.orientation] property
+     * [FoldingFeature.Orientation.VERTICAL] (The foldable device's hinge is in an intermediate
+     * position between opened and closed state, there is a non-flat angle between parts of the
+     * flexible screen or between physical screen panels, and the height of the [FoldingFeature] is
+     * greater than or equal to the width).
+     */
     data class BookPosture(
+        /**
+         * This is the [FoldingFeature.bounds] property for this device, the bounding rectangle of
+         * the feature within the application window in the window coordinate space.
+         */
         val hingePosition: Rect
     ) : DevicePosture
 
+    /**
+     * To be this type of [DevicePosture] the [FoldingFeature] that is found in the [WindowLayoutInfo]
+     * that the [WindowInfoTracker] emits for the device we are running on has the [FoldingFeature.state]
+     * [FoldingFeature.State.FLAT] and [FoldingFeature.isSeparating] is `true` (the foldable device
+     * is completely open, the screen space that is presented to the user is flat, and the [FoldingFeature]
+     * should be thought of as splitting the window into multiple physical areas that can be seen by
+     * users as logically separate).
+     */
     data class Separating(
+        /**
+         * This is the [FoldingFeature.bounds] property for this device, the bounding rectangle of
+         * the feature within the application window in the window coordinate space.
+         */
         val hingePosition: Rect,
+        /**
+         * This is the [FoldingFeature.orientation] property for this device,
+         * [FoldingFeature.Orientation.HORIZONTAL] if the width is greater than the height,
+         * [FoldingFeature.Orientation.VERTICAL] otherwise.
+         */
         var orientation: FoldingFeature.Orientation
     ) : DevicePosture
 }
 
+/**
+ *
+ */
 @OptIn(ExperimentalContracts::class)
 fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
     contract { returns(true) implies (foldFeature != null) }
@@ -44,6 +85,9 @@ fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
         foldFeature.orientation == FoldingFeature.Orientation.VERTICAL
 }
 
+/**
+ *
+ */
 @OptIn(ExperimentalContracts::class)
 fun isSeparating(foldFeature: FoldingFeature?): Boolean {
     contract { returns(true) implies (foldFeature != null) }
@@ -54,12 +98,33 @@ fun isSeparating(foldFeature: FoldingFeature?): Boolean {
  * Different type of navigation supported by app depending on size and state.
  */
 enum class ReplyNavigationType {
-    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+    /**
+     *
+     */
+    BOTTOM_NAVIGATION,
+
+    /**
+     *
+     */
+    NAVIGATION_RAIL,
+
+    /**
+     *
+     */
+    PERMANENT_NAVIGATION_DRAWER
 }
 
 /**
  * Content shown depending on size and state of device.
  */
 enum class ReplyContentType {
-    LIST_ONLY, LIST_AND_DETAIL
+    /**
+     *
+     */
+    LIST_ONLY,
+
+    /**
+     *
+     */
+    LIST_AND_DETAIL
 }
