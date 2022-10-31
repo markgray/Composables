60e387715be0dd23db677c5c1f4cff4eb0f0670f
markgray <mark.t.gray@gmail.com>
2022-10-31 06:52:38 -0400

    Added comments in AccessibilityCodelab

--- AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
@@ -17,6 +17,8 @@
 package com.example.jetnews.ui
 
 import android.annotation.SuppressLint
+import android.view.View
+import android.view.Window
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -24,22 +26,34 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.jetnews.data.AppContainer
 import com.example.jetnews.ui.theme.JetnewsTheme
+import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 
+/**
+ * This is the main Composable of the app. We start by initializing our [SystemUiController] variable
+ * `val systemUiController` using the [rememberSystemUiController] metod. It "remembers" it using the
+ * `current` [LocalView] and the [Window] associated with that [View] as `keys`. We then use the
+ * Composable [SideEffect] function to schedule its `effect` lambda to run when the current composition
+ * finishes and then every recomposition thereafter. In that lambda we call the method
+ * [SystemUiController.setSystemBarsColor] of `systemUiController` to set the color of the status and
+ * navigation bars [Color.Transparent], and to set its `darkIcons` property to `false` to prevent the
+ * use of dark navigation bar icons.
+ */
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun JetnewsApp(
     appContainer: AppContainer
 ) {
     JetnewsTheme {
-        val systemUiController = rememberSystemUiController()
+        val systemUiController: SystemUiController = rememberSystemUiController()
         SideEffect {
-            systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
+            systemUiController.setSystemBarsColor(color = Color.Transparent, darkIcons = false)
         }
 
         val navController = rememberNavController()
