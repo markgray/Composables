affce9f444ac189ed3c86bad7d50dd9d16e92d1b
markgray <mark.t.gray@gmail.com>
2022-11-04 09:01:26 -0400

    Added comments in AdaptiveUICodelab

--- AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -36,6 +36,7 @@ import androidx.compose.material.icons.filled.MenuOpen
 import androidx.compose.material.icons.outlined.Chat
 import androidx.compose.material.icons.outlined.People
 import androidx.compose.material.icons.outlined.Videocam
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -51,6 +52,8 @@ import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.PermanentNavigationDrawer
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
@@ -60,10 +63,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.window.layout.FoldingFeature
 import com.example.reply.R
 import com.example.reply.data.Email
+import com.example.reply.ui.theme.replyDarkInverseOnSurface
+import com.example.reply.ui.theme.replyLightInverseOnSurface
 import com.example.reply.ui.utils.DevicePosture
 import com.example.reply.ui.utils.ReplyContentType
 import com.example.reply.ui.utils.ReplyNavigationType
@@ -287,7 +293,52 @@ private fun ReplyNavigationWrapperUI(
 }
 
 /**
+ * This Composable is responsible for loading the Composables that are needed for the [ReplyNavigationType]
+ * and [ReplyContentType] that our caller has determined to be appropriate for the size and features of
+ * the device we are running on. Our root Composable is a [Row] whose `modifier` argument is a
+ * [Modifier.fillMaxSize] to have it fill the [Constraints.maxWidth] and [Constraints.maxHeight] of
+ * the incoming measurement constraints. The `content` of the [Row] is:
+ *  - an [AnimatedVisibility] wrapped [ReplyNavigationRail] whose `onDrawerClicked` argument is our
+ *  [onDrawerClicked] parameter. The `visible` argument of the [AnimatedVisibility] is `true` if our
+ *  [ReplyNavigationType] parameter [navigationType] is equal to [ReplyNavigationType.NAVIGATION_RAIL]
+ *  and the [ReplyNavigationRail] `content` is only visible when this is so, which is when the
+ *  [WindowWidthSizeClass] of the device is [WindowWidthSizeClass.Medium] (Represents the majority
+ *  of tablets in portrait and large unfolded inner displays in portrait) or [WindowWidthSizeClass.Expanded]
+ *  (Represents the majority of tablets in landscape and large unfolded inner displays in landscape)
+ *  and the [DevicePosture] of the device is [DevicePosture.BookPosture] (The foldable device's hinge
+ *  is in an intermediate position between opened and closed state, there is a non-flat angle between
+ *  parts of the flexible screen or between physical screen panels, and the height of its [FoldingFeature]
+ *  is greater than or equal to the width).
+ *  - The next Composable in the [Row] is a [Column] whose `modifier` argument is a [Modifier.fillMaxSize]
+ *  which has a [Modifier.background] that sets the background `color` to the [ColorScheme.inverseOnSurface]
+ *  [Color] of [MaterialTheme.colorScheme] which is [replyDarkInverseOnSurface] (a shade of black) for
+ *  our [darkColorScheme] and [replyLightInverseOnSurface] (a shade of white) for our [lightColorScheme].
  *
+ * The `content` of the [Column] depends on the value of our [ReplyContentType] parameter [contentType]:
+ *  - [ReplyContentType.LIST_AND_DETAIL] we call our [ReplyListAndDetailContent] Composable with its
+ *  `replyHomeUIState` argument our [ReplyHomeUIState] parameter [replyHomeUIState] and its `modifier`
+ *  argument a `ColumnScope` `Modifier.weight` of 1f which will cause it to occupy all the space left
+ *  in the [Column] once its sibling is measured and placed.
+ *  - For any other value of [ReplyContentType] we call our [ReplyListOnlyContent] Composable with its
+ *  `replyHomeUIState` argument our [ReplyHomeUIState] parameter [replyHomeUIState] and its `modifier`
+ *  argument a `ColumnScope` `Modifier.weight` of 1f which will cause it to occupy all the space left
+ *  in the [Column] once its sibling is measured and placed.
+ *  - At the bottom of the [Column] is an [AnimatedVisibility] wrapped [ReplyBottomNavigationBar].
+ *  The `visible` argument of the [AnimatedVisibility] is `true` if our [ReplyNavigationType] parameter
+ *  [navigationType] is equal to [ReplyNavigationType.BOTTOM_NAVIGATION] and the [ReplyBottomNavigationBar]
+ *  `content` is only visible when this is so, which is when the [WindowWidthSizeClass] of the device
+ *  is [WindowWidthSizeClass.Compact] (Represents the majority of phones in portrait) or is not a known
+ *  [WindowWidthSizeClass].
+ *
+ * @param navigationType the [ReplyNavigationType] that should be used for navigation on this device.
+ * One of [ReplyNavigationType.BOTTOM_NAVIGATION], [ReplyNavigationType.NAVIGATION_RAIL] or
+ * [ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER].
+ * @param contentType the [ReplyContentType] that should be displayed on this device. One of
+ * [ReplyContentType.LIST_ONLY] or [ReplyContentType.LIST_AND_DETAIL].
+ * @param replyHomeUIState the [ReplyHomeUIState] that can be used to retrieve the [Email] objects
+ * that are to be displayed.
+ * @param onDrawerClicked a lambda that can be called by [ReplyNavigationRail] to open the
+ * "Navigation Drawer".
  */
 @Composable
 fun ReplyAppContent(
@@ -302,9 +353,9 @@ fun ReplyAppContent(
                 onDrawerClicked = onDrawerClicked
             )
         }
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.inverseOnSurface)
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.inverseOnSurface)
         ) {
             if (contentType == ReplyContentType.LIST_AND_DETAIL) {
                 ReplyListAndDetailContent(
