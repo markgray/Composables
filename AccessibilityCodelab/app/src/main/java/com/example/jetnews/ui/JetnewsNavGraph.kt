bce0c7bf8970f49d98bed4dd58e8f8674624ef1b
markgray <mark.t.gray@gmail.com>
2022-11-04 07:23:50 -0400

    Added comments in AccessibilityCodelab

--- AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsNavGraph.kt
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsNavGraph.kt
@@ -21,11 +21,13 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetnews.data.AppContainer
+import com.example.jetnews.model.Post
 import com.example.jetnews.ui.MainDestinations.ARTICLE_ID_KEY
 import com.example.jetnews.ui.article.ArticleScreen
 import com.example.jetnews.ui.home.HomeScreen
@@ -36,12 +38,31 @@ import kotlinx.coroutines.launch
  * Destinations used in the ([JetnewsApp]).
  */
 object MainDestinations {
-    const val HOME_ROUTE = "home"
-    const val INTERESTS_ROUTE = "interests"
-    const val ARTICLE_ROUTE = "post"
-    const val ARTICLE_ID_KEY = "postId"
+    /**
+     * Route of the [composable] which launches the [HomeScreen] Composable
+     */
+    const val HOME_ROUTE: String = "home"
+
+    /**
+     * Route of the [composable] which launches the [InterestsScreen] Composable
+     */
+    const val INTERESTS_ROUTE: String = "interests"
+
+    /**
+     * Route of the [composable] which launches the [ArticleScreen] Composable
+     */
+    const val ARTICLE_ROUTE: String = "post"
+
+    /**
+     * Key under which the [Post.id] string of the article to be displayed is stored that the
+     * [ArticleScreen] Composable should display when the [ARTICLE_ROUTE] is navigated to.
+     */
+    const val ARTICLE_ID_KEY: String = "postId"
 }
 
+/**
+ *
+ */
 @Composable
 fun JetnewsNavGraph(
     appContainer: AppContainer,
@@ -57,20 +78,20 @@ fun JetnewsNavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(MainDestinations.HOME_ROUTE) {
+        composable(route = MainDestinations.HOME_ROUTE) {
             HomeScreen(
                 postsRepository = appContainer.postsRepository,
                 navigateToArticle = actions.navigateToArticle,
                 openDrawer = openDrawer
             )
         }
-        composable(MainDestinations.INTERESTS_ROUTE) {
+        composable(route = MainDestinations.INTERESTS_ROUTE) {
             InterestsScreen(
                 interestsRepository = appContainer.interestsRepository,
                 openDrawer = openDrawer
             )
         }
-        composable(route = "${MainDestinations.ARTICLE_ROUTE}/{$ARTICLE_ID_KEY}") { backStackEntry ->
+        composable(route = "${MainDestinations.ARTICLE_ROUTE}/{$ARTICLE_ID_KEY}") { backStackEntry: NavBackStackEntry ->
             ArticleScreen(
                 postId = backStackEntry.arguments?.getString(ARTICLE_ID_KEY),
                 onBack = actions.upPress,
@@ -84,9 +105,16 @@ fun JetnewsNavGraph(
  * Models the navigation actions in the app.
  */
 class MainActions(navController: NavHostController) {
+    /**
+     *
+     */
     val navigateToArticle: (String) -> Unit = { postId: String ->
-        navController.navigate("${MainDestinations.ARTICLE_ROUTE}/$postId")
+        navController.navigate(route = "${MainDestinations.ARTICLE_ROUTE}/$postId")
     }
+
+    /**
+     *
+     */
     val upPress: () -> Unit = {
         navController.navigateUp()
     }
