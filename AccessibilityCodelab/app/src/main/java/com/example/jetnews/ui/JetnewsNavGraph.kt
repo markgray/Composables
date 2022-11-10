601a80677e65cc599e1e564fb5abbfaed2d6e893
markgray <mark.t.gray@gmail.com>
2022-11-10 07:19:19 -0500

    Added comments in AccessibilityCodelab

--- AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsNavGraph.kt
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsNavGraph.kt
@@ -16,6 +16,7 @@
 
 package com.example.jetnews.ui
 
+import androidx.compose.material.DrawerState
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberScaffoldState
@@ -28,11 +29,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetnews.data.AppContainer
+import com.example.jetnews.data.interests.InterestsRepository
+import com.example.jetnews.data.posts.PostsRepository
 import com.example.jetnews.model.Post
 import com.example.jetnews.ui.MainDestinations.ARTICLE_ID_KEY
 import com.example.jetnews.ui.article.ArticleScreen
 import com.example.jetnews.ui.home.HomeScreen
 import com.example.jetnews.ui.interests.InterestsScreen
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 /**
@@ -64,7 +68,31 @@ object MainDestinations {
 /**
  * This Composable is used as the `content` of the [Scaffold] of the [JetnewsApp] Composable and
  * contains the [NavHost] which is used to navigate between the [HomeScreen], [InterestsScreen],
- * and [ArticleScreen] Composables.
+ * and [ArticleScreen] Composables. We initialize and remember our [MainActions] variable
+ * `val actions` to an instance constructed to use our [NavHostController] parameter [navController]
+ * to navigate between screens (we use [navController] as the key when we call [remember] which will
+ * cause `actions` to be reinitialized if [navController] is not equal to the value it held during
+ * the previous composition). We initialize and remember our [CoroutineScope] variable
+ * `val coroutineScope` to a [CoroutineScope] bound to this point in the composition using the
+ * [rememberCoroutineScope] method. We initialize our variable `val openDrawer` to a suspend lambda
+ * which uses the [CoroutineScope.launch] method of `coroutineScope` to launch a coroutine which
+ * calls the [DrawerState.open] method of the [ScaffoldState.drawerState] of [scaffoldState] to
+ * open the drawer of the [Scaffold]
+ *
+ * Our root Composable is a [NavHost] whose `navController` argument is our [navController] parameter,
+ * and whose `startDestination` argument is our [startDestination] parameter. The [NavHost] holds
+ * three [composable] whose `route` arguments are:
+ *  - [MainDestinations.HOME_ROUTE], its `content` is the [HomeScreen] Composable, whose [PostsRepository]
+ *  argument `postsRepository` is the [AppContainer.postsRepository] field of our [appContainer]
+ *  parameter, whose `navigateToArticle` argument is the [MainActions.navigateToArticle] method of
+ *  `actions`, and whose `openDrawer` argument is our `openDrawer` suspend lambda.
+ *  - [MainDestinations.INTERESTS_ROUTE], its `content` is the [InterestsScreen] Composable, whose
+ *  [InterestsRepository] argument `interestsRepository` is the [AppContainer.interestsRepository]
+ *  field of our [appContainer] parameter, and whose `openDrawer` argument is our `openDrawer`
+ *  suspend lambda.
+ *  - [MainDestinations.ARTICLE_ROUTE] (with [ARTICLE_ID_KEY] spliced to the end of that [String]
+ *  with a '/' character separating them to specify the key under which the article ID will be stored
+ *  in the [NavBackStackEntry.arguments] bundle)
  */
 @Composable
 fun JetnewsNavGraph(
@@ -73,8 +101,8 @@ fun JetnewsNavGraph(
     scaffoldState: ScaffoldState = rememberScaffoldState(),
     startDestination: String = MainDestinations.HOME_ROUTE
 ) {
-    val actions = remember(navController) { MainActions(navController) }
-    val coroutineScope = rememberCoroutineScope()
+    val actions: MainActions = remember(navController) { MainActions(navController) }
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
     val openDrawer: () -> Unit = { coroutineScope.launch { scaffoldState.drawerState.open() } }
 
     NavHost(
