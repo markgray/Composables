b37d5ad26fc687a9be0ca9ba9d6638530174ca7c
markgray <mark.t.gray@gmail.com>
2022-12-15 06:04:20 -0500

    Added comments in MigrationCodelab

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/data/PlantDao.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/data/PlantDao.kt
@@ -23,19 +23,52 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 /**
- * The Data Access Object for the Plant class.
+ * The Data Access Object for the Plant class (aka "plants" table in the database).
  */
 @Dao
 interface PlantDao {
+    /**
+     * Returns a [LiveData] wrapped [List] of all of the [Plant] instances in the "plants" table
+     * sorted by its "name" column.
+     *
+     * @return a [LiveData] wrapped [List] of all of the [Plant] instances in the "plants" table
+     * sorted by its "name" column.
+     */
     @Query("SELECT * FROM plants ORDER BY name")
     fun getPlants(): LiveData<List<Plant>>
 
+    /**
+     * Returns a [LiveData] wrapped [List] of all of the [Plant] instances in the "plants" table
+     * whose "growZoneNumber" column is equal to its [growZoneNumber] parameter, sorted by their
+     * "name" column.
+     *
+     * @param growZoneNumber the grow zone number that we want to match the "growZoneNumber" column
+     * of the [Plant]s selected to be returned.
+     * @return a [LiveData] wrapped [List] of all of the [Plant] instances in the "plants" table
+     * whose "growZoneNumber" column is equal to its [growZoneNumber] parameter, sorted by their
+     * "name" column.
+     */
     @Query("SELECT * FROM plants WHERE growZoneNumber = :growZoneNumber ORDER BY name")
     fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): LiveData<List<Plant>>
 
+    /**
+     * Returns the [LiveData] wrapped [Plant] instance in the "plants" table whose "id" column is
+     * equal to our [plantId] parameter.
+     *
+     * @param plantId the [String] that should match the "id" column of the [Plant] returned.
+     * @return the [LiveData] wrapped [Plant] instance in the "plants" table whose "id" column is
+     * equal to our [plantId] parameter.
+     */
     @Query("SELECT * FROM plants WHERE id = :plantId")
     fun getPlant(plantId: String): LiveData<Plant>
 
+    /**
+     * Inserts all of the [Plant] instances in its [List] of [Plant] parameter [plants] into the
+     * "plants" table of the database.
+     *
+     * @param plants the [List] of [Plant] instances that should be inserted into the "plants" table
+     * of the database.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(plants: List<Plant>)
 }
