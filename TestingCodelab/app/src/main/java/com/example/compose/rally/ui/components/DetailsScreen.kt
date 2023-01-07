0fb87bb24d7805d8c46f80dee84b011632d18a10
markgray <mark.t.gray@gmail.com>
2023-01-07 08:32:19 -0500

    Added comments in TestingCodelab

--- TestingCodelab/app/src/main/java/com/example/compose/rally/ui/components/DetailsScreen.kt
+++ TestingCodelab/app/src/main/java/com/example/compose/rally/ui/components/DetailsScreen.kt
@@ -16,6 +16,7 @@
 
 package com.example.compose.rally.ui.components
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -32,9 +33,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.compose.rally.data.Account
+import com.example.compose.rally.data.Bill
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
 
 /**
- * Generic component used by the accounts and bills screens to show a chart and a list of items.
+ * Generic component used by the accounts and bills screens to show a chart and a list of items. Our
+ * root Composable is a [Column] whose `modifier` argument is a [Modifier.verticalScroll] whose `state`
+ * argument is the [ScrollState] constructed and remembered by [rememberScrollState] (this [Modifier]
+ * modifies the element to allow it to scroll vertically when the height of the content is bigger than
+ * max constraints allow). The `content` of the [Column]
+ *
+ * @param items either a [List] of [Account] objects when we are used by [AccountsBody] or a [List]
+ * of [Bill] objects when we are used by [BillsBody]
+ * @param colors a lambda which tales an [Account] or [Bill] instance and returns a [Color]. This
+ * will return the [Account.color] property of an [Account] when we are used by [AccountsBody] or
+ * the [Bill.color] property of a [Bill] when we are used by [BillsBody].
+ * @param amounts a lambda which tales an [Account] or [Bill] instance and returns a [Float]. This
+ * will return the [Account.balance] property of an [Account] when we are used by [AccountsBody] or
+ * the [Bill.amount] property of a [Bill] when we are used by [BillsBody].
+ * @param amountsTotal the sum of all of the [Account.balance] or [Bill.amount] properties in the
+ * [List] of [Account] or [List] of [Bill] that is our [items] parameter.
+ * @param circleLabel this is the label that is displayed beneath our [AnimatedCircle], it is the
+ * [String] "Total" when we are used by [AccountsBody] or the [String] "Due" when we are used by
+ * [BillsBody].
+ * @param rows a lambda emitting a Composable displaying the [Account] or [Bill] that we pass it as
+ * the argument to the lambda that we take from our [List] of [Account] or [Bill] parameter [items].
+ * The Composable will be an [AccountRow] when we are used by [AccountsBody] or a [BillRow] when
+ * we are used by [BillsBody].
  */
 @Composable
 fun <T> StatementBody(
@@ -45,36 +72,36 @@ fun <T> StatementBody(
     circleLabel: String,
     rows: @Composable (T) -> Unit
 ) {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Box(Modifier.padding(16.dp)) {
+    Column(modifier = Modifier.verticalScroll(state = rememberScrollState())) {
+        Box(Modifier.padding(all = 16.dp)) {
             val accountsProportion: List<Float> = items.extractProportions { amounts(it) }
-            val circleColors = items.map { colors(it) }
+            val circleColors: List<Color> = items.map { colors(it) }
             AnimatedCircle(
-                accountsProportion,
-                circleColors,
-                Modifier
-                    .height(300.dp)
-                    .align(Alignment.Center)
+                proportions = accountsProportion,
+                colors = circleColors,
+                modifier = Modifier
+                    .height(height = 300.dp)
+                    .align(alignment = Alignment.Center)
                     .fillMaxWidth()
             )
-            Column(modifier = Modifier.align(Alignment.Center)) {
+            Column(modifier = Modifier.align(alignment = Alignment.Center)) {
                 Text(
                     text = circleLabel,
                     style = MaterialTheme.typography.body1,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
                 )
                 Text(
-                    text = formatAmount(amountsTotal),
+                    text = formatAmount(amount = amountsTotal),
                     style = MaterialTheme.typography.h2,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
                 )
             }
         }
-        Spacer(Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(height = 10.dp))
         Card {
-            Column(modifier = Modifier.padding(12.dp)) {
-                items.forEach { item ->
-                    rows(item)
+            Column(modifier = Modifier.padding(all = 12.dp)) {
+                items.forEach { accountOrBill ->
+                    rows(accountOrBill)
                 }
             }
         }
