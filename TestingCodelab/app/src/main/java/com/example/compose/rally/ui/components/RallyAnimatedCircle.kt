fd3102f28bfd28ec95832b427494a61f36a29660
markgray <mark.t.gray@gmail.com>
2023-01-10 08:04:00 -0500

    Added comments in TestingCodelab

--- TestingCodelab/app/src/main/java/com/example/compose/rally/ui/components/RallyAnimatedCircle.kt
+++ TestingCodelab/app/src/main/java/com/example/compose/rally/ui/components/RallyAnimatedCircle.kt
@@ -19,13 +19,18 @@ package com.example.compose.rally.ui.components
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Transition
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -34,10 +39,23 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
+/**
+ * The number of degrees that are left blank between the arcs drawn.
+ */
 private const val DividerLengthInDegrees = 1.8f
 
 /**
  * A donut chart that animates when loaded.
+ *
+ * @param proportions a [List] of [Float] with each entry representing the fraction of the circle we
+ * are to draw to represent the contribution of the entry to the whole circle.
+ * @param colors the [List] of [Color]'s we are to use to draw each segment of the arc corresponding
+ * to each entry in our [proportions] parameter.
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. [StatementBody] passes us a [Modifier.height] that sets our height to be 300.dp, to
+ * which is chained a [BoxScope] `Modifier.align` with its `alignment` argument [Alignment.Center]
+ * to align us with the center of the [Box] we are in, and a `Modifier.fillMaxWidth` to have us fill
+ * the entire incoming width constraint.
  */
 @Composable
 fun AnimatedCircle(
@@ -45,13 +63,14 @@ fun AnimatedCircle(
     colors: List<Color>,
     modifier: Modifier = Modifier
 ) {
-    val currentState = remember {
+    val currentState: MutableTransitionState<AnimatedCircleProgress> = remember {
         MutableTransitionState(AnimatedCircleProgress.START)
             .apply { targetState = AnimatedCircleProgress.END }
     }
-    val stroke = with(LocalDensity.current) { Stroke(width = 5.dp.toPx()) }
-    val transition = updateTransition(transitionState = currentState, label = "currentState")
-    val angleOffset by transition.animateFloat(
+    val stroke: Stroke = with(LocalDensity.current) { Stroke(width = 5.dp.toPx()) }
+    val transition: Transition<AnimatedCircleProgress> =
+        updateTransition(transitionState = currentState, label = "currentState")
+    val angleOffset: Float by transition.animateFloat(
         transitionSpec = {
             tween(
                 delayMillis = 500,
@@ -60,14 +79,14 @@ fun AnimatedCircle(
             )
         },
         label = "angleOffset"
-    ) { progress ->
+    ) { progress: AnimatedCircleProgress ->
         if (progress == AnimatedCircleProgress.START) {
             0f
         } else {
             360f
         }
     }
-    val shift by transition.animateFloat(
+    val shift: Float by transition.animateFloat(
         transitionSpec = {
             tween(
                 delayMillis = 500,
@@ -76,7 +95,7 @@ fun AnimatedCircle(
             )
         },
         label = "shift"
-    ) { progress ->
+    ) { progress: AnimatedCircleProgress ->
         if (progress == AnimatedCircleProgress.START) {
             0f
         } else {
@@ -84,16 +103,16 @@ fun AnimatedCircle(
         }
     }
 
-    Canvas(modifier) {
-        val innerRadius = (size.minDimension - stroke.width) / 2
-        val halfSize = size / 2.0f
+    Canvas(modifier = modifier) {
+        val innerRadius: Float = (size.minDimension - stroke.width) / 2
+        val halfSize: Size = size / 2.0f
         val topLeft = Offset(
             x = halfSize.width - innerRadius,
             y = halfSize.height - innerRadius
         )
         val size = Size(width = innerRadius * 2, height = innerRadius * 2)
-        var startAngle = shift - 90f
-        proportions.forEachIndexed { index, proportion ->
+        var startAngle: Float = shift - 90f
+        proportions.forEachIndexed { index: Int, proportion: Float ->
             val sweep = proportion * angleOffset
             drawArc(
                 color = colors[index],
@@ -109,4 +128,7 @@ fun AnimatedCircle(
     }
 }
 
+/**
+ * The enum representing the progress of our animations from [START] to [END].
+ */
 private enum class AnimatedCircleProgress { START, END }
