710e53d4f8da707074a27e1081f962c76dd939b0
markgray <mark.t.gray@gmail.com>
2023-01-11 06:39:12 -0500

    Added comments in MigrationCodelab

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantAndGardenPlantingsViewModel.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/viewmodels/PlantAndGardenPlantingsViewModel.kt
@@ -16,21 +16,44 @@
 
 package com.google.samples.apps.sunflower.viewmodels
 
+import androidx.recyclerview.widget.RecyclerView
+import com.google.samples.apps.sunflower.adapters.GardenPlantingAdapter
 import com.google.samples.apps.sunflower.data.GardenPlanting
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.data.PlantAndGardenPlantings
+import com.google.samples.apps.sunflower.GardenFragment
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Locale
 
 /**
- * TODO: Add kdoc
+ * This is used to construct a "pseudo"  `ViewModel` that is used for the "viewmodel" binding
+ * variable of views that are inflated from the file `layout/list_item_garden_planting.xml` (those
+ * views are used by [GardenPlantingAdapter] to display each of the [PlantAndGardenPlantings] objects
+ * of its dataset in the [RecyclerView] in the file `layout/fragment_garden.xml` that [GardenFragment]
+ * uses as its UI). Each of the [PlantAndGardenPlantingsViewModel]'s holds a different
+ * [PlantAndGardenPlantings] instance which provides access to the particular [Plant] and
+ * [GardenPlanting] that the [RecyclerView] item is to display allowing it to use binding expressions
+ * to update its UI from its "viewmodel" variable.
+ *
+ * @param plantings the [PlantAndGardenPlantings] instance which holds the [Plant] and the
+ * [GardenPlanting] that we are to provide access to.
  */
 class PlantAndGardenPlantingsViewModel(plantings: PlantAndGardenPlantings) {
+    /**
+     * The [Plant] object contained in our [PlantAndGardenPlantings] parameter `plantings`.
+     */
     private val plant: Plant = checkNotNull(plantings.plant)
+
+    /**
+     * The [GardenPlanting] object contained in our [PlantAndGardenPlantings] parameter `plantings`.
+     */
     private val gardenPlanting: GardenPlanting = plantings.gardenPlantings[0]
 
     /**
-     * TODO: Add kdoc
+     * This property "returns" the [Calendar.time] of the [GardenPlanting.lastWateringDate] property
+     * of our [PlantAndGardenPlantings] parameter `plantings` formatted into a [String] using our
+     * [SimpleDateFormat] field [dateFormat].
      */
     val waterDateString: String = dateFormat.format(gardenPlanting.lastWateringDate.time)
 
