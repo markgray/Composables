f20d98a194869a05d7eb4238b6824835d6505ec2
markgray <mark.t.gray@gmail.com>
2023-02-18 06:48:15 -0500

    Added comments in NavigationCodelab

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,21 +19,29 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.data.Account
 import com.example.compose.rally.ui.accounts.AccountsScreen
 import com.example.compose.rally.ui.accounts.SingleAccountScreen
 import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.components.RallyTab
 import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -43,7 +51,11 @@ import com.example.compose.rally.ui.theme.RallyTheme
  */
 class RallyActivity : ComponentActivity() {
     /**
-     * TODO: Add kdoc
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we call the [setContent] method to have it compose [RallyApp] into the activity. [RallyApp]
+     * will become the root view of the activity.
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,13 +66,35 @@ class RallyActivity : ComponentActivity() {
 }
 
 /**
- * TODO: Add kdoc
+ * This is the main screen of our app. First we initialize and remember our [NavHostController]
+ * variable `val navController` using the [rememberNavController] method, then we initialize our
+ * [NavBackStackEntry] variable `val currentBackStack` by using the method
+ * [NavHostController.currentBackStackEntryAsState] of `navController` (it gets the current
+ * navigation back stack entry as a [MutableState]. When `navController` changes the back stack
+ * due to a [NavHostController.navigate] or [NavHostController.popBackStack] this will trigger a
+ * recompose and return the top entry on the back stack). We initialize our [NavDestination]
+ * variable `val currentDestination` to the [NavBackStackEntry.destination] of `currentBackStack`,
+ * and initialize our [RallyDestination] variable `val currentScreen` by searching the [List] of
+ * [RallyDestination] field [rallyTabRowScreens] for a [RallyDestination] whose [RallyDestination.route]
+ * is equal to the [NavDestination.route] of `currentDestination`, defaulting to [Overview]'s
+ * [RallyDestination] if none is found.
+ *
+ * Our root Composable is a [Scaffold] whose `topBar` argument is a [RallyTabRow] constructed to
+ * use [rallyTabRowScreens] as its `allScreens` argument, with its `onTabSelected` argument a
+ * lambda which calls the [navigateSingleTopTo] extension method of `navController`, and the
+ * `currentScreen` argument is our `currentScreen` variable (the currently selected [RallyTab]
+ * in the [RallyTabRow], each [RallyTab] compares this value with the [RallyDestination] it is
+ * associated with and modifies its appearance if they are equal). The `content` of the [Scaffold]
+ * is a [RallyNavHost] whose `navController` argument is our [NavHostController] variable
+ * `navController` and whose `modifier` argument is a [Modifier.padding] that uses the
+ * [PaddingValues] passed the `content` lambda to pad the [RallyNavHost] on all sides with that
+ * value.
  */
 @Composable
 fun RallyApp() {
     RallyTheme {
         val navController: NavHostController = rememberNavController()
-        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentBackStack: NavBackStackEntry? by navController.currentBackStackEntryAsState()
         // Fetch your currentDestination:
         val currentDestination: NavDestination? = currentBackStack?.destination
         // Change the variable to this and use Overview as a backup screen if this returns null
@@ -78,17 +112,32 @@ fun RallyApp() {
                     currentScreen = currentScreen,
                 )
             }
-        ) { innerPadding ->
+        ) { innerPadding: PaddingValues ->
             RallyNavHost(
                 navController = navController,
-                modifier = Modifier.padding(innerPadding)
+                modifier = Modifier.padding(paddingValues = innerPadding)
             )
         }
     }
 }
 
 /**
- * TODO: Add kdoc
+ * This is the [NavHost] that is used as the `content` of the [Scaffold] of [RallyApp] and which is
+ * used to navigate between the [OverviewScreen] (for the `route` [Overview.route]), [AccountsScreen]
+ * (for the `route` [Accounts.route]), [BillsScreen] (for the `route` [Bills.route]), or the
+ * [SingleAccountScreen] (for the `route` [SingleAccount.routeWithArgs]). Our root Composable is a
+ * [NavHost] whose `navController` argument is our [NavHostController] parameter [navController],
+ * whose `startDestination` argument is [Overview.route], and whose `modifier` argument is our
+ * [Modifier] parameter [modifier]. The `builder` argument of the [NavHost] is a [NavGraphBuilder]
+ * that uses the [NavGraphBuilder.composable] method to add [NavDestination]'s to the [NavGraph]
+ * for the routes [Overview.route] (composes an [OverviewScreen]), [Accounts.route] (composes an
+ * [AccountsScreen]), [Bills.route] (composes a [BillsScreen]), and [SingleAccount.routeWithArgs]
+ * (composes a [SingleAccountScreen]).
+ *
+ * @param navController the [NavHostController] to be used by our [NavHost].
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. [RallyApp] passes us a [Modifier.padding] that adds the [PaddingValues] passed to the
+ * `content` of the [Scaffold] we are composed in to the all sides of our Composable.
  */
 @Composable
 fun RallyNavHost(
@@ -127,22 +176,33 @@ fun RallyNavHost(
             route = SingleAccount.routeWithArgs,
             arguments = SingleAccount.arguments,
             deepLinks = SingleAccount.deepLinks
-        ) { navBackStackEntry ->
+        ) { navBackStackEntry: NavBackStackEntry ->
             val accountType =
                 navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-            SingleAccountScreen(accountType)
+            SingleAccountScreen(accountType = accountType)
         }
     }
 }
 
 /**
- * TODO: Add kdoc
+ * This extension function calls the [NavHostController.navigate] method of its [NavHostController]
+ * receiver with its `route` argument our [String] parameter [route], and its [NavOptionsBuilder]
+ * `builder` argument a lambda which sets the [NavOptionsBuilder.launchSingleTop] option to `true`
+ * (Whether this navigation action should launch as single-top i.e., there will be at most one copy
+ * of a given destination on the top of the back stack).
+ *
+ * @param route the [String] route for the destination we are to navigate to.
  */
 fun NavHostController.navigateSingleTopTo(route: String): Unit =
     this.navigate(route = route) { launchSingleTop = true }
 
 /**
- * TODO: Add kdoc
+ * This extension function calls the [NavHostController.navigateToSingleAccount] extension method of
+ * its [NavHostController] receiver with its `route` argument our [String] parameter [accountType]
+ * appended to the [String] route [SingleAccount.route] separated by a "/" character.
+ *
+ * @param accountType the [Account.name] of the [Account] we are to have [SingleAccountScreen]
+ * display.
  */
 fun NavHostController.navigateToSingleAccount(accountType: String) {
     this.navigateSingleTopTo(route = "${SingleAccount.route}/$accountType")
--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -22,51 +22,102 @@ import androidx.compose.material.icons.filled.Money
 import androidx.compose.material.icons.filled.MoneyOff
 import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavDestination
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
+import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.accounts.SingleAccountScreen
+import com.example.compose.rally.ui.bills.BillsScreen
+import com.example.compose.rally.ui.components.RallyTab
+import com.example.compose.rally.ui.overview.OverviewScreen
 
 /**
- * Contract for information needed on every Rally navigation destination
+ * Contract for information needed on every Rally navigation destination.
  */
 interface RallyDestination {
+    /**
+     * This is the [ImageVector] that should be displayed by the [RallyTab] associated with the
+     * [RallyDestination].
+     */
     val icon: ImageVector
+
+    /**
+     * This is the route for the destination of the [RallyDestination].
+     */
     val route: String
 }
 
-/**
+/*
  * Rally app navigation destinations
  */
+
+/**
+ * This is the [RallyDestination] for navigating to the [OverviewScreen] Composable.
+ */
 object Overview : RallyDestination {
-    override val icon = Icons.Filled.PieChart
-    override val route = "overview"
+    override val icon: ImageVector = Icons.Filled.PieChart
+    override val route: String = "overview"
 }
 
+/**
+ * This is the [RallyDestination] for navigating to the [AccountsScreen] Composable.
+ */
 object Accounts : RallyDestination {
-    override val icon = Icons.Filled.AttachMoney
-    override val route = "accounts"
+    override val icon: ImageVector = Icons.Filled.AttachMoney
+    override val route: String = "accounts"
 }
 
+/**
+ * This is the [RallyDestination] for navigating to the [BillsScreen] Composable.
+ */
 object Bills : RallyDestination {
-    override val icon = Icons.Filled.MoneyOff
-    override val route = "bills"
+    override val icon: ImageVector = Icons.Filled.MoneyOff
+    override val route: String = "bills"
 }
 
+/**
+ * This is the [RallyDestination] for navigating to the [SingleAccountScreen] Composable.
+ */
 object SingleAccount : RallyDestination {
     // Added for simplicity, this icon will not in fact be used, as SingleAccount isn't
     // part of the RallyTabRow selection
-    override val icon = Icons.Filled.Money
-    override val route = "single_account"
-    const val accountTypeArg = "account_type"
-    val routeWithArgs = "${route}/{${accountTypeArg}}"
-    val arguments = listOf(
+    override val icon: ImageVector = Icons.Filled.Money
+    override val route: String = "single_account"
+
+    /**
+     * This is the key under which the `accountType` is stored in the arguments for navigating to
+     * the [SingleAccountScreen] Composable.
+     */
+    const val accountTypeArg: String = "account_type"
+
+    /**
+     * This is the `route` for the [NavDestination] of [SingleAccount.routeWithArgs], and specifies
+     * that the key of the argument of the `route` will be stored under the [accountTypeArg] key in
+     * the arguments passed in the [NavBackStackEntry.arguments] passed to [SingleAccountScreen].
+     */
+    val routeWithArgs: String = "${route}/{${accountTypeArg}}"
+
+    /**
+     * TODO: Add kdoc
+     */
+    val arguments: List<NamedNavArgument> = listOf(
         navArgument(accountTypeArg) { type = NavType.StringType }
     )
-    val deepLinks = listOf(
+
+    /**
+     * TODO: Add kdoc
+     */
+    val deepLinks: List<NavDeepLink> = listOf(
         navDeepLink { uriPattern = "rally://$route/{$accountTypeArg}" }
     )
 
 }
 
-// Screens to be displayed in the top RallyTabRow
-val rallyTabRowScreens = listOf(Overview, Accounts, Bills)
+/**
+ * Screens to be displayed in the top RallyTabRow
+ */
+val rallyTabRowScreens: List<RallyDestination> = listOf(Overview, Accounts, Bills)
