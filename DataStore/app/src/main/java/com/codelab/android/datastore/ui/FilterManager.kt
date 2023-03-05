f03cd87440b4173a23083ebd6fa97ca5f062a7ba
markgray <mark.t.gray@gmail.com>
2023-03-05 07:55:24 -0500

    Added FilterManager.kt (needs work) in DataStore

---
+++ DataStore/app/src/main/java/com/codelab/android/datastore/ui/FilterManager.kt
@@ -0,0 +1,67 @@
+package com.codelab.android.datastore.ui
+
+import com.codelab.android.datastore.UserPreferences
+
+/**
+ * This is a helper class for
+ */
+class FilterManager(
+    /**
+     * TODO: Add kdoc
+     */
+    private val viewModel: TasksViewModel,
+    /**
+     * TODO: Add kdoc
+     */
+    private var tasksUiModel: TasksUiModel
+) {
+    /**
+     * TODO: Add kdoc
+     */
+    fun priority(): Boolean {
+        return when (tasksUiModel.sortOrder) {
+            UserPreferences.SortOrder.BY_PRIORITY -> true
+            UserPreferences.SortOrder.BY_DEADLINE_AND_PRIORITY -> true
+            else -> false
+        }
+    }
+
+    /**
+     * TODO: Add kdoc
+     */
+    fun deadline(): Boolean {
+        return when (tasksUiModel.sortOrder) {
+            UserPreferences.SortOrder.BY_DEADLINE -> true
+            UserPreferences.SortOrder.BY_DEADLINE_AND_PRIORITY -> true
+            else -> false
+        }
+    }
+
+    /**
+     * TODO: Add kdoc
+     */
+    fun showCompleted(): Boolean {
+        return tasksUiModel.showCompleted
+    }
+
+    /**
+     * TODO: Add kdoc
+     */
+    fun priorityClicked() {
+        viewModel.enableSortByPriority(!priority())
+    }
+
+    /**
+     * TODO: Add kdoc
+     */
+    fun deadlineClicked() {
+        viewModel.enableSortByDeadline(!deadline())
+    }
+
+    /**
+     * TODO: Add kdoc
+     */
+    fun showCompletedClicked() {
+        viewModel.showCompletedTasks(!showCompleted())
+    }
+}
--- DataStore/app/src/main/java/com/codelab/android/datastore/ui/MainScreen.kt
+++ DataStore/app/src/main/java/com/codelab/android/datastore/ui/MainScreen.kt
@@ -20,13 +20,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.codelab.android.datastore.R
-import com.codelab.android.datastore.UserPreferences.SortOrder
 import com.codelab.android.datastore.data.Task
 import com.google.android.material.bottomappbar.BottomAppBar
 import kotlinx.coroutines.flow.Flow
@@ -41,9 +39,12 @@ fun MainScreen(
     tasks: Flow<List<Task>>
 ) {
     val taskList: List<Task> = tasks.collectAsState(initial = listOf()).value
+    val filterManager by remember(tasksUiModel) {
+        mutableStateOf(FilterManager(viewModel, tasksUiModel))
+    }
 
     Scaffold(
-        bottomBar = { OptionsBar(viewModel = viewModel, tasksUiModel = tasksUiModel) }
+        bottomBar = { OptionsBar(fm = filterManager) }
     ) { paddingValues: PaddingValues ->
         LazyColumn(modifier = Modifier.padding(paddingValues)) {
             items(taskList.size) {
@@ -59,43 +60,8 @@ fun MainScreen(
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun OptionsBar(
-    viewModel: TasksViewModel,
-    tasksUiModel: TasksUiModel
+    fm: FilterManager
 ) {
-    /**
-     * TODO: This needs to be moved to the `TasksUiModel` of course.
-     */
-    var showCompleted by remember {
-        mutableStateOf(false)
-    }
-    val priorityChipSelected: Boolean
-    val deadLineChipSelcted: Boolean
-    when (tasksUiModel.sortOrder) {
-        SortOrder.BY_PRIORITY -> {
-            priorityChipSelected = true
-            deadLineChipSelcted = false
-        }
-        SortOrder.UNSPECIFIED -> {
-            priorityChipSelected = false
-            deadLineChipSelcted = false
-        }
-        SortOrder.NONE -> {
-            priorityChipSelected = false
-            deadLineChipSelcted = false
-        }
-        SortOrder.BY_DEADLINE -> {
-            priorityChipSelected = false
-            deadLineChipSelcted = true
-        }
-        SortOrder.BY_DEADLINE_AND_PRIORITY -> {
-            priorityChipSelected = true
-            deadLineChipSelcted = true
-        }
-        SortOrder.UNRECOGNIZED -> {
-            priorityChipSelected = false
-            deadLineChipSelcted = false
-        }
-    }
 
     Column {
         Row(
@@ -111,10 +77,9 @@ fun OptionsBar(
             Text(text = "Show completed tasks")
             Spacer(modifier = Modifier.width(8.dp))
             Switch(
-                checked = showCompleted,
+                checked = fm.deadline(),
                 onCheckedChange = {
-                    viewModel.showCompletedTasks(showCompleted)
-                    showCompleted = !showCompleted
+                    fm.showCompletedClicked()
                 }
             )
         }
@@ -129,15 +94,19 @@ fun OptionsBar(
             )
             Spacer(modifier = Modifier.width(8.dp))
             FilterChip(
-                onClick = {},
-                selected = priorityChipSelected
+                onClick = {
+                    fm.priorityClicked()
+                },
+                selected = fm.priority()
             ) {
                 Text(text = "Priority")
             }
             Spacer(modifier = Modifier.width(8.dp))
             FilterChip(
-                onClick = {},
-                selected = deadLineChipSelcted
+                onClick = {
+                   fm.deadlineClicked()
+                },
+                selected = fm.deadline()
             ) {
                 Text(text = "Deadline")
             }
