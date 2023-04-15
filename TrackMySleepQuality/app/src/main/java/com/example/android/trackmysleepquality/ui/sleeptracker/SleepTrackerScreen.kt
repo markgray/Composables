4e226a4c7aa7ea5c8421e913d22d59b5f9bb486c
markgray <mark.t.gray@gmail.com>
2023-04-15 08:11:57 -0400

    Added comments in TrackMySleepQuality

--- TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/ui/sleeptracker/SleepTrackerScreen.kt
+++ TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/ui/sleeptracker/SleepTrackerScreen.kt
@@ -21,20 +21,45 @@ import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 //import com.example.android.trackmysleepquality.ui.sleepdetail.fakeSleepNight
 import com.example.android.trackmysleepquality.ui.sleepdetail.selectSleepImageId
 import com.example.android.trackmysleepquality.ui.sleepdetail.selectSleepQualityStringId
+import com.example.android.trackmysleepquality.ui.sleepquality.SleepQualityScreen
 import com.example.android.trackmysleepquality.ui.theme.Purple500
 
 /**
- * TODO: Add kdoc
+ * This is the main screen of our app. The root Composable is a [Scaffold] whose `topBar` argument is
+ * a [StartStopBar] whose `onStartClicked` argument (starts the "clock" for the night's sleep) is a
+ * lambda which calls the [SleepTrackerViewModel.onStart] method of our [viewModel] parameter (which
+ * constructs a new [SleepNight] and inserts it in our database) followed by a call to our parameter
+ * [onStartClicked] (our caller calls the [SleepTrackerViewModel.initializeTonight] method of the
+ * [viewModel] in the lambda it passes us), and the `onStopClicked` argument (stops the "clock" for
+ * the night's sleep) of the [StartStopBar] is a lambda which calls the [SleepTrackerViewModel.onStop]
+ * method of our [viewModel] parameter (sets the [SleepNight.endTimeMilli] field of the nights sleep
+ * to the current time and updates the entry in the database) followed by a call to our parameter
+ * [onStopClicked] (our caller sets its [MutableState] wrapped [Boolean] variable `showQuality` which
+ * causes the [SleepQualityScreen] to be composed over us). The `bottomBar` argument of the [Scaffold]
+ * is a [ClearBar] whose `onClearClicked` argument is a lambda which calls the
+ * [SleepTrackerViewModel.onClear] method of our [viewModel] parameter (which in turn calls the
+ * [SleepDatabaseDao.clear] method of our database to delete all values from the table holding our
+ * [SleepNight] data). The `content` of the [Scaffold] is a [LazyVerticalGrid] with the first `item`
+ * in it having a `span` argument is a lambda which uses [GridItemSpan] whose `currentLineSpan`
+ * argument is `maxLineSpan` to have it take up the entire width of the [LazyVerticalGrid] and the
+ * `content` of the `item` is a [Text] displaying the string "Sleep Results" using 30.sp as the
+ * `fontSize` and a `modifier` argument of [Modifier.fillMaxWidth] causing it to fill the entire
+ * incoming width constraint, with a [Modifier.padding] to add 8.dp padding to all sides of the
+ * [Text], followed by a [Modifier.background] to set the background color to [Purple500], and a
+ * [Modifier.wrapContentSize] whose `align` argument of [Alignment.Center] aligns its `text` content
+ * in its center.
  */
 @Composable
 fun SleepTrackerScreen(
