645bcc0833a24e7b28619fad916aeef69d3346dd
markgray <mark.t.gray@gmail.com>
2023-04-18 07:09:51 -0400

    Added comments in TrackMySleepQuality

--- TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -6,20 +6,26 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.ui.sleepdetail.SleepDetailScreen
 import com.example.android.trackmysleepquality.ui.sleepdetail.fakeSleepNight
 import com.example.android.trackmysleepquality.ui.sleepquality.SleepQualityScreen
+import com.example.android.trackmysleepquality.ui.sleeptracker.SleepNightItem
 import com.example.android.trackmysleepquality.ui.sleeptracker.SleepTrackerScreen
 import com.example.android.trackmysleepquality.ui.sleeptracker.SleepTrackerViewModel
 import com.example.android.trackmysleepquality.ui.sleeptracker.SleepTrackerViewModelFactory
@@ -35,6 +41,46 @@ class MainActivity : ComponentActivity() {
      * activity. We initialize our [SleepDatabaseDao] variable `val dataSource` to the
      * [SleepDatabase.sleepDatabaseDao] field of the singleton [SleepDatabase] instance returned by
      * the [SleepDatabase.getInstance] method when passed our [Application] variable `application`.
+     * We then initialize our [SleepTrackerViewModelFactory] variable `val viewModelFactory` to a
+     * new instance constructed to use `dataSource` and `application` when asked to build a
+     * [SleepTrackerViewModel]., and then we initialize our [SleepTrackerViewModel] variable
+     * `val sleepTrackerViewModel` by calling [ViewModelProvider] with `viewModelFactory` as its
+     * `factory` argument and `this` as its [ViewModelStoreOwner]. We initialize our [MutableState]
+     * wrapped [List] of [SleepNight] variable `var sleepNightList` by the [mutableStateOf] method.
+     * Then we add an observer to the [SleepTrackerViewModel.nights] field of `sleepTrackerViewModel`
+     * that sets `sleepNightList` to the field whenever it changes value.
+     *
+     * We then call the [setContent] method to have it Compose our [TrackMySleepQualityTheme] custom
+     * [MaterialTheme] wrapped composable into our activity. In that Composable block we first
+     * initialize and remember our [MutableState] wrapped [Boolean] variables `var showDetail`, and
+     * `var showQuality` to `false` (when `true` they cause the Composables [SleepDetailScreen] and
+     * [SleepQualityScreen] respectively to be composed over our [SleepTrackerScreen] main screen).
+     * Then our root Composable is a [Surface] whose `modifier` argument is a [Modifier.fillMaxSize]
+     * to cause it to occupy its entire incoming constraints, and whose `color` argument set its
+     * background color to the [Colors.background] color of [MaterialTheme.colors] (in our case this
+     * is the default [Color.White]). The `content` of the [Surface] is our [SleepTrackerScreen] with
+     * its `viewModel` argument our [SleepTrackerViewModel] variable `sleepTrackerViewModel`, its
+     * `sleepNightList` argument our [List] of [SleepNight] variable `sleepNightList`. Its
+     * `onSleepNightClicked` argument is a lambda which sets `showDetail` to `true` (which will cause
+     * the [SleepDetailScreen] to be composed atop the [SleepTrackerScreen]) and set `sleepNightClicked`
+     * to the [SleepNight] passed the lambda by the [SleepNightItem] that was clicked in the
+     * [LazyVerticalGrid] of [SleepTrackerScreen], [SleepDetailScreen] will display the details of
+     * this [SleepNight]). Its `onStartClicked` argument is a lambda that calls the
+     * [SleepTrackerViewModel.initializeTonight] method of `sleepTrackerViewModel` to initialize the
+     * [SleepTrackerViewModel.tonight] field of `sleepTrackerViewModel` with the newest [SleepNight]
+     * entry in the database thus starting the "recording" of tonight's sleep duration. Its
+     * `onStopClicked` argument is a lambda which sets our [Boolean] variable `showQuality` to `true`
+     * causing the [SleepQualityScreen] to be composed atop the [SleepTrackerScreen] to allow the
+     * user to assign a quality rating to the [SleepNight] whose duration has just been recorded.
+     *
+     * Following the call to [SleepTrackerScreen] are two if statements. The first checks if
+     * `showDetail` is `true` and if it is will compose [SleepDetailScreen] into the UI with its
+     * `sleepNight` argument our [SleepNight] variable `sleepNightClicked`, and with its
+     * `onCloseClicked` argument a lambda which sets `showDetail` back to `false`. The second checks
+     * if `showQuality` is `true` and if it is will compose [SleepQualityScreen] into the UI with its
+     * `onQualityClicked` argument a lambda which sets `showQuality` back to `false` then calls the
+     * [SleepTrackerViewModel.onSetSleepQuality] with the [Int] "quality" rating passed the lambda
+     * to have `sleepTrackerViewModel` update the quality of the [SleepNight] which was just recorded.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -46,8 +92,7 @@ class MainActivity : ComponentActivity() {
         val dataSource: SleepDatabaseDao = SleepDatabase.getInstance(application).sleepDatabaseDao
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
         val sleepTrackerViewModel =
-            ViewModelProvider(
-                this, viewModelFactory)[SleepTrackerViewModel::class.java]
+            ViewModelProvider(this, viewModelFactory)[SleepTrackerViewModel::class.java]
 
         var sleepNightList by mutableStateOf(listOf<SleepNight>())
 
