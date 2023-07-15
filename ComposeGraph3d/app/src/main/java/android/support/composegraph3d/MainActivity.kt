4d5de62d47cc31cdbba4d4c7fc98d0e29b4cf3ee
markgray <mark.t.gray@gmail.com>
2023-07-15 07:17:22 -0400

    Silenced some warnings in ComposeGraph3d

--- ComposeGraph3d/app/src/main/java/android/support/composegraph3d/MainActivity.kt
+++ ComposeGraph3d/app/src/main/java/android/support/composegraph3d/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 package android.support.composegraph3d
 
 import android.os.Bundle
@@ -8,23 +10,34 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.isActive
 
+/**
+ * TODO: Add kdoc
+ */
 class MainActivity : ComponentActivity() {
+    /**
+     * TODO: Add kdoc
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -41,6 +54,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * TODO: Add kdoc
+ */
 @Composable
 fun Greeting(name: String) {
     val w = 600
@@ -52,14 +68,18 @@ fun Greeting(name: String) {
     ) {
         Text(text = "Hello $name!")
         Graph3D(
-            Modifier.fillMaxWidth().aspectRatio(1f)
-
+            Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
                 .background(Color.White))
         Text(text = "Hello $name!")
     }
 }
 
 
+/**
+ * TODO: Add kdoc
+ */
 @Composable
 fun Graph3D(modifier: Modifier) {
     val graph = remember { Graph() }
@@ -95,13 +115,16 @@ fun Graph3D(modifier: Modifier) {
             )
         }) {
         time.longValue
-        scale(2.0f, pivot = Offset(0f,0f)) {
+        scale(2.0f, pivot = Offset(0f, 0f)) {
             drawImage(graph.bitmap)
         }
     }
 
 }
 
+/**
+ * TODO: Add kdoc
+ */
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
