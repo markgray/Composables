fe7b80e0556f2be476234304f04fa6bec14580c7
markgray <mark.t.gray@gmail.com>
2023-07-16 07:28:28 -0400

    Added comments in ComposeGraph3d

--- ComposeGraph3d/app/src/main/java/android/support/composegraph3d/lib/Graph.kt
+++ ComposeGraph3d/app/src/main/java/android/support/composegraph3d/lib/Graph.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package android.support.composegraph3d.lib
 
 import androidx.compose.ui.geometry.Offset
@@ -6,14 +8,41 @@ import androidx.compose.ui.graphics.ImageBitmapConfig
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.input.pointer.PointerInputChange
 
+/**
+ * TODO: Add kdoc
+ */
 class Graph {
-    var w = 512
-    var h = 512
-    val scale = 2
-    var downX = 0.0f
-    var downY = 0.0f
-    var graphFunctions = FunctionSetup(w, h)
-    var bitmap = ImageBitmap(w, h, ImageBitmapConfig.Argb8888)
+    /**
+     * TODO: Add kdoc
+     */
+    var w: Int = 512
+    /**
+     * TODO: Add kdoc
+     */
+    var h: Int = 512
+    /**
+     * TODO: Add kdoc
+     */
+    val scale: Int = 2
+    /**
+     * TODO: Add kdoc
+     */
+    var downX: Float = 0.0f
+    /**
+     * TODO: Add kdoc
+     */
+    var downY: Float = 0.0f
+    /**
+     * TODO: Add kdoc
+     */
+    var graphFunctions: FunctionSetup = FunctionSetup(w, h)
+    /**
+     * TODO: Add kdoc
+     */
+    var bitmap: ImageBitmap = ImageBitmap(w, h, ImageBitmapConfig.Argb8888)
+    /**
+     * TODO: Add kdoc
+     */
     fun setSize(width: Int, height: Int) {
         if (w == width/scale && h == height/scale) {
             return
@@ -25,23 +54,36 @@ class Graph {
         println("$w x $h")
     }
 
+    /**
+     * TODO: Add kdoc
+     */
     fun getImageForTime(nanoTime: Long): ImageBitmap {
         val pix = graphFunctions.getImageBuff(nanoTime)
         bitmap.asAndroidBitmap().setPixels(pix, 0, w, 0, 0, w, h)
         return bitmap
     }
 
+    /**
+     * TODO: Add kdoc
+     */
     fun dragStart(down: Offset) {
         downX = down.x/scale
         downY = down.y/scale
         graphFunctions.onMouseDown(downX, downY)
     }
 
+    /**
+     * TODO: Add kdoc
+     */
     fun dragStopped() {
         downX = 0.0f
         downY = 0.0f
     }
 
+    /**
+     * TODO: Add kdoc
+     */
+    @Suppress("UNUSED_PARAMETER")
     fun drag(change: PointerInputChange, drag: Offset) {
         downX += drag.x/scale
         downY += drag.y/scale
