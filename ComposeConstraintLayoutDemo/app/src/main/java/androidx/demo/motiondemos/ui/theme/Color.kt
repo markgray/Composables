812a6ab72da618acd393f451f8e88cd016f8885d
markgray <mark.t.gray@gmail.com>
2023-07-25 07:07:53 -0400

    Added comments in ComposeConstraintLayoutDemo

--- ComposeConstraintLayoutDemo/app/src/main/java/androidx/demo/motiondemos/ui/theme/Color.kt
+++ ComposeConstraintLayoutDemo/app/src/main/java/androidx/demo/motiondemos/ui/theme/Color.kt
@@ -1,8 +1,29 @@
 package androidx.demo.motiondemos.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+/**
+ * Used as the [Colors.primary] of the [darkColors] used by the [MotionDemosTheme] custom [MaterialTheme]
+ */
+val Purple200: Color = Color(0xFFBB86FC)
+
+/**
+ * Used as the [Colors.primary] of the [lightColors] used by the [MotionDemosTheme] custom [MaterialTheme]
+ */
+val Purple500: Color = Color(0xFF6200EE)
+
+/**
+ * Used as the [Colors.primaryVariant] of both the [darkColors] and the [lightColors] used by the
+ * [MotionDemosTheme] custom [MaterialTheme]
+ */
+val Purple700: Color = Color(0xFF3700B3)
+
+/**
+ * Used as the [Colors.secondary] of both the [darkColors] and the [lightColors] used by the
+ * [MotionDemosTheme] custom [MaterialTheme]
+ */
+val Teal200: Color = Color(0xFF03DAC5)
