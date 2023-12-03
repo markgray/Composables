e067daceb954d485abd048b5736430560ff0b782
Mark Gray <mark.t.gray@gmail.com>
2023-12-03 04:02:04 -0500

    Updated AGP to 8.2.0 and removed android.defaults.buildfeatures.buildconfig=true in ColorMyViewsCompose

--- ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/ui/theme/Theme.kt
+++ ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/ui/theme/Theme.kt
@@ -6,14 +6,14 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-@Suppress("PrivatePropertyName") // It is a Compose constant of sorts
+// It is a Compose constant of sorts
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
-@Suppress("PrivatePropertyName") // It is a Compose constant of sorts
+// It is a Compose constant of sorts
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
