74b806a28b5f06c7e683db4f534c5b9f75b3006b
markgray <mark.t.gray@gmail.com>
2023-12-16 07:03:03 -0500

    Added comments in ThemingCodelab

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -21,6 +21,7 @@ package com.example.reply.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Article
 import androidx.compose.material.icons.filled.Inbox
@@ -31,6 +32,8 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -38,13 +41,32 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.reply.R
+import com.example.reply.data.Email
+import com.example.reply.ui.components.ReplyEmailListItem
+import com.example.reply.ui.components.ReplyEmailThreadItem
+import kotlinx.coroutines.flow.MutableStateFlow
 
 /**
  * This is the root composable of our app. It appears to exist solely to wrap [ReplyAppContent] in
  * a [Surface] whose `tonalElevation` argument of 5.dp causes its background color to be a darker
  * color in light theme and lighter color in dark theme in order to suggest elevation.
  *
- * @param replyHomeUIState the current [ReplyHomeUIState]
+ * @param replyHomeUIState the current [ReplyHomeUIState] that is collected as [State] from the
+ * [ReplyHomeViewModel.uiState] property by [MainActivity].
+ * @param closeDetailScreen a lambda to call in order to close the [ReplyEmailDetail] email detail
+ * screen. [MainActivity] passes a lambda which calls the [ReplyHomeViewModel.closeDetailScreen]
+ * method of our apps viewmodel. This method updates the value of its [MutableStateFlow] of
+ * [ReplyHomeUIState] by setting its [ReplyHomeUIState.isDetailOnlyOpen] property to `false` and
+ * its [ReplyHomeUIState.selectedEmail] property to the [List.first] of the [List] of [Email] in
+ * the [ReplyHomeUIState.emails] property of the old [ReplyHomeUIState].
+ * @param navigateToDetail a lambda for [ReplyEmailListItem] to call with the [Email.id] of its
+ * [Email] in order to have [ReplyEmailListContent] have [ReplyEmailDetail] display that [Email]'s
+ * [Email.threads] property in [ReplyEmailThreadItem] widgets in a [LazyColumn] ([Email.threads] is
+ * a [List] of [Email]). [MainActivity] passes a lambda which calls the [ReplyHomeViewModel.setSelectedEmail]
+ * method of [ReplyHomeViewModel] with the [Long] passed to the lambda. This method updates the value
+ * of its [MutableStateFlow] of  [ReplyHomeUIState] by setting its [ReplyHomeUIState.isDetailOnlyOpen]
+ * property to `true` and its [ReplyHomeUIState.selectedEmail] to the [Email] that the [List.find]
+ * method of the [List] of [Email] property [ReplyHomeUIState.emails] finds with that [Email.id].
  */
 @Composable
 fun ReplyApp(
@@ -72,11 +94,10 @@ fun ReplyAppContent(
     navigateToDetail: (Long) -> Unit,
 ) {
 
-    val selectedDestination = remember { mutableStateOf(ReplyRoute.INBOX) }
+    val selectedDestination: MutableState<String> = remember { mutableStateOf(ReplyRoute.INBOX) }
 
     Column(
-        modifier = modifier
-            .fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
 
         if (selectedDestination.value == ReplyRoute.INBOX) {
