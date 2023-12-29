385942c4286d36806f965ed564d7cc3b0d8d68df
markgray <mark.t.gray@gmail.com>
2023-12-29 07:41:16 -0500

    Initial commit ColorInM3

---
+++ ColorInM3/app/src/main/java/com/example/android/colorinm3/ui/theme/Color.kt
@@ -0,0 +1,314 @@
+package com.example.android.colorinm3.ui.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Used as the [ColorScheme.primary] of our [lightColorScheme].
+ */
+val md_theme_light_primary: Color = Color(0xFF825500)
+
+/**
+ * Used as the [ColorScheme.onPrimary] of our [lightColorScheme].
+ */
+val md_theme_light_onPrimary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.primaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_primaryContainer: Color = Color(0xFFFFDDB3)
+
+/**
+ * Used as the [ColorScheme.onPrimaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_onPrimaryContainer: Color = Color(0xFF291800)
+
+/**
+ * Used as the [ColorScheme.secondary] of our [lightColorScheme].
+ */
+val md_theme_light_secondary: Color = Color(0xFF6F5B40)
+
+/**
+ * Used as the [ColorScheme.onSecondary] of our [lightColorScheme].
+ */
+val md_theme_light_onSecondary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.secondaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_secondaryContainer: Color = Color(0xFFFBDEBC)
+
+/**
+ * Used as the [ColorScheme.onSecondaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_onSecondaryContainer: Color = Color(0xFF271904)
+
+/**
+ * Used as the [ColorScheme.tertiary] of our [lightColorScheme].
+ */
+val md_theme_light_tertiary: Color = Color(0xFF51643F)
+
+/**
+ * Used as the [ColorScheme.onTertiary] of our [lightColorScheme].
+ */
+val md_theme_light_onTertiary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.tertiaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_tertiaryContainer: Color = Color(0xFFD4EABB)
+
+/**
+ * Used as the [ColorScheme.onTertiaryContainer] of our [lightColorScheme].
+ */
+val md_theme_light_onTertiaryContainer: Color = Color(0xFF102004)
+
+/**
+ * Used as the [ColorScheme.error] of our [lightColorScheme].
+ */
+val md_theme_light_error: Color = Color(0xFFBA1A1A)
+
+/**
+ * Used as the [ColorScheme.errorContainer] of our [lightColorScheme].
+ */
+val md_theme_light_errorContainer: Color = Color(0xFFFFDAD6)
+
+/**
+ * Used as the [ColorScheme.onError] of our [lightColorScheme].
+ */
+val md_theme_light_onError: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.onErrorContainer] of our [lightColorScheme].
+ */
+val md_theme_light_onErrorContainer: Color = Color(0xFF410002)
+
+/**
+ * Used as the [ColorScheme.background] of our [lightColorScheme].
+ */
+val md_theme_light_background: Color = Color(0xFFFFFBFF)
+
+/**
+ * Used as the [ColorScheme.onBackground] of our [lightColorScheme].
+ */
+val md_theme_light_onBackground: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.surface] of our [lightColorScheme].
+ */
+val md_theme_light_surface: Color = Color(0xFFFFFBFF)
+
+/**
+ * Used as the [ColorScheme.onSurface] of our [lightColorScheme].
+ */
+val md_theme_light_onSurface: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.surfaceVariant] of our [lightColorScheme].
+ */
+val md_theme_light_surfaceVariant: Color = Color(0xFFF0E0CF)
+
+/**
+ * Used as the [ColorScheme.onSurfaceVariant] of our [lightColorScheme].
+ */
+val md_theme_light_onSurfaceVariant: Color = Color(0xFF4F4539)
+
+/**
+ * Used as the [ColorScheme.outline] of our [lightColorScheme].
+ */
+val md_theme_light_outline: Color = Color(0xFF817567)
+
+/**
+ * Used as the [ColorScheme.inverseOnSurface] of our [lightColorScheme].
+ */
+val md_theme_light_inverseOnSurface: Color = Color(0xFFF9EFE7)
+
+/**
+ * Used as the [ColorScheme.inverseSurface] of our [lightColorScheme].
+ */
+val md_theme_light_inverseSurface: Color = Color(0xFF34302A)
+
+/**
+ * Used as the [ColorScheme.inversePrimary] of our [lightColorScheme].
+ */
+val md_theme_light_inversePrimary: Color = Color(0xFFFFB951)
+
+/**
+ * Could be used as the `ColorScheme.shadow` of our `lightColorScheme` it only it existed.
+ *
+ * val md_theme_light_shadow: Color = Color(0xFF000000)
+ */
+
+/**
+ * Used as the [ColorScheme.surfaceTint] of our [lightColorScheme].
+ */
+val md_theme_light_surfaceTint: Color = Color(0xFF825500)
+
+/**
+ * Used as the [ColorScheme.outlineVariant] of our [lightColorScheme].
+ */
+val md_theme_light_outlineVariant: Color = Color(0xFFD3C4B4)
+
+/**
+ * Used as the [ColorScheme.scrim] of our [lightColorScheme].
+ */
+val md_theme_light_scrim: Color = Color(0xFF000000)
+
+/**
+ * Used as the [ColorScheme.primary] of our [darkColorScheme].
+ */
+val md_theme_dark_primary: Color = Color(0xFFFFB951)
+
+/**
+ * Used as the [ColorScheme.onPrimary] of our [darkColorScheme].
+ */
+val md_theme_dark_onPrimary: Color = Color(0xFF452B00)
+
+/**
+ * Used as the [ColorScheme.primaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_primaryContainer: Color = Color(0xFF633F00)
+
+/**
+ * markgray  Used as the [ColorScheme.onPrimaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_onPrimaryContainer: Color = Color(0xFFFFDDB3)
+
+/**
+ * Used as the [ColorScheme.secondary] of our [darkColorScheme].
+ */
+val md_theme_dark_secondary: Color = Color(0xFFDDC2A1)
+
+/**
+ * Used as the [ColorScheme.onSecondary] of our [darkColorScheme].
+ */
+val md_theme_dark_onSecondary: Color = Color(0xFF3E2D16)
+
+/**
+ * Used as the [ColorScheme.secondaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_secondaryContainer: Color = Color(0xFF56442A)
+
+/**
+ * Used as the [ColorScheme.onSecondaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_onSecondaryContainer: Color = Color(0xFFFBDEBC)
+
+/**
+ * Used as the [ColorScheme.tertiary] of our [darkColorScheme].
+ */
+val md_theme_dark_tertiary: Color = Color(0xFFB8CEA1)
+
+/**
+ * Used as the [ColorScheme.onTertiary] of our [darkColorScheme].
+ */
+val md_theme_dark_onTertiary: Color = Color(0xFF243515)
+
+/**
+ * Used as the [ColorScheme.tertiaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_tertiaryContainer: Color = Color(0xFF3A4C2A)
+
+/**
+ * Used as the [ColorScheme.onTertiaryContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_onTertiaryContainer: Color = Color(0xFFD4EABB)
+
+/**
+ * Used as the [ColorScheme.error] of our [darkColorScheme].
+ */
+val md_theme_dark_error: Color = Color(0xFFFFB4AB)
+
+/**
+ * Used as the [ColorScheme.errorContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_errorContainer: Color = Color(0xFF93000A)
+
+/**
+ * Used as the [ColorScheme.onError] of our [darkColorScheme].
+ */
+val md_theme_dark_onError: Color = Color(0xFF690005)
+
+/**
+ * Used as the [ColorScheme.onErrorContainer] of our [darkColorScheme].
+ */
+val md_theme_dark_onErrorContainer: Color = Color(0xFFFFDAD6)
+
+/**
+ * Used as the [ColorScheme.background] of our [darkColorScheme].
+ */
+val md_theme_dark_background: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.onBackground] of our [darkColorScheme].
+ */
+val md_theme_dark_onBackground: Color = Color(0xFFEAE1D9)
+
+/**
+ * Used as the [ColorScheme.surface] of our [darkColorScheme].
+ */
+val md_theme_dark_surface: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.onSurface] of our [darkColorScheme].
+ */
+val md_theme_dark_onSurface: Color = Color(0xFFEAE1D9)
+
+/**
+ * Used as the [ColorScheme.surfaceVariant] of our [darkColorScheme].
+ */
+val md_theme_dark_surfaceVariant: Color = Color(0xFF4F4539)
+
+/**
+ * Used as the [ColorScheme.onSurfaceVariant] of our [darkColorScheme].
+ */
+val md_theme_dark_onSurfaceVariant: Color = Color(0xFFD3C4B4)
+
+/**
+ * Used as the [ColorScheme.outline] of our [darkColorScheme].
+ */
+val md_theme_dark_outline: Color = Color(0xFF9C8F80)
+
+/**
+ * Used as the [ColorScheme.inverseOnSurface] of our [darkColorScheme].
+ */
+val md_theme_dark_inverseOnSurface: Color = Color(0xFF1F1B16)
+
+/**
+ * Used as the [ColorScheme.inverseSurface] of our [darkColorScheme].
+ */
+val md_theme_dark_inverseSurface: Color = Color(0xFFEAE1D9)
+
+/**
+ * Used as the [ColorScheme.inversePrimary] of our [darkColorScheme].
+ */
+val md_theme_dark_inversePrimary: Color = Color(0xFF825500)
+
+/**
+ * Could be used as the `ColorScheme.shadow` of our `darkColorScheme` it only it existed.
+ *
+ * val md_theme_dark_shadow: Color = Color(0xFF000000)
+ */
+
+/**
+ * Used as the [ColorScheme.surfaceTint] of our [darkColorScheme].
+ */
+val md_theme_dark_surfaceTint: Color = Color(0xFFFFB951)
+
+/**
+ * Used as the [ColorScheme.outlineVariant] of our [darkColorScheme].
+ */
+val md_theme_dark_outlineVariant: Color = Color(0xFF4F4539)
+
+/**
+ * Used as the [ColorScheme.scrim] of our [darkColorScheme].
+ */
+val md_theme_dark_scrim: Color = Color(0xFF000000)
+
+
+/**
+ * Used as the seed of our current `ColorScheme` (maybe?).
+ * val seed: Color = Color(0xFF825500)
+ */
---
+++ ColorInM3/app/src/main/java/com/example/android/colorinm3/ui/theme/Theme.kt
@@ -0,0 +1,163 @@
+package com.example.android.colorinm3.ui.theme
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.Window
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+/**
+ * The [ColorScheme] that is used when the device is considered to be in Light Mode, either
+ * [ColorInM3Theme] is called with its `darkTheme` parmeter `false` or if no value for
+ * `darkTheme` is passed then [isSystemInDarkTheme] returns `false`.
+ */
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    outline = md_theme_light_outline,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
+    surfaceTint = md_theme_light_surfaceTint,
+    outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
+)
+
+/**
+ * The [ColorScheme] that is used when the device is considered to be in Dark Mode, either
+ * [ColorInM3Theme] is called with its `darkTheme` parmeter `true` or if no value for
+ * `darkTheme` is passed then [isSystemInDarkTheme] returns `true`.
+ */
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
+)
+
+
+/**
+ * This is the custom [MaterialTheme] used to wrap our UI and supply Material 3 default values to
+ * the widgets it wraps for [ColorScheme], [Typography], and [Shapes]. We start by initializing our
+ * [Context] variable `val context` to the [Context] returned by `LocalContext.current`. Then we
+ * initialize [ColorScheme] variable `val colors` to [dynamicDarkColorScheme] the device is newer
+ * than [Build.VERSION_CODES.S] and [darkTheme] is `true` or to [dynamicLightColorScheme] if it
+ * is `false`. When the device is older than [Build.VERSION_CODES.S] our custom [darkColorScheme]
+ * field [DarkColorScheme] is used if [darkTheme] is `true` or else our custom [lightColorScheme]
+ * field [LightColorScheme] is used. We initialized our [View] variable `val view` to the current Compose
+ * [View] then if the [View.isInEditMode] method of `view` returns `true` (a [View] is usually in
+ * edit mode when displayed within a developer tool) we lauch a [SideEffect] that schedules its
+ * `effect` lambda to run when the current composition completes successfully and applies changes.
+ * The lambda initializes its [Window] variable `val window` to the current [Window] for the activity,
+ * then it calls the [Window.setStatusBarColor] method (kotlin `statusBarColor` property) to set the
+ * the color of the status bar to the [ColorScheme.primary] color of `colors` (after converting it
+ * to to an ARGB color [Int]), and use the [WindowCompat.getInsetsController] to fetch the single
+ * [WindowInsetsControllerCompat] of the window this `view` is attached to call its
+ * [WindowInsetsControllerCompat.setAppearanceLightStatusBars] method (kotlin
+ * `isAppearanceLightStatusBars` property) to the value of [darkTheme].
+ *
+ * Finally we call the [MaterialTheme] Composable with its [MaterialTheme.colorScheme] argument our
+ * [ColorScheme] `colors` variable, its [MaterialTheme.typography] argument our custom [Typography]
+ * class [Typography], and its `content` lambda our Composable [content] parameter.
+ *
+ * @param darkTheme if `true` a dark [ColorScheme] is used, either [dynamicDarkColorScheme] if
+ * the device is newer than [Build.VERSION_CODES.S], or else our custom [darkColorScheme] field
+ * [DarkColorScheme] is used if it is older, and if [darkTheme] is `false` a light [ColorScheme] is
+ * used, either [dynamicLightColorScheme] if the device is newer than [Build.VERSION_CODES.S], or
+ * else our custom [lightColorScheme] field [LightColorScheme] is used if it is older. Our callers do not
+ * pass a value so the result of calling [isSystemInDarkTheme] is used instead.
+ * @param content the Composable hierarchy that we are wrapping in order to supply default values.
+ *
+ */
+@Composable
+fun ColorInM3Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
