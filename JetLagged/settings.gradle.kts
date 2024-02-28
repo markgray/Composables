53ecd191fa2842a27f1f17cf2ccae48b4080c0e3
markgray <mark.t.gray@gmail.com>
2024-02-28 06:34:02 -0500

    Updated kotlin to 1.9.22, compose-compiler to 1.5.10, and gradle dependencies in JetLagged

--- JetLagged/settings.gradle.kts
+++ JetLagged/settings.gradle.kts
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * TODO: Figure out what this is.
+ */
 val snapshotVersion : String? = System.getenv("COMPOSE_SNAPSHOT_ID")
 
 pluginManagement {
