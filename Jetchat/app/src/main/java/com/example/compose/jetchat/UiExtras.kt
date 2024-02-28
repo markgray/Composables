c3e92e6d512669547c562df9c5fe9c9ddc6c4c2e
markgray <mark.t.gray@gmail.com>
2024-02-28 05:52:33 -0500

    Added comments in Jetchat

--- Jetchat/app/src/main/java/com/example/compose/jetchat/UiExtras.kt
+++ Jetchat/app/src/main/java/com/example/compose/jetchat/UiExtras.kt
@@ -20,10 +20,24 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 
 /**
+ * This custom [AlertDialog] is "popped up" in several places when the user tries to use a feature
+ * for which there is no implementation written yet. It consists of an [AlertDialog] whose
+ * `onDismissRequest` argument is our lambda parameter [onDismiss], whose `text` argument is a
+ * lambda that composes a [Text] whose `text` argument is "Functionality not available \uD83D\uDE48",
+ * using as its `style` the [TextStyle] of the [Typography.bodyMedium] of our custom
+ * [MaterialTheme.typography]. The `confirmButton` argument of the [AlertDialog] is a lambda that
+ * composes a [TextButton] whose `onClick` argument is our [onDismiss] lambda parameter, and with
+ * its `content` lambda composing a [Text] whose `text` argument is the [String] "CLOSE".
  *
+ * @param onDismiss a lambda that our [AlertDialog] can call to dismiss this
+ * [FunctionalityNotAvailablePopup] either because the user tries to dismiss the Dialog by clicking
+ * outside or pressing the back button (the `onDismissRequest` argument of the [AlertDialog]) or
+ * by clicking the `confirmButton` argument of the [AlertDialog].
  */
 @Composable
 fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
