cf09dae188a5c1d7d722b0420200e88325fc1a3d
markgray <mark.t.gray@gmail.com>
2024-03-24 06:12:59 -0400

    Added comments in JetLagged

--- JetLagged/app/src/main/java/com/example/jetlagged/TimeGraph.kt
+++ JetLagged/app/src/main/java/com/example/jetlagged/TimeGraph.kt
@@ -18,6 +18,8 @@ package com.example.jetlagged
 
 import androidx.compose.foundation.layout.LayoutScopeMarker
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
@@ -25,7 +27,9 @@ import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.layout.ParentDataModifier
+import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
@@ -37,12 +41,53 @@ import kotlin.math.roundToInt
 /**
  * This is called by [JetLaggedTimeGraph] to layout the final graph of the [SleepGraphData] using
  * the Composables it creates using its lambda parameters to provide Composables for the various
- * components of the graph.
+ * components of the graph. We start by using our [dayLabel] parameter to create a [List] containing
+ * [dayItemsCount] Composables to initialize our variable `val dayLabels` with each Composable in the
+ * [List] displaying the [LocalDateTime.getDayOfWeek] (kotlin `dayOfWeek` property) for the
+ * [SleepDayData.startDate] of the corresponding [SleepDayData] in the [SleepGraphData.sleepDayData]
+ * list. We then initialize our [List] of Composables variable `val bars` by using our [bar] lambda
+ * parameter to create a [SleepBar] for each of the [SleepDayData] in the [SleepGraphData.sleepDayData]
+ * list.
+ *
+ * Our root Composable is a [Layout] whose `contents` argument is a [listOf] our [hoursHeader]
+ * Composable parameter, our `dayLabels` [List] of Composables variable, and our `bars` [List] of
+ * Composables variable. The `modifier` argument of the [Layout] chains a [Modifier.padding] to our
+ * [modifier] parameter which sets the `bottom` padding to 32.dp.
+ *
+ * In the [MeasureScope] block of the [Layout] we use destructuring to split the [List] of [List]
+ * of [Measurable]'s passed the block into the [List] of [Measurable] variables `hoursHeaderMeasurables`
+ * (created from `hoursHeader` Composable), `dayLabelMeasurables` (created from `dayLabels` [List] of
+ * Composables), and `barMeasureables` (created from `bars` [List] of Composables), and accept the
+ * [Constraints] passed the block as variable `constraints`.
+ *
+ * We perform a [require] sanity check that "hoursHeader should only emit one composable", then
+ * initialize our [Placeable] variable `val hoursHeaderPlaceable` to the value returned by the
+ * [Measurable.measure] method of the [List.first] of `hoursHeaderMeasurables` for [Constraints]
+ * variable `constraints`.
+ *
+ * We initialize our [List] of [Placeable] variable `val dayLabelPlaceables` using the [List.map]
+ * method of `dayLabelMeasurables` to loop through all of the [Measurable] in [List] of [Measurable]
+ * `dayLabelMeasurables` calling their [Measurable.measure] method to produce a [Placeable] for that
+ * [Measurable] given [Constraints] variable `constraints`.
+ *
+ * We initialize our [Int] variable `var totalHeight` to the [Placeable.height] of `hoursHeaderPlaceable`.
+ *
+ * We initialize our [List] of [Placeable] variable
  *
  * @param hoursHeader a lambda returning a header bar Composable created by [HoursHeader] which has
  * a [Text] for each [Int] hour included by the [SleepDayData] that is in [SleepGraphData].
  * @param dayItemsCount the number of [SleepDayData] that are to have sleep bars drawn for them.
- *
+ * @param dayLabel a lambda which produces a [DayLabel] Composable displaying the value returned by
+ * [LocalDateTime.getDayOfWeek] (kotlin `dayOfWeek` property) for the [SleepDayData.startDate] of
+ * the [SleepDayData] at the [Int] `index` passed the lambda in the [List] of [SleepDayData] field
+ * [SleepGraphData.sleepDayData].
+ * @param bar a lambda which produces a [SleepBar] Composable displaying the [SleepDayData] at the
+ * [Int] `index` passed the lambda in the [List] of [SleepDayData] field [SleepGraphData.sleepDayData].
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. Our caller [JetLaggedTimeGraph] passes us a [Modifier.horizontalScroll] (allows us to
+ * scroll horizontally when width of our content is bigger than max constraints allow) with a
+ * [Modifier.wrapContentSize] chained to that (allows us to measure at our desired size without
+ * regard for the incoming measurement minimum width or minimum height constraints) .
  */
 @Composable
 fun TimeGraph(
@@ -58,26 +103,30 @@ fun TimeGraph(
         contents = listOf(hoursHeader, dayLabels, bars),
         modifier = modifier.padding(bottom = 32.dp)
     ) {
-        (hoursHeaderMeasurables: List<Measurable>, dayLabelMeasurables: List<Measurable>, barMeasureables: List<Measurable>),
+        (
+            hoursHeaderMeasurables: List<Measurable>,
+            dayLabelMeasurables: List<Measurable>,
+            barMeasureables: List<Measurable>
+        ),
         constraints: Constraints,
         ->
         require(hoursHeaderMeasurables.size == 1) {
             "hoursHeader should only emit one composable"
         }
-        val hoursHeaderPlaceable = hoursHeaderMeasurables.first().measure(constraints)
+        val hoursHeaderPlaceable: Placeable = hoursHeaderMeasurables.first().measure(constraints)
 
-        val dayLabelPlaceables = dayLabelMeasurables.map { measurable ->
-            val placeable = measurable.measure(constraints)
+        val dayLabelPlaceables: List<Placeable> = dayLabelMeasurables.map { measurable ->
+            val placeable: Placeable = measurable.measure(constraints)
             placeable
         }
 
-        var totalHeight = hoursHeaderPlaceable.height
+        var totalHeight: Int = hoursHeaderPlaceable.height
 
-        val barPlaceables = barMeasureables.map { measurable ->
+        val barPlaceables: List<Placeable> = barMeasureables.map { measurable: Measurable ->
             val barParentData = measurable.parentData as TimeGraphParentData
-            val barWidth = (barParentData.duration * hoursHeaderPlaceable.width).roundToInt()
+            val barWidth: Int = (barParentData.duration * hoursHeaderPlaceable.width).roundToInt()
 
-            val barPlaceable = measurable.measure(
+            val barPlaceable: Placeable = measurable.measure(
                 constraints.copy(
                     minWidth = barWidth,
                     maxWidth = barWidth
@@ -87,21 +136,21 @@ fun TimeGraph(
             barPlaceable
         }
 
-        val totalWidth = dayLabelPlaceables.first().width + hoursHeaderPlaceable.width
+        val totalWidth: Int = dayLabelPlaceables.first().width + hoursHeaderPlaceable.width
 
-        layout(totalWidth, totalHeight) {
-            val xPosition = dayLabelPlaceables.first().width
-            var yPosition = hoursHeaderPlaceable.height
+        layout(width = totalWidth, height = totalHeight) {
+            val xPosition: Int = dayLabelPlaceables.first().width
+            var yPosition: Int = hoursHeaderPlaceable.height
 
-            hoursHeaderPlaceable.place(xPosition, 0)
+            hoursHeaderPlaceable.place(x = xPosition, y = 0)
 
-            barPlaceables.forEachIndexed { index, barPlaceable ->
+            barPlaceables.forEachIndexed { index: Int, barPlaceable: Placeable ->
                 val barParentData = barPlaceable.parentData as TimeGraphParentData
-                val barOffset = (barParentData.offset * hoursHeaderPlaceable.width).roundToInt()
+                val barOffset: Int = (barParentData.offset * hoursHeaderPlaceable.width).roundToInt()
 
-                barPlaceable.place(xPosition + barOffset, yPosition)
+                barPlaceable.place(x = xPosition + barOffset, y = yPosition)
                 // the label depend on the size of the bar content - so should use the same y
-                val dayLabelPlaceable = dayLabelPlaceables[index]
+                val dayLabelPlaceable: Placeable = dayLabelPlaceables[index]
                 dayLabelPlaceable.place(x = 0, y = yPosition)
 
                 yPosition += barPlaceable.height
