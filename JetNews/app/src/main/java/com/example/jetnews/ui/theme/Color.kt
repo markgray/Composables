267da9e2cd96ff2335536843a4835403a9c84191
markgray <mark.t.gray@gmail.com>
2024-04-03 06:14:28 -0400

    Added comments in JetNews

--- JetNews/app/src/main/java/com/example/jetnews/ui/theme/Color.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/theme/Color.kt
@@ -16,112 +16,162 @@
 
 package com.example.jetnews.ui.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
+
+/**
+ * Used as the [ColorScheme.primary] of our [lightColorScheme]. The primary color is the color
+ * displayed most frequently across your app’s screens and components.
+ */
+val md_theme_light_primary: Color = Color(0xFFBF0031)
+
+/**
+ * Used as the [ColorScheme.onPrimary] of our [lightColorScheme]. Color used for text and icons
+ * displayed on top of the primary color.
+ */
+val md_theme_light_onPrimary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.primaryContainer] of our [lightColorScheme]. The preferred tonal color
+ * of containers.
+ */
+val md_theme_light_primaryContainer: Color = Color(0xFFFFDAD9)
+
+/**
+ * Used as the [ColorScheme.onPrimaryContainer] of our [lightColorScheme]. The color (and state
+ * variants) that should be used for content on top of primaryContainer.
+ */
+val md_theme_light_onPrimaryContainer: Color = Color(0xFF40000A)
+
+/**
+ * Used as the [ColorScheme.secondary] of our [lightColorScheme]. The secondary color provides more
+ * ways to accent and distinguish your product. Secondary colors are best for:
+ *  - Floating action buttons
+ *  - Selection controls, like checkboxes and radio buttons
+ *  - Highlighting selected text
+ *  - Links and headlines
+ */
+val md_theme_light_secondary: Color = Color(0xFF775656)
+
+/**
+ * Used as the [ColorScheme.onSecondary] of our [lightColorScheme]. Color used for text and icons
+ * displayed on top of the secondary color.
+ */
+val md_theme_light_onSecondary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.secondaryContainer] of our [lightColorScheme]. A tonal color to be used
+ * in containers.
+ */
+val md_theme_light_secondaryContainer: Color = Color(0xFFFFDAD9)
+
+/**
+ * Used as the [ColorScheme.onSecondaryContainer] of our [lightColorScheme]. The color (and state
+ * variants) that should be used for content on top of secondaryContainer.
+ */
+val md_theme_light_onSecondaryContainer: Color = Color(0xFF2C1516)
+
+/**
+ * Used as the [ColorScheme.tertiary] of our [lightColorScheme]. The tertiary color that can be used
+ * to balance primary and secondary colors, or bring heightened attention to an element such as an
+ * input field.
+ */
+val md_theme_light_tertiary: Color = Color(0xFF755A2F)
+
+/**
+ * Used as the [ColorScheme.onTertiary] of our [lightColorScheme]. Color used for text and icons
+ * displayed on top of the tertiary color.
+ */
+val md_theme_light_onTertiary: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.tertiaryContainer] of our [lightColorScheme]. A tonal color to be used
+ * in containers.
+ */
+val md_theme_light_tertiaryContainer: Color = Color(0xFFFFDDAF)
+
+/**
+ * Used as the [ColorScheme.onTertiaryContainer] of our [lightColorScheme]. The color (and state
+ * variants) that should be used for content on top of tertiaryContainer.
+ */
+val md_theme_light_onTertiaryContainer: Color = Color(0xFF281800)
+
+/**
+ * Used as the [ColorScheme.error] of our [lightColorScheme]. The error color is used to indicate
+ * errors in components, such as invalid text in a text field.
+ */
+val md_theme_light_error: Color = Color(0xFFBA1A1A)
+
+/**
+ * Used as the [ColorScheme.errorContainer] of our [lightColorScheme]. The preferred tonal color of
+ * error containers.
+ */
+val md_theme_light_errorContainer: Color = Color(0xFFFFDAD6)
+
+/**
+ * Used as the [ColorScheme.onError] of our [lightColorScheme]. Color used for text and icons
+ * displayed on top of the error color.
+ */
+val md_theme_light_onError: Color = Color(0xFFFFFFFF)
+
+/**
+ * Used as the [ColorScheme.onErrorContainer] of our [lightColorScheme]. The color (and state
+ * variants) that should be used for content on top of errorContainer.
+ */
+val md_theme_light_onErrorContainer: Color = Color(0xFF410002)
+
+/**
+ * Used as the [ColorScheme.background] of our [lightColorScheme]. The background color that appears
+ * behind scrollable content.
+ */
+val md_theme_light_background: Color = Color(0xFFFFFBFF)
 
 /**
  * TODO: Add kdoc
  */
-val md_theme_light_primary: Color = Color(0xFFBF0031)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onPrimary: Color = Color(0xFFFFFFFF)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_primaryContainer: Color = Color(0xFFFFDAD9)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onPrimaryContainer: Color = Color(0xFF40000A)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_secondary: Color = Color(0xFF775656)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onSecondary: Color = Color(0xFFFFFFFF)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_secondaryContainer: Color = Color(0xFFFFDAD9)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onSecondaryContainer: Color = Color(0xFF2C1516)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_tertiary: Color = Color(0xFF755A2F)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onTertiary: Color = Color(0xFFFFFFFF)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_tertiaryContainer: Color = Color(0xFFFFDDAF)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onTertiaryContainer: Color = Color(0xFF281800)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_error: Color = Color(0xFFBA1A1A)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_errorContainer: Color = Color(0xFFFFDAD6)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onError: Color = Color(0xFFFFFFFF)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_onErrorContainer: Color = Color(0xFF410002)
-/**
- * TODO: Add kdoc
- */
-val md_theme_light_background: Color = Color(0xFFFFFBFF)
-/**
- * TODO: Add kdoc
- */
 val md_theme_light_onBackground: Color = Color(0xFF201A1A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_surface: Color = Color(0xFFFFFBFF)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_onSurface: Color = Color(0xFF201A1A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_surfaceVariant: Color = Color(0xFFF4DDDD)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_onSurfaceVariant: Color = Color(0xFF524343)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_outline: Color = Color(0xFF857373)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_inverseOnSurface: Color = Color(0xFFFBEEED)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_inverseSurface: Color = Color(0xFF362F2F)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_light_inversePrimary: Color = Color(0xFFFFB3B4)
+
 /**
  * TODO: Add kdoc
  */
@@ -131,106 +181,132 @@ val md_theme_light_surfaceTint: Color = Color(0xFFBF0031)
  * TODO: Add kdoc
  */
 val md_theme_dark_primary: Color = Color(0xFFFFB3B4)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onPrimary: Color = Color(0xFF680016)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_primaryContainer: Color = Color(0xFF920023)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onPrimaryContainer: Color = Color(0xFFFFDAD9)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_secondary: Color = Color(0xFFE6BDBC)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onSecondary: Color = Color(0xFF44292A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_secondaryContainer: Color = Color(0xFF5D3F3F)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onSecondaryContainer: Color = Color(0xFFFFDAD9)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_tertiary: Color = Color(0xFFE5C18D)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onTertiary: Color = Color(0xFF422C05)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_tertiaryContainer: Color = Color(0xFF5B421A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onTertiaryContainer: Color = Color(0xFFFFDDAF)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_error: Color = Color(0xFFFFB4AB)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_errorContainer: Color = Color(0xFF93000A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onError: Color = Color(0xFF690005)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onErrorContainer: Color = Color(0xFFFFDAD6)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_background: Color = Color(0xFF201A1A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onBackground: Color = Color(0xFFECE0DF)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_surface: Color = Color(0xFF201A1A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onSurface: Color = Color(0xFFECE0DF)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_surfaceVariant: Color = Color(0xFF524343)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_onSurfaceVariant: Color = Color(0xFFD7C1C1)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_outline: Color = Color(0xFFA08C8C)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_inverseOnSurface: Color = Color(0xFF201A1A)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_inverseSurface: Color = Color(0xFFECE0DF)
+
 /**
  * TODO: Add kdoc
  */
 val md_theme_dark_inversePrimary: Color = Color(0xFFBF0031)
+
 /**
  * TODO: Add kdoc
  */
