e65af873ae0d0e8671de960db719fff807ed237b
markgray <mark.t.gray@gmail.com>
2024-04-11 07:43:26 -0400

    Added comments in JetNews

--- JetNews/app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
@@ -16,6 +16,7 @@
 
 package com.example.jetnews.ui
 
+import android.app.Application
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -23,15 +24,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.jetnews.JetnewsApplication
 import com.example.jetnews.data.AppContainer
+import com.example.jetnews.data.interests.InterestsRepository
+import com.example.jetnews.data.posts.PostsRepository
 import com.example.jetnews.ui.components.AppNavRail
 import com.example.jetnews.ui.home.HomeRoute
 import com.example.jetnews.ui.interests.InterestsRoute
@@ -50,7 +58,58 @@ import kotlinx.coroutines.launch
  * [JetnewsNavigationActions.navigateToHome] method is called or to [JetnewsDestinations.INTERESTS_ROUTE]
  * which displays [InterestsRoute] if its [JetnewsNavigationActions.navigateToInterests] method is
  * called). Next we initialize and remember our [CoroutineScope] variable `val coroutineScope` to the
- * new instance returned by [rememberCoroutineScope].
+ * new instance returned by [rememberCoroutineScope]. Then we initialize our [State] wrapped
+ * [NavBackStackEntry] variable `val navBackStackEntry` to the value returned by the
+ * [NavController.currentBackStackEntryAsState] method of `navController` (a [NavBackStackEntry] is
+ * a representation of an entry in the back stack of a [NavController]. The `Lifecycle`, `ViewModelStore`,
+ * and `SavedStateRegistry` provided via this object are valid for the lifetime of this destination
+ * on the back stack: when this destination is popped off the back stack, the lifecycle will be
+ * destroyed, state will no longer be saved, and ViewModels will be cleared). Next we initialize our
+ * [String] variable `val currentRoute` to the [NavDestination.route] of `navBackStackEntry` defaulting
+ * to [JetnewsDestinations.HOME_ROUTE] ("home") if it is `null`. We then initialize our [Boolean]
+ * variable to `true` if our [WindowWidthSizeClass] parameter [widthSizeClass] is equal to
+ * [WindowWidthSizeClass.Expanded] (represents the majority of tablets in landscape and large unfolded
+ * inner displays in landscape). Next we initialize and remember our [DrawerState] variable
+ * `val sizeAwareDrawerState` to the value returned by our [rememberSizeAwareDrawerState] method when
+ * its `isExpandedScreen` argument is our [Boolean] variable `isExpandedScreen` (this returns a
+ * a [DrawerState] whose `initialValue` is [DrawerValue.Closed] locking it closed if `isExpandedScreen`
+ * if `isExpandedScreen` is `true` or a remembered [State] wrapped [DrawerState] whose `initialValue`
+ * is [DrawerValue.Closed] if it is `false`.
+ *
+ * The arguments of our root [ModalNavigationDrawer] Composable are:
+ *  - `drawerContent` is a lambda composing an [AppDrawer] Composable whose `currentRoute` argument
+ *  is our [String] variable `currentRoute`, whose `navigateToHome` argument is the
+ *  [JetnewsNavigationActions.navigateToHome] method of our `navigationActions` variable, whose
+ *  `navigateToInterests` argument is the [JetnewsNavigationActions.navigateToInterests] method of
+ *  our `navigationActions` variable, whose `closeDrawer` argument is openDrawer
+ *  [CoroutineScope.launch] method of our `coroutineScope` variable to launch a new coroutine which
+ *  calls the [DrawerState.close] method of our `sizeAwareDrawerState` to close the
+ *  [ModalNavigationDrawer].
+ *  - `drawerState` our [DrawerState] variable `sizeAwareDrawerState`.
+ *  - `gesturesEnabled` is `true` if our [Boolean] variable `isExpandedScreen` is `false` (Only
+ *  enable opening the drawer via gestures if the screen is not expanded).
+ *
+ * The `content` of the [ModalNavigationDrawer] is a [Row] which if `isExpandedScreen` is `true`
+ * hold an [AppNavRail] whose `currentRoute` argument is our [String] variable `currentRoute`, whose
+ * `navigateToHome` argument is the [JetnewsNavigationActions.navigateToHome] method of our
+ * `navigationActions` variable, and whose `navigateToInterests` argument is the
+ * [JetnewsNavigationActions.navigateToInterests] method of our navigationActions variable (providing
+ * essentially the same functionality provided as the [AppDrawer] used as the `drawerContent` for
+ * the [ModalNavigationDrawer] on phones but in an always visible form on tablets).
+ *
+ * Next in the [Row] whether `isExpandedScreen` is `true` or not is a [JetnewsNavGraph] whose
+ * `appContainer` argument is our [AppContainer] parameter [appContainer], whose `isExpandedScreen`
+ * argument is our `isExpandedScreen` [Boolean] variable, whose `navController` argument is our
+ * [NavHostController] variable `navController`, and whose `openDrawer` argument is a lambda which
+ * calls the [CoroutineScope.launch] method of our `coroutineScope` variable to launch a new coroutine
+ * which calls the [DrawerState.open] method of our `sizeAwareDrawerState` to open the
+ * [ModalNavigationDrawer].
+ *
+ * @param appContainer this is the [AppContainer] which is created in our [JetnewsApplication] custom
+ * [Application] (it contains a reference to our singleton [PostsRepository] in [AppContainer.postsRepository]
+ * and a reference to our singleton [InterestsRepository] in [AppContainer.interestsRepository]).
+ * @param widthSizeClass the [WindowWidthSizeClass] of the device we are running on. It is calculated
+ * by a call to the [calculateWindowSizeClass] in the `onCreate` override of [MainActivity].
  */
 @Composable
 fun JetnewsApp(
@@ -110,7 +169,7 @@ fun JetnewsApp(
  */
 @Composable
 private fun rememberSizeAwareDrawerState(isExpandedScreen: Boolean): DrawerState {
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
     return if (!isExpandedScreen) {
         // If we want to allow showing the drawer, we use a real, remembered drawer
@@ -120,6 +179,6 @@ private fun rememberSizeAwareDrawerState(isExpandedScreen: Boolean): DrawerState
         // If we don't want to allow the drawer to be shown, we provide a drawer state
         // that is locked closed. This is intentionally not remembered, because we
         // don't want to keep track of any changes and always keep it closed
-        DrawerState(DrawerValue.Closed)
+        DrawerState(initialValue = DrawerValue.Closed)
     }
 }
