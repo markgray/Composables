64c75a6401ee8cb159d3734229630a94c27c52fd
markgray <mark.t.gray@gmail.com>
2024-05-15 06:43:31 -0400

    Updated kotlin to 1.9.24, compose compiler to 1.5.14, and gradle dependency in ColorInM3

--- ColorInM3/app/build.gradle.kts
+++ ColorInM3/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.13"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
@@ -50,9 +50,8 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
