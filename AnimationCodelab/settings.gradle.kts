ad7ff5a33d1e37d2829ec7229cd8e0ec40a1a1bd
markgray <mark.t.gray@gmail.com>
2024-06-17 05:49:23 -0400

    Converted build files to kts in AnimationCodelab

---
+++ AnimationCodelab/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AnimationCodelab"
+include(":finished", ":start")
