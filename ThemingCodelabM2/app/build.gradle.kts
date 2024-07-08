5e25d21031cf662e20d7e082da05e1131d803884
markgray <mark.t.gray@gmail.com>
2024-07-08 06:45:05 -0400

    Updated kotlin and compose plugin to 2.0.0 in ThemingCodelabM2

--- ThemingCodelabM2/app/build.gradle.kts
+++ ThemingCodelabM2/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -22,8 +23,8 @@ android {
     buildFeatures {
          compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
+    composeCompiler {
+        enableStrongSkippingMode = true
     }
     namespace = "com.codelab.theming"
 }
