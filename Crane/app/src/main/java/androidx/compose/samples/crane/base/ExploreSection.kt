9f81b2b406a68261a018d24e8442ab9117d8396a
markgray <mark.t.gray@gmail.com>
2024-07-14 07:22:32 -0400

    Added comments in Crane

--- Crane/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
@@ -45,6 +46,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.Typography
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.samples.crane.data.City
 import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.samples.crane.details.launchDetailsActivity
 import androidx.compose.samples.crane.home.OnExploreItemClicked
@@ -62,6 +64,7 @@ import androidx.compose.samples.crane.home.CraneScreen
 import androidx.compose.samples.crane.home.HomeTabBar
 import androidx.compose.samples.crane.home.MainActivity
 import androidx.compose.samples.crane.ui.CraneTheme
+import androidx.compose.ui.text.TextStyle
 import com.google.maps.android.compose.GoogleMap
 
 /**
@@ -98,7 +101,23 @@ import com.google.maps.android.compose.GoogleMap
  *
  * In the `content` lambda of the [LazyVerticalStaggeredGrid] we use the [itemsIndexed] extension
  * function to loop through all of the [ExploreModel] in our [List] of [ExploreModel] parameter
- * [exploreList] using a when switch to branch
+ * [exploreList] capturing the [ExploreModel] in our `exploreItem` variable. Then using a when switch
+ * to branch based on the value of our [WindowWidthSizeClass] parameter:
+ *  - [WindowWidthSizeClass.Medium] or [WindowWidthSizeClass.Expanded]: We compose an [ExploreItemColumn]
+ *  whose `modifier` argument is a [Modifier.fillMaxWidth] to have it take up all or its incoming width
+ *  constraint, its `item` argument is the current [ExploreModel] in our `exploreItem` variable, and its
+ *  `onItemClicked` argument is our [onItemClicked] lambda parameter.
+ *  - [WindowWidthSizeClass.Compact] (or any other): We compose an [ExploreItemRow] whose `modifier`
+ *  argument is a [Modifier.fillMaxWidth] to have it take up all or its incoming width constraint,
+ *  its `item` argument is the current [ExploreModel] in our `exploreItem` variable, and its
+ *  `onItemClicked` argument is our [onItemClicked] lambda parameter.
+ *
+ * At the bottom of the `content` of the [LazyVerticalStaggeredGrid] is a single
+ * [LazyStaggeredGridScope.item] whose `span` argument is [StaggeredGridItemSpan.FullLine] (causing
+ * it to take up an entire line by itself) that holds a [Spacer] whose `modifier` argument is a
+ * [Modifier.windowInsetsBottomHeight] whose `insets` argument is [WindowInsets.Companion.navigationBars]
+ * (which sets the height to that of insets at the bottom of the screen where system UI places
+ * navigation bars).
  *
  * @param widthSize the [WindowWidthSizeClass] of the device we are running on, one of
  * [WindowWidthSizeClass.Medium] (the majority of tablets in portrait and large unfolded inner
@@ -163,7 +182,7 @@ fun ExploreSection(
                     item(span = StaggeredGridItemSpan.FullLine) {
                         Spacer(
                             modifier = Modifier
-                                .windowInsetsBottomHeight(WindowInsets.navigationBars)
+                                .windowInsetsBottomHeight(insets = WindowInsets.navigationBars)
                         )
                     }
                 }
@@ -173,7 +192,42 @@ fun ExploreSection(
 }
 
 /**
- * Composable with large image card and text underneath.
+ * Composable with large image card and text underneath. Used in [ExploreSection] in the `content`
+ * of the [LazyVerticalStaggeredGrid] when the [WindowWidthSizeClass] parameter of [ExploreSection]
+ * is either [WindowWidthSizeClass.Medium] (the majority of tablets in portrait and large unfolded
+ * inner displays in portrait), or [WindowWidthSizeClass.Expanded] (the majority of tablets in
+ * landscape and large unfolded inner displays in landscape). Our root Composable is a [Column]
+ * whose `modifier` argument chains a [Modifier.clickable] to our [Modifier] parameter [modifier]
+ * whose lambda argument calls our [onItemClicked] lambda parameter with our [ExploreModel] parameter
+ * [item], and to that is chained a [Modifier.padding] that adds 12.dp padding to the `top` of the
+ * [Column], and 12.dp padding to the `bottom` of the [Column]. The `content` of the [Column] is:
+ *  - an [ExploreImageContainer] whose `modifier` arugment is a [Modifier.fillMaxWidth] holding a
+ *  [ExploreImage] whose `item` argument is our [ExploreModel] parameter [item] (this downloads
+ *  and displays the picture whose URL is the [ExploreModel.imageUrl] property of [item]).
+ *  - a [Spacer] whose `modifier` argument is a [Modifier.height] setting its height to 8.dp.
+ *  - a [Column] whose `modifier` argument is a [Modifier.fillMaxWidth] (occupies its entire incoming
+ *  width constraint) to which is chained a [Modifier.wrapContentHeight] (allows its content to
+ *  measure at its desired height without regard to the height constraints). The `content` of the
+ *  inner [Column] consists of:
+ *  - a [Text] whose `modifier` argument is a [Modifier.fillMaxWidth] (to have it take up its entire
+ *  incoming width constraint), whose `text` is the [City.nameToDisplay] of the [ExploreModel.city]
+ *  of our [ExploreModel] parameter [item], and whose [TextStyle] `style` argument is the
+ *  [Typography.subtitle1] of our custom [MaterialTheme.typography].
+ *  - a [Spacer] whose `modifier` argument is a [Modifier.height] that sets its height to 4.dp
+ *  - a [Text] whose `modifier` argument is a [Modifier.fillMaxWidth] (to have it take up its entire
+ *  incoming width constraint), whose `text` is the [ExploreModel.description] property of our
+ *  [ExploreModel] parameter [item], and whose [TextStyle] `style` argument is a copy of the
+ *  [Typography.caption] of our custom [MaterialTheme.typography] with its color overridden by
+ *  [crane_caption] ([Color.DarkGray]).
+ *
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. Our caller [ExploreSection] passes us a [Modifier.fillMaxWidth] to have us take up our
+ * entire incoming width constraint.
+ * @param item the [ExploreModel] whose information we are to display.
+ * @param onItemClicked a lambda we should call with our [item] parameter when we are clicked. Traced
+ * up the Composable hierarchy we end up with a lambda created in the `onCreate` override of
+ * [MainActivity] that calls the [launchDetailsActivity] method to have it launch the [DetailsActivity]
+ * to display a [GoogleMap] for the [ExploreModel] that it is called with.
  */
 @Composable
 private fun ExploreItemColumn(
@@ -187,9 +241,9 @@ private fun ExploreItemColumn(
             .padding(top = 12.dp, bottom = 12.dp)
     ) {
         ExploreImageContainer(modifier = Modifier.fillMaxWidth()) {
-            ExploreImage(item)
+            ExploreImage(item = item)
         }
-        Spacer(Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(height = 8.dp))
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -200,7 +254,7 @@ private fun ExploreItemColumn(
                 text = item.city.nameToDisplay,
                 style = MaterialTheme.typography.subtitle1
             )
-            Spacer(Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(height = 4.dp))
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 text = item.description,
