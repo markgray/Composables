14f0490303f4aaa8f9de968658ccee97e0a9e824
markgray <mark.t.gray@gmail.com>
2024-07-28 06:17:25 -0400

    Added comments in Crane

--- Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Day.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Day.kt
@@ -23,12 +23,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.samples.crane.R
 import androidx.compose.samples.crane.calendar.model.CalendarUiState
+import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,6 +43,7 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.semantics.text
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
@@ -49,7 +51,18 @@ import java.time.LocalDate
 import java.time.YearMonth
 
 /**
- * Displays the first letter of the each day of the week at the top of each month.
+ * Used by the [DaysOfWeek] Composable to display the first letter of the each day of the week at
+ * the top of each month. Our root composable is a [DayContainer] whose `content` argument is a
+ * [Text] whose `modifier` argument is a [Modifier.fillMaxSize] that causes it to occupy its entire
+ * incoming size constraints. with a [Modifier.wrapContentHeight] chained to that whose `align`
+ * argument is a [Alignment.CenterVertically] causes [Text] to center its `text` vertically, whose
+ * `textAlign` argument is [TextAlign.Center] to align the text in the center of the container, whose
+ * `text` argument is our [String] parameter [day], and whose [TextStyle] `style` argument is the
+ * a copy of the [Typography.caption] of our [CraneTheme] custom [MaterialTheme.typography] with its
+ * `color` overridden by a copy of [Color.White] with an `alpha` of 0.6f
+ *
+ * @param day the first letter of the day of the week we are to represent (ie "M", "T", "W", "T",
+ * "F", "S", or "S").
  */
 @Composable
 internal fun DayOfWeekHeading(day: String) {
@@ -60,12 +73,31 @@ internal fun DayOfWeekHeading(day: String) {
                 .wrapContentHeight(align = Alignment.CenterVertically),
             textAlign = TextAlign.Center,
             text = day,
-            style = MaterialTheme.typography.caption.copy(Color.White.copy(alpha = 0.6f))
+            style = MaterialTheme.typography.caption.copy(color = Color.White.copy(alpha = 0.6f))
         )
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
+/**
+ * This is used by [DayOfWeekHeading] to display the first letter of each day of the week, and by
+ * [Day] to display a numbered day in the Calendar.
+ *
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. [DayOfWeekHeading] passes us none so the default empty [Modifier] is used, but [Day]
+ * passes us a [Modifier.semantics] whose `text` is an [AnnotatedString] describing the day of the
+ * month that we represent.
+ * @param selected if `true` we are in the selected dates range of the [CalendarUiState].
+ * @param onClick a lambda that we should call when we are clicked.
+ * @param onClickEnabled this is always `true` so it always adds some [Modifier.semantics] to the
+ * `modifier` argument of our [Box].
+ * @param backgroundColor this is always [Color.Transparent], and is used to specify the background
+ * [Color] of our [Box].
+ * @param onClickLabel used as the `label` argument of a [SemanticsPropertyReceiver.onClick] that
+ * is used in a [Modifier.semantics] of our [Box]. [DayOfWeekHeading] passes us none, but [Day]
+ * passes us the [String] with resource ID [R.string.click_label_select] ("select").
+ * @param content the Composable lambda that we should use as the `content` Composable lambda of our
+ * [Box]. Both of our callers pass us a [Text] configured for their purposes.
+ */
 @Composable
 private fun DayContainer(
     modifier: Modifier = Modifier,
@@ -97,7 +129,7 @@ private fun DayContainer(
                     modifier.clearAndSetSemantics { }
                 }
             )
-            .background(backgroundColor)
+            .background(color = backgroundColor)
     ) {
         content()
     }
@@ -137,5 +169,13 @@ internal fun Day(
     }
 }
 
-val DayStatusKey = SemanticsPropertyKey<Boolean>("DayStatusKey")
-var SemanticsPropertyReceiver.dayStatusProperty by DayStatusKey
+/**
+ * TODO: Add kdoc
+ */
+val DayStatusKey: SemanticsPropertyKey<Boolean> =
+    SemanticsPropertyKey("DayStatusKey")
+
+/**
+ * TODO: Add kdoc
+ */
+var SemanticsPropertyReceiver.dayStatusProperty: Boolean by DayStatusKey
