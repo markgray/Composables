b1290abeb92f998f30ce976be64e52f101add6b0
markgray <mark.t.gray@gmail.com>
2024-07-30 06:43:23 -0400

    Updated gradle dependencies in AnimationCodelab

--- AnimationCodelab/start/build.gradle.kts
+++ AnimationCodelab/start/build.gradle.kts
@@ -50,6 +50,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.material:material:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
