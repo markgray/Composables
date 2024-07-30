9684d486f322ca2245a1a3dcf6b97f864cab7529
markgray <mark.t.gray@gmail.com>
2024-07-30 06:52:48 -0400

    Updated gradle dependency in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.ui:ui:1.6.8")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.material:material:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
