553b244e2c4a8f846c734fc5e257caaa57bf2453
markgray <mark.t.gray@gmail.com>
2024-07-30 06:24:20 -0400

    Updated gradle dependencies in AdaptiveUICodelab

--- AdaptiveUICodelab/app/build.gradle.kts
+++ AdaptiveUICodelab/app/build.gradle.kts
@@ -74,9 +74,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
     implementation("androidx.window:window:1.3.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
