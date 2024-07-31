89e6cf9bc3d4e27b52ca3c5e9000efd0f21ef6b7
Mark Gray <mark.t.gray@gmail.com>
2024-07-31 04:12:37 -0400

    Updated gradle dependencies in ComposeDemos

--- ComposeDemos/app/build.gradle.kts
+++ ComposeDemos/app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
     implementation("androidx.compose.material:material:1.6.8")
