82189d4e9ed9dd6105a484f4497df60b6aef6b5e
markgray <mark.t.gray@gmail.com>
2024-07-31 06:26:16 -0400

    Added comments in Crane

--- Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Month.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Month.kt
@@ -17,15 +17,42 @@
 package androidx.compose.samples.crane.calendar
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.samples.crane.calendar.model.Month
+import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.TextStyle
+import java.time.YearMonth
 
 /**
- * This [Composable]
+ * This Composable is used to display the month name and year at the top of each month in the
+ * calendar. Our root Composable is a [Row] whose `modifier` argument chains a
+ * [Modifier.clearAndSetSemantics] to our [Modifier] parameter [modifier] to clear the semantics of
+ * all the descendant nodes. The [RowScope] Composable `content` lambda holds:
+ *  - a [Text] whose `modifier` argument is a [RowScope.weight] of 1.0f which will cause it to take
+ *  up all the incoming size constraint after its sibling has been measured and placed, whose `text`
+ *  argument is our [String] parameter [month], and whose [TextStyle] `style` argument is the
+ *  [Typography.h6] of our [CraneTheme] custom [MaterialTheme.typography].
+ *  - a [Text] whose `modifier` argument is a [RowScope.align] with an `alignment` argument of
+ *  [Alignment.CenterVertically] to center it vertically, whose `text` argument is our [String]
+ *  parameter [year], and whose [TextStyle] `style` argument is the [Typography.caption] of our
+ *  [CraneTheme] custom [MaterialTheme.typography].
+ *
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. Our caller (the private extension function `LazyListScope.itemsCalendarMonth` in the
+ * file `Calendar.kt`) calls us with a [Modifier.padding] that adds 32.dp to our `start`, 32.dp to
+ * our `end`, and 32.dp to our `top`.
+ * @param month the [String] name of the month. Our caller calls us with the `name` of the
+ * [YearMonth.getMonth] of the [Month.yearMonth] of the [Month] it is working on.
+ * @param year the [String] version of the year. Our caller calls us with the [String] value of the
+ * [YearMonth.getYear] of the [Month.yearMonth] of the [Month] it is working on.
  */
 @Composable
 internal fun MonthHeader(modifier: Modifier = Modifier, month: String, year: String) {
@@ -36,7 +63,7 @@ internal fun MonthHeader(modifier: Modifier = Modifier, month: String, year: Str
             style = MaterialTheme.typography.h6
         )
         Text(
-            modifier = Modifier.align(Alignment.CenterVertically),
+            modifier = Modifier.align(alignment = Alignment.CenterVertically),
             text = year,
             style = MaterialTheme.typography.caption
         )
--- Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Week.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Week.kt
@@ -19,11 +19,14 @@ package androidx.compose.samples.crane.calendar
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.samples.crane.calendar.model.CalendarUiState
 import androidx.compose.samples.crane.calendar.model.Week
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.unit.dp
@@ -31,10 +34,20 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.temporal.TemporalAdjusters
 
+/**
+ * The Composable displays the first letter of each day of the week in a header at the top of each
+ * month.
+ *
+ * @param modifier a [Modifier] instance that our caller can use to modify our appearance and/or
+ * behavior. Our caller (the private extension function `LazyListScope.itemsCalendarMonth` in the
+ * file `Calendar.kt`) calls us with a [Modifier.fillMaxWidth] with a [Modifier.wrapContentWidth]
+ * whose `align` is [Alignment.CenterHorizontally] chained to that (expanding width and centering
+ * horizontally).
+ */
 @Composable
 internal fun DaysOfWeek(modifier: Modifier = Modifier) {
     Row(modifier = modifier.clearAndSetSemantics { }) {
-        for (day in DayOfWeek.values()) {
+        for (day: DayOfWeek in DayOfWeek.values()) {
             DayOfWeekHeading(day = day.name.take(1))
         }
     }
@@ -47,13 +60,13 @@ internal fun Week(
     onDayClicked: (LocalDate) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val beginningWeek = week.yearMonth.atDay(1).plusWeeks(week.number.toLong())
-    var currentDay = beginningWeek.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+    val beginningWeek: LocalDate = week.yearMonth.atDay(1).plusWeeks(week.number.toLong())
+    var currentDay: LocalDate = beginningWeek.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
 
     Box {
         Row(modifier = modifier) {
             Spacer(
-                Modifier
+                modifier = Modifier
                     .weight(1f)
                     .heightIn(max = CELL_SIZE)
             )
