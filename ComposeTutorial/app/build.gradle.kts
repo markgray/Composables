c8a0fc3bfc3b53793926cfc9639e1f3c155b4430
Mark Gray <mark.t.gray@gmail.com>
2024-07-31 04:36:52 -0400

    Updated gradle dependencies in ComposeTutorial

--- ComposeTutorial/app/build.gradle.kts
+++ ComposeTutorial/app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.material:material:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
