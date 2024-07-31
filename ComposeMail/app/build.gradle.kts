823b158ddfa463abba0e211bda145d3947e589e8
Mark Gray <mark.t.gray@gmail.com>
2024-07-31 04:28:46 -0400

    Updated gradle dependencies in ComposeMail

--- ComposeMail/app/build.gradle.kts
+++ ComposeMail/app/build.gradle.kts
@@ -85,8 +85,8 @@ dependencies {
     implementation("androidx.paging:paging-compose:1.0.0-alpha17")
     //noinspection GradleDependency TODO: Newer version breaks compilation!
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha07")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.window:window:1.3.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
 
