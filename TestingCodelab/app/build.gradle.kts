2028ed0c7b23d66e52e4ba8d99819e15cae95b22
Mark Gray <mark.t.gray@gmail.com>
2024-08-01 04:05:49 -0400

    Updated gradle dependencies in TestingCodelab

--- TestingCodelab/app/build.gradle.kts
+++ TestingCodelab/app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
 
     // Compose
     implementation("androidx.compose.runtime:runtime:1.6.8")
@@ -97,7 +97,7 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 
     // Testing dependencies
