ea968ac93f93b83861d84637edea60deb69e3eef
markgray <mark.t.gray@gmail.com>
2024-08-11 03:56:42 -0400

    Updated AGP to 8.5.2 in ComposeConstraintLayoutDemo

--- ComposeConstraintLayoutDemo/app/build.gradle.kts
+++ ComposeConstraintLayoutDemo/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
+    //noinspection GradleDependency TODO: alpha14 requires SDK 35, keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
