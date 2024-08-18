f981dbd28b4199783b5d4db34006077b64e399c8
markgray <mark.t.gray@gmail.com>
2024-08-18 04:46:04 -0400

    Updated gradle dependency in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/app/build.gradle.kts
+++ AdvancedStateAndSideEffectsCodelab/app/build.gradle.kts
@@ -110,7 +110,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
     androidTestImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:core:1.6.1")
-    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.2.1")
