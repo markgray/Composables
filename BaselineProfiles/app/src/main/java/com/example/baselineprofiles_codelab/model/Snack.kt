24cdb0195c19a333c9625f2fca0ba01eae444f1d
markgray <mark.t.gray@gmail.com>
2024-08-21 05:24:35 -0400

    Replaced obsolete unslash.com urls with a cupcake url in BaselineProfiles

--- BaselineProfiles/app/src/main/java/com/example/baselineprofiles_codelab/model/Snack.kt
+++ BaselineProfiles/app/src/main/java/com/example/baselineprofiles_codelab/model/Snack.kt
@@ -37,190 +37,190 @@ val snacks = listOf(
         id = 1L,
         name = "Cupcake",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/pGM4sjt_BdQ",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 2L,
         name = "Donut",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/Yc5sL-ejk6U",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 3L,
         name = "Eclair",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/-LojFX9NfPY",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 4L,
         name = "Froyo",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/3U2V5WqK1PQ",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 5L,
         name = "Gingerbread",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/Y4YR9OjdIMk",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 499
     ),
     Snack(
         id = 6L,
         name = "Honeycomb",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/bELvIg_KZGU",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 7L,
         name = "Ice Cream Sandwich",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/YgYJsFDd4AU",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 1299
     ),
     Snack(
         id = 8L,
         name = "Jellybean",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/0u_vbeOkMpk",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 9L,
         name = "KitKat",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/yb16pT5F_jE",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 549
     ),
     Snack(
         id = 10L,
         name = "Lollipop",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/AHF_ZktTL6Q",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 11L,
         name = "Marshmallow",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/rqFm0IgMVYY",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 12L,
         name = "Nougat",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/qRE_OpbVPR8",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 13L,
         name = "Oreo",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/33fWPnyN6tU",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 14L,
         name = "Pie",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/aX_ljOOyWJY",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 15L,
         name = "Chips",
-        imageUrl = "https://source.unsplash.com/UsSdMZ78Q3E",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 16L,
         name = "Pretzels",
-        imageUrl = "https://source.unsplash.com/7meCnGCJ5Ms",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 17L,
         name = "Smoothies",
-        imageUrl = "https://source.unsplash.com/m741tj4Cz7M",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 18L,
         name = "Popcorn",
-        imageUrl = "https://source.unsplash.com/iuwMdNq0-s4",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 19L,
         name = "Almonds",
-        imageUrl = "https://source.unsplash.com/qgWWQU1SzqM",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 20L,
         name = "Cheese",
-        imageUrl = "https://source.unsplash.com/9MzCd76xLGk",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 21L,
         name = "Apples",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/1d9xXWMtQzQ",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 22L,
         name = "Apple sauce",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/wZxpOw84QTU",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 23L,
         name = "Apple chips",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/okzeRxm_GPo",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 24L,
         name = "Apple juice",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/l7imGdupuhU",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 25L,
         name = "Apple pie",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/bkXzABDt08Q",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 26L,
         name = "Grapes",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/y2MeW00BdBo",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 27L,
         name = "Kiwi",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/1oMGgHn-M8k",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     ),
     Snack(
         id = 28L,
         name = "Mango",
         tagline = "A tag line",
-        imageUrl = "https://source.unsplash.com/TIGDsyy0TK4",
+        imageUrl = "https://images.unsplash.com/photo-1519869325930-281384150729?q=80&w=2066&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         price = 299
     )
 )
