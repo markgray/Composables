0c7bfc1470ad9bf7f79d956eb9602c91b50a1218
markgray <mark.t.gray@gmail.com>
2024-08-26 05:43:24 -0400

    Updated kotlin and compose plugin to 2.0.10, and updated bom to 2024.08.00 in BasicLayoutsCodelab

--- BasicLayoutsCodelab/app/build.gradle.kts
+++ BasicLayoutsCodelab/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 }
 
 dependencies {
-    val composeBom: Dependency = platform("androidx.compose:compose-bom:2024.06.00")
+    val composeBom: Dependency = platform("androidx.compose:compose-bom:2024.08.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
