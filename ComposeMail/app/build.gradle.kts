a6ba7c7f68a4eb76e8f5fd4cac6070f883cf596c
markgray <mark.t.gray@gmail.com>
2024-08-27 05:49:42 -0400

    Updated kotlin and compose plugin to 2.0.10, and updated gradle dependency in ComposeMail

--- ComposeMail/app/build.gradle.kts
+++ ComposeMail/app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.window:window:1.3.0")
-    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
