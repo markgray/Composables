8b9240b29471962a4ae25c5ae6fba83cf3d760fe
markgray <mark.t.gray@gmail.com>
2024-09-01 04:34:22 -0400

    Updated kotlin and compose plugin to 2.0.20 in BasicStateCodelab

--- BasicStateCodelab/app/build.gradle.kts
+++ BasicStateCodelab/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeCompiler {
-        enableStrongSkippingMode = true
+        // enableStrongSkippingMode = true is now default
     }
     packaging {
         resources {
