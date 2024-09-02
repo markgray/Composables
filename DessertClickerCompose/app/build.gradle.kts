637b4ff21c934db419a09f56aeea66e5cf82b96f
markgray <mark.t.gray@gmail.com>
2024-09-02 04:05:22 -0400

    Updated AGP to 8.6.0, and updated kotlin and compose plugins to 2.0.20 in DessertClickerCompose

--- DessertClickerCompose/app/build.gradle.kts
+++ DessertClickerCompose/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         compose = true
     }
     composeCompiler {
-        enableStrongSkippingMode = true
+        // enableStrongSkippingMode = true is now default
     }
     packaging {
         resources {
