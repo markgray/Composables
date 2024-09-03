ca7973112ebd7f47fb4003349fd760aaf19b4724
markgray <mark.t.gray@gmail.com>
2024-09-03 05:22:02 -0400

    Updated AGP to 8.6.0, and updated kotlin and compose plugins to 2.0.20 in MigrationCodelab

--- MigrationCodelab/app/build.gradle.kts
+++ MigrationCodelab/app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeCompiler {
-        enableStrongSkippingMode = true
+        // enableStrongSkippingMode = true is now default
     }
     packaging {
         resources {
