a5c5675fdd7459d8fd9119ee8099022288bb6c40
markgray <mark.t.gray@gmail.com>
2024-09-03 05:36:52 -0400

    Updated AGP to 8.6.0, updated kotlin and compose plugins to 2.0.20, and updated libs.versions.toml dependencies in PerformanceCodelab

--- PerformanceCodelab/app/build.gradle.kts
+++ PerformanceCodelab/app/build.gradle.kts
@@ -69,7 +69,7 @@ composeCompiler {
     // This settings enables strong-skipping mode for all module in this project.
     // As an effect, Compose can skip a composable even if it's unstable by comparing it's instance equality (===).
     // TODO Codelab task: Enable Strong Skipping Mode DONE
-    enableStrongSkippingMode = true
+    // enableStrongSkippingMode = true is now default
 
     // TODO Codelab task: Enable Stability Configuration file DONE
     stabilityConfigurationFile = project.rootDir.resolve("stability_config.conf")
