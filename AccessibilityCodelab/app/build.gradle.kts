3de25030c5650bd1074a4033f3a61dcee59ce344
markgray <mark.t.gray@gmail.com>
2024-09-07 05:09:50 -0400

    Updated gradle dependency in AccessibilityCodelab

--- AccessibilityCodelab/app/build.gradle.kts
+++ AccessibilityCodelab/app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.5")
