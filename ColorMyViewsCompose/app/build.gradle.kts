60dac5fe984a1b72248bd2f711e16be1a9784a5f
markgray <mark.t.gray@gmail.com>
2024-09-07 05:59:42 -0400

    Updated gradle dependencies in ColorMyViewsCompose

--- ColorMyViewsCompose/app/build.gradle.kts
+++ ColorMyViewsCompose/app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.0")
     implementation("androidx.compose.material:material:1.7.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.0")
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
