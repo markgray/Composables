ecf4508bcaebdd8f2cfccb051d444ddceeb2aeb2
markgray <mark.t.gray@gmail.com>
2024-09-08 05:58:13 -0400

    Updated gradle dependency in ComposeConstraintLayoutDemo

--- ComposeConstraintLayoutDemo/app/build.gradle.kts
+++ ComposeConstraintLayoutDemo/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
-    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.activity:activity-compose:1.9.2")
     //noinspection GradleDependency TODO: alpha14 requires SDK 35, keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
     implementation("androidx.compose.ui:ui:1.7.0")
