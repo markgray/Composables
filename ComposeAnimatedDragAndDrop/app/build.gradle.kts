6ee55f2ad6c4466516f39f5c12a6373fe5b9b07a
markgray <mark.t.gray@gmail.com>
2024-09-08 05:53:02 -0400

    Updated gradle dependencies in ComposeAnimatedDragAndDrop

--- ComposeAnimatedDragAndDrop/app/build.gradle.kts
+++ ComposeAnimatedDragAndDrop/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("androidx.activity:activity-compose:1.9.2")
     implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
