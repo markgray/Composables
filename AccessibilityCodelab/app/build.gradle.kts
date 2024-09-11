c8bec80c81b435e1ee2cec3c30524f1daa97c6dc
markgray <mark.t.gray@gmail.com>
2024-09-11 05:05:20 -0400

    Updated gradle dependencies in AccessibilityCodelab

--- AccessibilityCodelab/app/build.gradle.kts
+++ AccessibilityCodelab/app/build.gradle.kts
@@ -83,16 +83,16 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
-    implementation("androidx.compose.runtime:runtime:1.7.0")
-    implementation("androidx.compose.ui:ui:1.7.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.7.0")
-    implementation("androidx.compose.material:material:1.7.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.0")
-    implementation("androidx.compose.foundation:foundation:1.7.0")
-    implementation("androidx.compose.animation:animation:1.7.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.0")
+    implementation("androidx.compose.runtime:runtime:1.7.1")
+    implementation("androidx.compose.ui:ui:1.7.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.7.1")
+    implementation("androidx.compose.material:material:1.7.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.1")
+    implementation("androidx.compose.foundation:foundation:1.7.1")
+    implementation("androidx.compose.animation:animation:1.7.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.1")
 
     implementation("com.google.accompanist:accompanist-swiperefresh:0.34.0")
     implementation("com.google.accompanist:accompanist-insets:0.30.1")
@@ -111,12 +111,12 @@ dependencies {
 
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.0")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.7.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.1")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.1")
 
     // Robolectric dependencies
-    testImplementation("androidx.compose.ui:ui-test-junit4:1.7.0")
+    testImplementation("androidx.compose.ui:ui-test-junit4:1.7.1")
     testImplementation("org.robolectric:robolectric:4.12.2")
 }
