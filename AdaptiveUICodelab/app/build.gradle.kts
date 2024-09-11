4400d326ceea28f27a9d1eca26af98e278f89308
markgray <mark.t.gray@gmail.com>
2024-09-11 05:10:17 -0400

    Updated gradle dependencies in AdaptiveUICodelab

--- AdaptiveUICodelab/app/build.gradle.kts
+++ AdaptiveUICodelab/app/build.gradle.kts
@@ -67,12 +67,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
-    implementation("androidx.compose.ui:ui:1.7.0")
+    implementation("androidx.compose.ui:ui:1.7.1")
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
 
-    implementation("androidx.compose.material:material-icons-extended:1.7.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
     implementation("androidx.window:window:1.3.0")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
@@ -81,7 +81,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.1")
 }
