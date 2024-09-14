19576dd4e11b9d73306dc17f70b6792e8186dfe5
markgray <mark.t.gray@gmail.com>
2024-09-14 05:19:24 -0400

    Updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -50,9 +50,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.ui:ui:1.7.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.0")
-    implementation("androidx.compose.material:material:1.7.0")
+    implementation("androidx.compose.ui:ui:1.7.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
+    implementation("androidx.compose.material:material:1.7.1")
     //noinspection GradleDependency TODO: alpha14 requires SDK 35 - keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
 }
