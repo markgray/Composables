da19b953ddd7552512f634cba1d681a1bf02f0ad
markgray <mark.t.gray@gmail.com>
2024-09-16 06:31:51 -0400

    Added enableEdgeToEdge in AndroidTriviaCompose

--- AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/MainActivity.kt
+++ AndroidTriviaCompose/app/src/main/java/com/example/androidtriviacompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.androidtriviacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Button
@@ -60,9 +63,12 @@ class MainActivity : ComponentActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AndroidTriviaComposeTheme {
-                AndroidTriviaApp()
+                Box(modifier = Modifier.safeDrawingPadding()) {
+                    AndroidTriviaApp()
+                }
             }
         }
     }
