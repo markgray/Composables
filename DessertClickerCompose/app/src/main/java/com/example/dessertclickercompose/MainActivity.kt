a36190a595ffdb82a1c8cba1b7df353de71e408e
markgray <mark.t.gray@gmail.com>
2024-09-19 06:43:07 -0400

    Updated AGP to 8.6.1, updated gradle dependencies, and added enableEdgeToEdge in DessertClickerCompose

--- DessertClickerCompose/app/src/main/java/com/example/dessertclickercompose/MainActivity.kt
+++ DessertClickerCompose/app/src/main/java/com/example/dessertclickercompose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dessertclickercompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
@@ -49,14 +51,17 @@ class MainActivity : ComponentActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             DessertClickerComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MainScreen()
+                Box(modifier = Modifier.safeDrawingPadding()) {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        MainScreen()
+                    }
                 }
             }
         }
