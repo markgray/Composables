048f6e308ddc3b4daa7dfec7585a81db2c855b06
markgray <mark.t.gray@gmail.com>
2024-09-24 06:10:19 -0400

    Updated gradle dependencies in ColorMyViewsCompose

--- ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
+++ ColorMyViewsCompose/app/src/main/java/com/example/colormyviewscompose/MainActivity.kt
@@ -57,8 +57,8 @@ class MainActivity : ComponentActivity() {
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             ColorMyViewsComposeTheme {
                 Box(modifier = Modifier.safeDrawingPadding()) {
