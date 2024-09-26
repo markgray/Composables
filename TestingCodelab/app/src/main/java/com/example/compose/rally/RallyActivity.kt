31f4092b283a39019b3cedbd20bbc71e71a55145
markgray <mark.t.gray@gmail.com>
2024-09-26 05:56:29 -0400

    Moved enableEdgeToEdge in TestingCodelab

--- TestingCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ TestingCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -52,8 +52,8 @@ class RallyActivity : ComponentActivity() {
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             Box(modifier = Modifier.safeDrawingPadding()) {
                 RallyApp()
