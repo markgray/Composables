a220826dd7184db015ea58b4813983d2d9f80511
markgray <mark.t.gray@gmail.com>
2024-09-26 05:34:34 -0400

    Moved enableEdgeToEdge in MigrationCodelab

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -45,8 +45,8 @@ class GardenActivity : AppCompatActivity() {
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
 
         // Displaying edge-to-edge
         WindowCompat.setDecorFitsSystemWindows(window, false)
