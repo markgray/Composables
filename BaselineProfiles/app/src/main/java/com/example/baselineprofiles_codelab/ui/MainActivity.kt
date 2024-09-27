7ea44d4a6b88abc81bb248729b705e799b01b290
markgray <mark.t.gray@gmail.com>
2024-09-27 06:13:25 -0400

    Moved enableEdgeToEdge in BaselineProfiles

--- BaselineProfiles/app/src/main/java/com/example/baselineprofiles_codelab/ui/MainActivity.kt
+++ BaselineProfiles/app/src/main/java/com/example/baselineprofiles_codelab/ui/MainActivity.kt
@@ -27,8 +27,8 @@ import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
 
         // This app draws behind the system bars, so we want to handle fitting system windows
         WindowCompat.setDecorFitsSystemWindows(window, false)
