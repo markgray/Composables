3fc3e29d95cce2c892c688190d03fa54eb71a397
markgray <mark.t.gray@gmail.com>
2024-10-03 04:35:14 -0400

    Updated AGP to 8.7.0, and updated bom to 2024.09.03 in BasicLayoutsCodelab

--- BasicLayoutsCodelab/app/build.gradle.kts
+++ BasicLayoutsCodelab/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 }
 
 dependencies {
-    val composeBom: Dependency = platform("androidx.compose:compose-bom:2024.09.01")
+    val composeBom: Dependency = platform("androidx.compose:compose-bom:2024.09.03")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
