87f294a0757919516c6c2086b7d60db72c004631
markgray <mark.t.gray@gmail.com>
2024-10-04 06:10:18 -0400

    Updated AGP to 8.7.0, updated gradle dependencies, and added kotlinOptions{jvmTarget = "17"} in Jetchat

--- Jetchat/app/build.gradle.kts
+++ Jetchat/app/build.gradle.kts
@@ -66,6 +66,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
         viewBinding = true
