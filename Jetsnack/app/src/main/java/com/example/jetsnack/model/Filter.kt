5d59fd6d1e111bc315dde1c8eabee1c0e54c5f72
markgray <mark.t.gray@gmail.com>
2024-10-13 06:17:55 -0400

    Added comments in Jetsnack

--- Jetsnack/app/src/main/java/com/example/jetsnack/model/Filter.kt
+++ Jetsnack/app/src/main/java/com/example/jetsnack/model/Filter.kt
@@ -20,17 +20,37 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.SortByAlpha
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.jetsnack.ui.components.FilterBar
+import com.example.jetsnack.ui.components.FilterChip
+import com.example.jetsnack.ui.home.FilterScreen
+import com.example.jetsnack.ui.home.SortFilters
+import com.example.jetsnack.ui.home.SortFiltersSection
 
+/**
+ * This class holds the state of the various [FilterChip] that are at the top of the home screen in
+ * a [FilterBar].
+ *
+ * @param name [String] describing the type of filtering that ths [FilterChip] will perform if it is
+ * in a selected state.
+ * @param enabled the initial state of the [MutableState] wrapped [Boolean] that is accessed via the
+ * [Filter.enabled] property.
+ * @param icon an [ImageVector] that will be displayed by the [SortFilters] in the [SortFiltersSection]
+ * of the home screen (which is opened by clicking on the icon at the far left oa the [FilterScreen]
+ */
 @Stable
 class Filter(
     val name: String,
     enabled: Boolean = false,
     val icon: ImageVector? = null
 ) {
-    val enabled = mutableStateOf(enabled)
+    /**
+     * This is used to hold and set the selected state of the [FilterChip] using this [Filter]
+     */
+    val enabled: MutableState<Boolean> = mutableStateOf(enabled)
 }
 
 val filters = listOf(
