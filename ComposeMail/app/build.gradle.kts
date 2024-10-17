764d45a94058c82e6ba8f98b1d2c9ce96226fd1b
Mark Gray <mark.t.gray@gmail.com>
2024-10-17 04:05:17 -0400

    Updated AGP to 8.7.1, and updated gradle dependencies in ComposeMail

--- ComposeMail/app/build.gradle.kts
+++ ComposeMail/app/build.gradle.kts
@@ -77,23 +77,22 @@ dependencies {
     //noinspection GradleDependency TODO: Newer version breaks compilation!
     implementation("androidx.paging:paging-common-ktx:3.1.1")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
-    implementation("androidx.compose.ui:ui:1.7.3")
-    implementation("androidx.compose.material:material:1.7.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.3")
-    implementation("androidx.compose.material:material-icons-extended:1.7.3")
+    implementation("androidx.compose.ui:ui:1.7.4")
+    implementation("androidx.compose.material:material:1.7.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
+    implementation("androidx.compose.material:material-icons-extended:1.7.4")
     //noinspection GradleDependency TODO: Newer version breaks compilation!
     implementation("androidx.paging:paging-compose:1.0.0-alpha17")
     //noinspection GradleDependency TODO: Newer version breaks compilation!
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha07")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.window:window:1.3.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
 }
-
