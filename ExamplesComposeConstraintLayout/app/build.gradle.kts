c0002b373c86061fff04e335ea75e3fcfceed0aa
Mark Gray <mark.t.gray@gmail.com>
2024-10-17 05:03:20 -0400

    Updated AGP to 8.7.1, and updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -49,10 +49,10 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.ui:ui:1.7.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.3")
-    implementation("androidx.compose.material:material:1.7.3")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui:1.7.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
+    implementation("androidx.compose.material:material:1.7.4")
     //noinspection GradleDependency TODO: alpha14 requires SDK 35 - keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
 }
