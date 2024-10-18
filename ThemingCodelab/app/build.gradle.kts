e6c867c261694184d83012c331480b97071a2a00
Mark Gray <mark.t.gray@gmail.com>
2024-10-18 04:00:05 -0400

    Updated AGP to 8.8.1, and updated gradle dependencies in ThemingCodelab

--- ThemingCodelab/app/build.gradle.kts
+++ ThemingCodelab/app/build.gradle.kts
@@ -75,27 +75,27 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.runtime:runtime:1.7.3")
-    implementation("androidx.compose.ui:ui:1.7.3")
+    implementation("androidx.compose.runtime:runtime:1.7.4")
+    implementation("androidx.compose.ui:ui:1.7.4")
     implementation("androidx.compose.material3:material3:1.3.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.3")
-    implementation("androidx.compose.foundation:foundation-layout:1.7.3")
-    implementation("androidx.compose.foundation:foundation:1.7.3")
-    implementation("androidx.compose.animation:animation:1.7.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.3")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.3")
-    testImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.7.3")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
+    implementation("androidx.compose.material:material-icons-extended:1.7.4")
+    implementation("androidx.compose.foundation:foundation-layout:1.7.4")
+    implementation("androidx.compose.foundation:foundation:1.7.4")
+    implementation("androidx.compose.animation:animation:1.7.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.4")
+    testImplementation("androidx.compose.ui:ui-test-junit4:1.7.4")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.7.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.4")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.6")
@@ -110,4 +110,3 @@ dependencies {
 tasks.withType<Test> {
     systemProperty("robolectric.logging", "stdout")
 }
-
