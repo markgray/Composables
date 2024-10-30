eb0b504bcea451ed56636a25ac7a868b05185144
Mark Gray <mark.t.gray@gmail.com>
2024-10-30 03:19:31 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in AnimationCodelab

--- AnimationCodelab/finished/build.gradle.kts
+++ AnimationCodelab/finished/build.gradle.kts
@@ -21,11 +21,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.codelab.animation"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
