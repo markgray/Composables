fcaec3551844fcf5142dd0719ae2feb3321da0bf
Mark Gray <mark.t.gray@gmail.com>
2024-10-30 03:58:01 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.codelab.basics"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
