927ff17f0449d27a9f71da55cfd729a686c8dd17
Mark Gray <mark.t.gray@gmail.com>
2024-10-30 04:36:35 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in ColorMyViewsCompose

--- ColorMyViewsCompose/app/build.gradle.kts
+++ ColorMyViewsCompose/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.colormyviewscompose"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
