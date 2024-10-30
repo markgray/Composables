c102b3b55e670412ab88ac3f5532bbbe71e71556
Mark Gray <mark.t.gray@gmail.com>
2024-10-30 04:17:29 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in ColorInM3

--- ColorInM3/app/build.gradle.kts
+++ ColorInM3/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.android.colorinm3"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.android.colorinm3"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
