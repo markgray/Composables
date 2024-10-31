559a87da4671565da6fcd92958fa30f37aa8caf1
Mark Gray <mark.t.gray@gmail.com>
2024-10-31 03:08:15 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependency in ComposeGraph3d

--- ComposeGraph3d/app/build.gradle.kts
+++ ComposeGraph3d/app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "android.support.composegraph3d"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "android.support.composegraph3d"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui:1.7.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
-    implementation("androidx.compose.runtime:runtime-android:1.7.4")
+    implementation("androidx.compose.runtime:runtime-android:1.7.5")
     implementation("androidx.compose.material:material:1.7.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
