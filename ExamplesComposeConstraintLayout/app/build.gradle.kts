3b6608055e6a88844d0412a961a21070687c18f4
Mark Gray <mark.t.gray@gmail.com>
2024-11-01 03:32:07 -0400

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.examplescomposeconstraintlayout"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.examplescomposeconstraintlayout"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -47,11 +47,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.ui:ui:1.7.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
-    implementation("androidx.compose.material:material:1.7.4")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-rc01")
+    implementation("androidx.compose.ui:ui:1.7.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
 }
