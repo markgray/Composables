bdf3320eb548658375e2e156e8433e4607082317
Mark Gray <mark.t.gray@gmail.com>
2024-11-01 04:24:11 -0400

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependencies in MigrationCodelab

--- MigrationCodelab/app/build.gradle.kts
+++ MigrationCodelab/app/build.gradle.kts
@@ -24,11 +24,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.google.samples.apps.sunflower"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionCode = 1
         versionName = "0.1.6"
@@ -71,34 +71,34 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
     ksp("com.github.bumptech.glide:compiler:4.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     // Compose
-    implementation("androidx.compose.runtime:runtime:1.7.4")
-    implementation("androidx.compose.ui:ui:1.7.4")
-    implementation("androidx.compose.foundation:foundation:1.7.4")
-    implementation("androidx.compose.foundation:foundation-layout:1.7.4")
-    implementation("androidx.compose.material:material:1.7.4")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.4")
+    implementation("androidx.compose.runtime:runtime:1.7.5")
+    implementation("androidx.compose.ui:ui:1.7.5")
+    implementation("androidx.compose.foundation:foundation:1.7.5")
+    implementation("androidx.compose.foundation:foundation-layout:1.7.5")
+    implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 
     implementation("com.google.accompanist:accompanist-themeadapter-material:0.34.0")
 
@@ -109,8 +109,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
-    androidTestImplementation("androidx.work:work-testing:2.9.1")
+    androidTestImplementation("androidx.work:work-testing:2.10.0")
     androidTestImplementation("com.google.truth:truth:1.4.4")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.5")
     testImplementation("junit:junit:4.13.2")
 }
