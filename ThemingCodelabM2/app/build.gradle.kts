26caa4cdc2c0806a37924be2485f02f55c00c6eb
Mark Gray <mark.t.gray@gmail.com>
2024-11-02 03:14:34 -0400

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependencies in ThemingCodelabM2

--- ThemingCodelabM2/app/build.gradle.kts
+++ ThemingCodelabM2/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.codelab.theming"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -30,11 +30,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.compose.ui:ui:1.7.4")
-    implementation("androidx.compose.material:material:1.7.4")
-    implementation("androidx.compose.material:material-icons-extended:1.7.4")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.compose.ui:ui:1.7.5")
+    implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 }
