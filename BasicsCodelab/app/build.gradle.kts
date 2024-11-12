f0bc79ff41975d6fc73bfd6988f2b3306db0b4b2
markgray <mark.t.gray@gmail.com>
2024-11-12 07:03:12 -0500

    Updated AGP to 8.7.2 and updated gradle dependencies in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -49,15 +49,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.compose.ui:ui:1.7.4")
+    implementation("androidx.compose.ui:ui:1.7.5")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.material:material:1.7.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
-    implementation("androidx.compose.material:material-icons-extended:1.7.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
+    implementation("androidx.compose.material:material:1.7.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
