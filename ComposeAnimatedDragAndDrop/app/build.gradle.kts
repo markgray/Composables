704aa6ce0bdbc03a43e416fc33a7a6d6e5827f5e
markgray <mark.t.gray@gmail.com>
2024-11-13 05:30:52 -0500

    Updated AGP to 8.7.2, updated gradle dependencies and removed deprecated setting of statusBarColor in ComposeAnimatedDragAndDrop

--- ComposeAnimatedDragAndDrop/app/build.gradle.kts
+++ ComposeAnimatedDragAndDrop/app/build.gradle.kts
@@ -46,23 +46,23 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-core:1.7.4")
-    implementation("androidx.compose.material:material-icons-extended:1.7.4")
+    implementation("androidx.compose.material:material-icons-core:1.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    //noinspection GradleDependency TODO: alpha14 requires SDK 35, keep an eye on this
+    //noinspection GradleDependency TODO: 1.1.0 breaks compilation, keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
 }
