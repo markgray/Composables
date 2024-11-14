131b5680ec4aa3079757fff62ccc41fe7cca8c71
markgray <mark.t.gray@gmail.com>
2024-11-14 06:32:51 -0500

    Updated gradle dependencies in NavigationCodelab

--- NavigationCodelab/app/build.gradle.kts
+++ NavigationCodelab/app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.compose.material:material:1.7.5")
     implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
 
     // Testing dependencies
@@ -99,5 +99,5 @@ dependencies {
     // Compose testing dependencies
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.5")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.5")
-    androidTestImplementation("androidx.navigation:navigation-testing:2.8.3")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.4")
 }
