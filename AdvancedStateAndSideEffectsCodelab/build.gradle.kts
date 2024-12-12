4fe5fae64009d25f7e6d384b906302af2b04e660
Mark Gray <mark.t.gray@gmail.com>
2024-12-12 04:07:28 -0500

    Updated gradle dependencies, switched from kapt to ksp in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/build.gradle.kts
+++ AdvancedStateAndSideEffectsCodelab/build.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.53" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.dagger.hilt.android") version "2.53.1" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
