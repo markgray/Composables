40ade8aff9fe460f02166117c1a71a08b66b1e17
Mark Gray <mark.t.gray@gmail.com>
2024-12-14 03:37:56 -0500

    Updated AGP to 8.7.3, updated kotlin and compose to 2.1.0, and updated gradle dependencies in ThemingCodelabM2

--- ThemingCodelabM2/app/build.gradle.kts
+++ ThemingCodelabM2/app/build.gradle.kts
@@ -31,10 +31,10 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.compose.ui:ui:1.7.5")
-    implementation("androidx.compose.material:material:1.7.5")
-    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material:material:1.7.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.5")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 }
