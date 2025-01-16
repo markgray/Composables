fe8744d6126ba34ce7ca0139844e29c49d11af91
Mark Gray <mark.t.gray@gmail.com>
2025-01-16 04:14:53 -0500

    Updated AGP to 8.8.0 and updated gradle dependencies in DataStore

--- DataStore/app/build.gradle.kts
+++ DataStore/app/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 
     // ProtBuf
-    implementation("androidx.datastore:datastore-core:1.1.1")
+    implementation("androidx.datastore:datastore-core:1.1.2")
     implementation("com.google.protobuf:protobuf-javalite:4.26.1")
-    implementation("androidx.datastore:datastore:1.1.1")
+    implementation("androidx.datastore:datastore:1.1.2")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
