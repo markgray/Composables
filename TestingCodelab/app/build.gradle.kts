0aaca18a3963fd3a17514b6899b2a942dbea6a2c
markgray <mark.t.gray@gmail.com>
2025-01-16 07:42:17 -0500

    Updated AGP to 8.8.0 and updated gradle dependency in TestingCodelab

--- TestingCodelab/app/build.gradle.kts
+++ TestingCodelab/app/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
     implementation("androidx.compose.material:material-icons-extended:1.7.6")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 
     // Testing dependencies
