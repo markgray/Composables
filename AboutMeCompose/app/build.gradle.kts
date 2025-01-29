b46a01f4afaa51338ef6a06a522d886eb78daa8c
Mark Gray <mark.t.gray@gmail.com>
2025-01-29 04:08:39 -0500

    Updated gradle dependency in AboutMeCompose

--- AboutMeCompose/app/build.gradle.kts
+++ AboutMeCompose/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     testImplementation("junit:junit:4.13.2")
