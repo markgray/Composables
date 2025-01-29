d36b3d83047884ff0609e93ce317d6f58acb7494
markgray <mark.t.gray@gmail.com>
2025-01-29 07:11:23 -0500

    Updated gradle dependency in BasicLayoutsCodelab

--- BasicLayoutsCodelab/app/build.gradle.kts
+++ BasicLayoutsCodelab/app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
