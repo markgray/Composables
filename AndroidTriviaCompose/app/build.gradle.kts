79e3c5525737a7fd3003698ef61e93147b0f7add
markgray <mark.t.gray@gmail.com>
2025-01-29 06:28:56 -0500

    Updated gradle dependency in AndroidTriviaCompose

--- AndroidTriviaCompose/app/build.gradle.kts
+++ AndroidTriviaCompose/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.material:material:1.7.6")
     implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
