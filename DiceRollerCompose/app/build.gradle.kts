6856330a887686e9520649f6433e59eff4305a5a
Mark Gray <mark.t.gray@gmail.com>
2025-02-13 04:36:51 -0500

    Updated kotlin to 2.1.10 and updated gradle dependencies in DiceRollerCompose

--- DiceRollerCompose/app/build.gradle.kts
+++ DiceRollerCompose/app/build.gradle.kts
@@ -47,16 +47,16 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.compose.ui:ui:1.7.7")
-    implementation("androidx.compose.material:material:1.7.7")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.7")
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.material:material:1.7.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.7")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.7")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.7")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.8")
 }
