e8f7eb3f57454346164bd18f4f78b9f087328022
Mark Gray <mark.t.gray@gmail.com>
2025-02-27 04:06:56 -0500

    Updated AGP to 8.8.2 and updated gradle dependencies in ThemingCodelab

--- ThemingCodelab/app/build.gradle.kts
+++ ThemingCodelab/app/build.gradle.kts
@@ -90,12 +90,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test:1.7.8")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
