009a569889001f411a1d1b0066913edaaca7a922
Mark Gray <mark.t.gray@gmail.com>
2025-02-27 04:39:32 -0500

    Updated AGP to 8.8.2 and updated gradle dependencies in TrackMySleepQuality

--- TrackMySleepQuality/app/build.gradle.kts
+++ TrackMySleepQuality/app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
     implementation("androidx.compose.material:material:1.7.8")
