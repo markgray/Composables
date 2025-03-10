9149251ca16426fed46f4f764c74a486d738961f
markgray <mark.t.gray@gmail.com>
2025-03-10 06:41:27 -0400

    Updated AGP to 8.9.0 and updated gradle dependency in ColorMyViewsCompose

--- ColorMyViewsCompose/app/build.gradle.kts
+++ ColorMyViewsCompose/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     testImplementation("junit:junit:4.13.2")
