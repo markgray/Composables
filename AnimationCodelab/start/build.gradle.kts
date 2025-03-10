f05d78bcd1cc301864cf97da95d1b7f8513fa4f5
Mark Gray <mark.t.gray@gmail.com>
2025-03-10 04:27:37 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in AnimationCodelab

--- AnimationCodelab/start/build.gradle.kts
+++ AnimationCodelab/start/build.gradle.kts
@@ -51,6 +51,6 @@ dependencies {
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
 }
