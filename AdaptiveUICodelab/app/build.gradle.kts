cb82ad15f83010643fca8c717d9ad8494615bd3c
Mark Gray <mark.t.gray@gmail.com>
2025-03-10 04:10:11 -0400

    Updated AGP to 8.9.0 and updated gradle dependency in AdaptiveUICodelab

--- AdaptiveUICodelab/app/build.gradle.kts
+++ AdaptiveUICodelab/app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
     implementation("androidx.window:window:1.3.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
