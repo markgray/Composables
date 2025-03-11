92a6fa40702e0dcd7e422039e864ef438314a0db
markgray <mark.t.gray@gmail.com>
2025-03-11 07:25:33 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in NavigationCodelab

--- NavigationCodelab/app/build.gradle.kts
+++ NavigationCodelab/app/build.gradle.kts
@@ -87,8 +87,8 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:1.7.8")
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.navigation:navigation-compose:2.8.7")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.navigation:navigation-compose:2.8.8")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
 
     // Testing dependencies
@@ -99,5 +99,5 @@ dependencies {
     // Compose testing dependencies
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.8")
-    androidTestImplementation("androidx.navigation:navigation-testing:2.8.7")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.8")
 }
