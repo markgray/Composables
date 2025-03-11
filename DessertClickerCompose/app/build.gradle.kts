39edad6eedf599a2c0a1abdc592ce379d61042d9
Mark Gray <mark.t.gray@gmail.com>
2025-03-11 04:25:35 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in DessertClickerCompose

--- DessertClickerCompose/app/build.gradle.kts
+++ DessertClickerCompose/app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     testImplementation("junit:junit:4.13.2")
