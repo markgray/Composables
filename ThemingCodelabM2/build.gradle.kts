944986b9b47d99b4d6432d01c572e0b5f5195a33
markgray <mark.t.gray@gmail.com>
2025-03-23 06:07:55 -0400

    Updated kotlin to 2.1.20 in ThemingCodelabM2

--- ThemingCodelabM2/build.gradle.kts
+++ ThemingCodelabM2/build.gradle.kts
@@ -18,6 +18,6 @@
 
 plugins {
     id("com.android.application") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
 }
