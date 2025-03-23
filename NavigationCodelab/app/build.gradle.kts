c45aa3493c97c03bfe6f93d144395aa7a45cf583
Mark Gray <mark.t.gray@gmail.com>
2025-03-23 04:08:21 -0400

    Updated kotlin to 2.1.20 and updated gradle dependencies in NavigationCodelab

--- NavigationCodelab/app/build.gradle.kts
+++ NavigationCodelab/app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.8.8")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
 
     // Testing dependencies
@@ -99,5 +99,5 @@ dependencies {
     // Compose testing dependencies
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.8")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.8")
-    androidTestImplementation("androidx.navigation:navigation-testing:2.8.8")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.9")
 }
