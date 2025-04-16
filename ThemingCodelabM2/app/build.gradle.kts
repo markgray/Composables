16588a9f0786b3641d8b9e16af6df12657c62663
markgray <mark.t.gray@gmail.com>
2025-04-16 06:10:32 -0400

    Updated gradle dependency in ThemingCodelabM2

--- ThemingCodelabM2/app/build.gradle.kts
+++ ThemingCodelabM2/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.material:material:1.7.8")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
