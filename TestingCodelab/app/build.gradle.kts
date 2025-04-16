c026a9e46e4a1215d0892cd8a1f253b8c665f62a
markgray <mark.t.gray@gmail.com>
2025-04-16 06:03:34 -0400

    Updated gradle dependency in TestingCodelab

--- TestingCodelab/app/build.gradle.kts
+++ TestingCodelab/app/build.gradle.kts
@@ -84,7 +84,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
 
