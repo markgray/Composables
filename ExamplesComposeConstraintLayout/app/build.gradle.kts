e4ce8bed75d2ba56c33dd838e29cbc2f8b12261f
Mark Gray <mark.t.gray@gmail.com>
2025-04-24 03:55:55 -0400

    Updated AGP to 8.9.2 and updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    implementation("androidx.compose.material:material:1.7.8")
+    implementation("androidx.compose.ui:ui:1.8.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.0")
+    implementation("androidx.compose.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
 }
