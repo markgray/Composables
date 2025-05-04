67da16a7f6e348ecf4adb69ba2eede70587b69c1
Mark Gray <mark.t.gray@gmail.com>
2025-05-04 03:45:27 -0400

    Updated gradle dependencies in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/build.gradle.kts
+++ AdvancedStateAndSideEffectsCodelab/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("com.android.application") version "8.9.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
     id("com.google.dagger.hilt.android") version "2.56.1" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
