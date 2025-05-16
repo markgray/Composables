12002c3135990ab25a7df9077a115f5fb45d83c4
Mark Gray <mark.t.gray@gmail.com>
2025-05-16 04:09:11 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in AccessibilityCodelab

--- AccessibilityCodelab/app/build.gradle.kts
+++ AccessibilityCodelab/app/build.gradle.kts
@@ -82,16 +82,16 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
-    implementation("androidx.compose.runtime:runtime:1.8.0")
-    implementation("androidx.compose.ui:ui:1.8.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.8.0")
-    implementation("androidx.compose.material:material:1.8.0")
+    implementation("androidx.compose.runtime:runtime:1.8.1")
+    implementation("androidx.compose.ui:ui:1.8.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.8.1")
+    implementation("androidx.compose.material:material:1.8.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.foundation:foundation:1.8.0")
-    implementation("androidx.compose.animation:animation:1.8.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.8.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.0")
+    implementation("androidx.compose.foundation:foundation:1.8.1")
+    implementation("androidx.compose.animation:animation:1.8.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.8.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.8.1")
 
     implementation("com.google.accompanist:accompanist-swiperefresh:0.36.0")
     implementation("com.google.accompanist:accompanist-insets:0.30.1")
@@ -101,21 +101,21 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
 
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.8.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.0")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.8.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.1")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.1")
 
     // Robolectric dependencies
-    testImplementation("androidx.compose.ui:ui-test-junit4:1.8.0")
+    testImplementation("androidx.compose.ui:ui-test-junit4:1.8.1")
     testImplementation("org.robolectric:robolectric:4.14.1")
 }
