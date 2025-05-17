f2037d9dcbb210d63adada4202d363cd92bf6fca
markgray <mark.t.gray@gmail.com>
2025-05-17 06:47:54 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -48,10 +48,10 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.8.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.0")
-    implementation("androidx.compose.material:material:1.8.0")
+    implementation("androidx.compose.ui:ui:1.8.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
+    implementation("androidx.compose.material:material:1.8.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
 }
