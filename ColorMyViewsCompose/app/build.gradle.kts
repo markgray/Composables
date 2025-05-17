fda2687a1a1dc863b579049ec0881f255b51e9e3
Mark Gray <mark.t.gray@gmail.com>
2025-05-17 03:28:47 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in ColorMyViewsCompose

--- ColorMyViewsCompose/app/build.gradle.kts
+++ ColorMyViewsCompose/app/build.gradle.kts
@@ -48,16 +48,16 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.compose.ui:ui:1.8.0")
-    implementation("androidx.compose.material:material:1.8.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.0")
+    implementation("androidx.compose.ui:ui:1.8.1")
+    implementation("androidx.compose.material:material:1.8.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.8.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.1")
 }
