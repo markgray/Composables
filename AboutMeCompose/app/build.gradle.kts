107035ecdfd2d589e9a567747a68b781ebee768f
Mark Gray <mark.t.gray@gmail.com>
2025-05-29 03:42:12 -0400

    Updated AGP to 8.10.1, updated gradle to 8.14.1 and updated gradle dependencies in AboutMeCompose

--- AboutMeCompose/app/build.gradle.kts
+++ AboutMeCompose/app/build.gradle.kts
@@ -47,17 +47,17 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.compose.ui:ui:1.8.1")
+    implementation("androidx.compose.ui:ui:1.8.2")
     implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.2")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.8.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.2")
 }
