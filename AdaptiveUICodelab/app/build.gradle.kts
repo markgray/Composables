64949eca4a3813386c602468b2597af836c993d7
markgray <mark.t.gray@gmail.com>
2025-06-12 06:32:55 -0400

    Updated gradle to 8.14.2 and updated gradle dependencies in AdaptiveUICodelab

--- AdaptiveUICodelab/app/build.gradle.kts
+++ AdaptiveUICodelab/app/build.gradle.kts
@@ -75,8 +75,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.8.2")
     implementation("androidx.window:window:1.4.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
