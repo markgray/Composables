9b75fac5caabf8ece720bf53e391597bd9afe468
markgray <mark.t.gray@gmail.com>
2025-06-13 07:04:34 -0400

    Updated gradle to 8.14.2 and updated gradle dependency in ComposeGraph3d

--- ComposeGraph3d/app/build.gradle.kts
+++ ComposeGraph3d/app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.8.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.8.2")
