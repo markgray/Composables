9a715b1c37743dc5035b3355e09b24c70e855099
Mark Gray <mark.t.gray@gmail.com>
2025-06-15 03:40:53 -0400

    Updated gradle to 8.14.2 and updated gradle dependencies in MigrationCodelab

--- MigrationCodelab/app/build.gradle.kts
+++ MigrationCodelab/app/build.gradle.kts
@@ -70,10 +70,10 @@ android {
 dependencies {
     ksp("androidx.room:room-compiler:2.7.1")
     ksp("com.github.bumptech.glide:compiler:4.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.7")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
@@ -86,8 +86,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
 
     // Compose
     implementation("androidx.compose.runtime:runtime:1.8.2")
