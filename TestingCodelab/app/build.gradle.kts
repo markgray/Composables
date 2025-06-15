883e64f2244ac77ff9beb5450219b88e4932678a
markgray <mark.t.gray@gmail.com>
2025-06-15 05:52:25 -0400

    Updated gradle to 8.14.2 and updated gradle dependencies in TestingCodelab

--- TestingCodelab/app/build.gradle.kts
+++ TestingCodelab/app/build.gradle.kts
@@ -83,10 +83,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.7")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
 
     // Compose
     implementation("androidx.compose.runtime:runtime:1.8.2")
