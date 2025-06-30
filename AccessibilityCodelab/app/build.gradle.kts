f16084697b973152af189e1f42ce879be1e7b5ff
Mark Gray <mark.t.gray@gmail.com>
2025-06-30 03:13:37 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in AccessibilityCodelab

--- AccessibilityCodelab/app/build.gradle.kts
+++ AccessibilityCodelab/app/build.gradle.kts
@@ -1,18 +1,4 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
@@ -21,11 +7,11 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.jetnews"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -62,8 +48,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     buildFeatures {
         compose = true
@@ -82,16 +70,16 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
-    implementation("androidx.compose.runtime:runtime:1.8.2")
-    implementation("androidx.compose.ui:ui:1.8.2")
-    implementation("androidx.compose.foundation:foundation-layout:1.8.2")
-    implementation("androidx.compose.material:material:1.8.2")
+    implementation("androidx.compose.runtime:runtime:1.8.3")
+    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.foundation:foundation-layout:1.8.3")
+    implementation("androidx.compose.material:material:1.8.3")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.foundation:foundation:1.8.2")
-    implementation("androidx.compose.animation:animation:1.8.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.2")
-    implementation("androidx.compose.runtime:runtime-livedata:1.8.2")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.2")
+    implementation("androidx.compose.foundation:foundation:1.8.3")
+    implementation("androidx.compose.animation:animation:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.8.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
 
     implementation("com.google.accompanist:accompanist-swiperefresh:0.36.0")
     implementation("com.google.accompanist:accompanist-insets:0.30.1")
@@ -110,12 +98,12 @@ dependencies {
 
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.compose.ui:ui-test:1.8.2")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.2")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.8.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.3")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.3")
 
     // Robolectric dependencies
-    testImplementation("androidx.compose.ui:ui-test-junit4:1.8.2")
-    testImplementation("org.robolectric:robolectric:4.14.1")
+    testImplementation("androidx.compose.ui:ui-test-junit4:1.8.3")
+    testImplementation("org.robolectric:robolectric:4.15.1")
 }
