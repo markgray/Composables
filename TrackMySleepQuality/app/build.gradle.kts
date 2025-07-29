1c9bdcc91b1ec4b200c1d1555e30dc5576237804
markgray <mark.t.gray@gmail.com>
2025-07-29 07:28:50 -0400

    Updated gradle dependencies in TrackMySleepQuality

--- TrackMySleepQuality/app/build.gradle.kts
+++ TrackMySleepQuality/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.8.3")
