d025f7699cf3c71b12b154667ed6698b491e8f96
markgray <mark.t.gray@gmail.com>
2025-08-09 06:55:02 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -47,6 +47,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
