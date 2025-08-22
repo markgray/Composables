ca059489e552ee563397afeebce34da30cec7101
Mark Gray <mark.t.gray@gmail.com>
2025-08-22 04:17:32 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in AdaptiveUICodelab

--- AdaptiveUICodelab/app/build.gradle.kts
+++ AdaptiveUICodelab/app/build.gradle.kts
@@ -52,15 +52,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
-    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.ui:ui:1.9.0")
     implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
 
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
     implementation("androidx.window:window:1.4.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
@@ -69,7 +69,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.0")
 }
--- AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ AdaptiveUICodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+@file:Suppress("UnusedImport")
+
 package com.example.reply.ui
 
 import androidx.compose.animation.AnimatedVisibility
