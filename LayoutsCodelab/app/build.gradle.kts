9fe8bb678386aa3976807eb9bb7e88373af54502
markgray <mark.t.gray@gmail.com>
2025-08-23 08:21:12 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in LayoutsCodelab

--- LayoutsCodelab/app/build.gradle.kts
+++ LayoutsCodelab/app/build.gradle.kts
@@ -35,15 +35,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.compose.ui:ui:1.8.3")
+    implementation("androidx.compose.ui:ui:1.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
-    implementation("androidx.compose.material:material:1.8.3")
+    implementation("androidx.compose.material:material:1.9.0")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling:1.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
