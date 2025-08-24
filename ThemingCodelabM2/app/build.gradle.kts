b03120567a845716c92703e8ca86888369b81f20
markgray <mark.t.gray@gmail.com>
2025-08-24 08:29:32 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in ThemingCodelabM2

--- ThemingCodelabM2/app/build.gradle.kts
+++ ThemingCodelabM2/app/build.gradle.kts
@@ -34,11 +34,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.compose.ui:ui:1.8.3")
-    implementation("androidx.compose.material:material:1.8.3")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.compose.ui:ui:1.9.0")
+    implementation("androidx.compose.material:material:1.9.0")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.8.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
 }
