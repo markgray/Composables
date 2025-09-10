4b57e2b14c8d388e5acb9f3c12d20b63aa66f348
markgray <mark.t.gray@gmail.com>
2025-09-10 08:19:43 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.compose.ui:ui:1.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material:material:1.9.0")
