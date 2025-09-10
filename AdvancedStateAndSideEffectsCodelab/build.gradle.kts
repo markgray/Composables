c9f78a8f96ad4267e37118d2259d0ace969eb67f
Mark Gray <mark.t.gray@gmail.com>
2025-09-10 04:06:14 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/build.gradle.kts
+++ AdvancedStateAndSideEffectsCodelab/build.gradle.kts
@@ -17,10 +17,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.12.1" apply false
+    id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.57" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
