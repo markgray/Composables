ff19c2f3b15cedb9fa810f616867ef7cf4cbbd9a
Mark Gray <mark.t.gray@gmail.com>
2025-09-12 04:56:13 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in DataStore

--- DataStore/app/build.gradle.kts
+++ DataStore/app/build.gradle.kts
@@ -52,31 +52,31 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.9.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
-    implementation("androidx.compose.material:material:1.9.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.9.0")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.compose.ui:ui:1.9.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
+    implementation("androidx.compose.material:material:1.9.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.1")
 
     // architecture components
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.3")
 
     // ProtBuf
     implementation("androidx.datastore:datastore-core:1.1.7")
-    implementation("com.google.protobuf:protobuf-javalite:4.32.0")
+    implementation("com.google.protobuf:protobuf-javalite:4.32.1")
     implementation("androidx.datastore:datastore:1.1.7")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
 
     // testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.1")
 }
 
 protobuf {
