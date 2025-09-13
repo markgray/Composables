598ea6c5c393552f548f68388951888637069b0a
Mark Gray <mark.t.gray@gmail.com>
2025-09-13 04:04:57 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in ExamplesComposeMotionLayout

--- ExamplesComposeMotionLayout/app/build.gradle.kts
+++ ExamplesComposeMotionLayout/app/build.gradle.kts
@@ -52,14 +52,14 @@ android {
 
 dependencies {
     implementation("com.google.accompanist:accompanist-pager:0.36.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.9.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
-    implementation("androidx.compose.material:material:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.compose.ui:ui:1.9.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
+    implementation("androidx.compose.material:material:1.9.1")
     //noinspection GradleDependency TODO: Newer versions break compilation: keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha07")
 }
