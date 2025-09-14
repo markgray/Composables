5b02202cf8cc208da430095f4222767ef1d8ef83
Mark Gray <mark.t.gray@gmail.com>
2025-09-14 04:19:37 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in TestingCodelab

--- TestingCodelab/app/build.gradle.kts
+++ TestingCodelab/app/build.gradle.kts
@@ -73,20 +73,20 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
 
     // Compose
-    implementation("androidx.compose.runtime:runtime:1.9.0")
-    implementation("androidx.compose.ui:ui:1.9.0")
-    implementation("androidx.compose.foundation:foundation:1.9.0")
-    implementation("androidx.compose.foundation:foundation-layout:1.9.0")
-    implementation("androidx.compose.material:material:1.9.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.9.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.9.1")
+    implementation("androidx.compose.ui:ui:1.9.1")
+    implementation("androidx.compose.foundation:foundation:1.9.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.9.1")
+    implementation("androidx.compose.material:material:1.9.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
 
     // Testing dependencies
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
@@ -94,7 +94,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     // Compose testing dependencies
-    androidTestImplementation("androidx.compose.ui:ui-test:1.9.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.0")
+    androidTestImplementation("androidx.compose.ui:ui-test:1.9.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.1")
 }
