2b464110eebe39fd000196e2c9e5a13198316f83
Mark Gray <mark.t.gray@gmail.com>
2025-09-24 04:07:30 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in BasicLayoutsCodelab

--- BasicLayoutsCodelab/app/build.gradle.kts
+++ BasicLayoutsCodelab/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-    val composeBom: Dependency = platform("androidx.compose:compose-bom:2025.08.00")
+    val composeBom: Dependency = platform("androidx.compose:compose-bom:2025.09.00")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
@@ -63,8 +63,8 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.activity:activity-compose:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
