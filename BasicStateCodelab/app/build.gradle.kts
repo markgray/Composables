dac10e1b540ae513ba671fbc976906dcc8008c65
Mark Gray <mark.t.gray@gmail.com>
2025-09-24 04:14:39 -0400

    Updated gradle dependencies in BasicStateCodelab

--- BasicStateCodelab/app/build.gradle.kts
+++ BasicStateCodelab/app/build.gradle.kts
@@ -53,18 +53,18 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.compose.ui:ui:1.9.0")
-    implementation("androidx.compose.material:material:1.9.0")
+    implementation("androidx.compose.ui:ui:1.9.1")
+    implementation("androidx.compose.material:material:1.9.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
+    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.1")
 }
