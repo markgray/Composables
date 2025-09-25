cce2a026680012671dd9e9d3853108b5e626b210
markgray <mark.t.gray@gmail.com>
2025-09-25 07:54:12 -0400

    Updated gradle dependencies in MigrationCodelab

--- MigrationCodelab/build.gradle.kts
+++ MigrationCodelab/build.gradle.kts
@@ -20,6 +20,6 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("androidx.navigation.safeargs") version "2.9.4" apply false
+    id("androidx.navigation.safeargs") version "2.9.5" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
 }
