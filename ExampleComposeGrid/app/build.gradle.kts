899fec28261b3035abfb1669ea2e1c35bd87bddd
Mark Gray <mark.t.gray@gmail.com>
2025-09-25 04:00:46 -0400

    Updated gradle dependencies in ExampleComposeGrid

--- ExampleComposeGrid/app/build.gradle.kts
+++ ExampleComposeGrid/app/build.gradle.kts
@@ -51,17 +51,17 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.activity:activity-compose:1.11.0")
-    implementation("androidx.compose.ui:ui:1.9.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
-    implementation("androidx.compose.material:material:1.9.1")
+    implementation("androidx.compose.ui:ui:1.9.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.2")
+    implementation("androidx.compose.material:material:1.9.2")
     //noinspection GradleDependency TODO: newer versions break complilation - keep eye on this one.
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha08")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.2")
 }
