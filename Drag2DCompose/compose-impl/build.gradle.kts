3eb96f8c6c3b26d5232b3ab082e8a3de0329a6ee
Mark Gray <mark.t.gray@gmail.com>
2025-09-25 03:45:14 -0400

    Updated gradle dependencies in Drag2DCompose

--- Drag2DCompose/compose-impl/build.gradle.kts
+++ Drag2DCompose/compose-impl/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
 dependencies {
     implementation(project(":engine"))
-    implementation("androidx.compose.animation:animation-core:1.9.1")
+    implementation("androidx.compose.animation:animation-core:1.9.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
