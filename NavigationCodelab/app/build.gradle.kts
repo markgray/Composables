6aa067167fcf6de8181a8f80f32a07674bf87d7f
markgray <mark.t.gray@gmail.com>
2025-10-09 06:52:09 -0400

    Updated gradle to 9.1.0, and updated gradle dependencies in NavigationCodelab

--- NavigationCodelab/app/build.gradle.kts
+++ NavigationCodelab/app/build.gradle.kts
@@ -70,14 +70,14 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
 
     // Compose
-    implementation("androidx.compose.runtime:runtime:1.9.2")
-    implementation("androidx.compose.ui:ui:1.9.2")
-    implementation("androidx.compose.foundation:foundation:1.9.2")
-    implementation("androidx.compose.material:material:1.9.2")
+    implementation("androidx.compose.runtime:runtime:1.9.3")
+    implementation("androidx.compose.ui:ui:1.9.3")
+    implementation("androidx.compose.foundation:foundation:1.9.3")
+    implementation("androidx.compose.material:material:1.9.3")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.activity:activity-compose:1.11.0")
     implementation("androidx.navigation:navigation-compose:2.9.5")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.3")
 
     // Testing dependencies
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
@@ -85,7 +85,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     // Compose testing dependencies
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.2")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.2")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.3")
     androidTestImplementation("androidx.navigation:navigation-testing:2.9.5")
 }
