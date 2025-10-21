731466b7de8dfb145fdc6b9eb78aa25306a26ddb
Mark Gray <mark.t.gray@gmail.com>
2025-10-21 04:08:01 -0400

    Updated date of gradle-wrapper.properties, updated gradle dependencies, and added signingConfig to release variant in AdvancedStateAndSideEffectsCodelab

--- AdvancedStateAndSideEffectsCodelab/build.gradle.kts
+++ AdvancedStateAndSideEffectsCodelab/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.4" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
