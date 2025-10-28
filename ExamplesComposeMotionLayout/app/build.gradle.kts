625aa6ad50fd83148813e39a6ecb820dbf29a3d4
markgray <mark.t.gray@gmail.com>
2025-10-28 03:58:53 -0400

    Updated kotlin to 2.2.21, and updated gradle dependencies in ExamplesComposeMotionLayout

--- ExamplesComposeMotionLayout/app/build.gradle.kts
+++ ExamplesComposeMotionLayout/app/build.gradle.kts
@@ -53,14 +53,14 @@ android {
 
 dependencies {
     implementation("com.google.accompanist:accompanist-pager:0.36.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.4")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.activity:activity-compose:1.11.0")
-    implementation("androidx.compose.ui:ui:1.9.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.3")
-    implementation("androidx.compose.material:material:1.9.3")
+    implementation("androidx.compose.ui:ui:1.9.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.4")
+    implementation("androidx.compose.material:material:1.9.4")
     //noinspection GradleDependency TODO: Newer versions break compilation: keep an eye on this
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha07")
 }
