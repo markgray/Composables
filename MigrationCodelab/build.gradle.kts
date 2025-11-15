df69249f481b6497d60fa13a6db7da2b9ea97e0e
Mark Gray <mark.t.gray@gmail.com>
2025-11-15 03:32:07 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies in MigrationCodelab

--- MigrationCodelab/build.gradle.kts
+++ MigrationCodelab/build.gradle.kts
@@ -17,9 +17,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.13.0" apply false
+    id("com.android.application") version "8.13.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
-    id("androidx.navigation.safeargs") version "2.9.5" apply false
-    id("com.google.devtools.ksp") version "2.3.0" apply false
+    id("androidx.navigation.safeargs") version "2.9.6" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
 }
