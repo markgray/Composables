751bc26d13999181e5164bf0314561b514535e1f
Mark Gray <mark.t.gray@gmail.com>
2025-11-26 03:26:12 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in DessertClickerCompose

--- DessertClickerCompose/app/build.gradle.kts
+++ DessertClickerCompose/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.dessertclickercompose"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -52,17 +52,17 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.compose.ui:ui:1.9.4")
-    implementation("androidx.compose.material:material:1.9.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.4")
-    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.compose.ui:ui:1.9.5")
+    implementation("androidx.compose.material:material:1.9.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.5")
+    implementation("androidx.activity:activity-compose:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.5")
 }
