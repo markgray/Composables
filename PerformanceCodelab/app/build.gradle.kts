b113d65bd0bc841ad64760c0bca8600441de3afa
markgray <mark.t.gray@gmail.com>
2025-11-26 07:47:09 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in PerformanceCodelab

--- PerformanceCodelab/app/build.gradle.kts
+++ PerformanceCodelab/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.compose.performance"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
