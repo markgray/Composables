a12ac8a1c01ccfd6e15345b2fe68e2f7dccdcb89
markgray <mark.t.gray@gmail.com>
2025-11-27 07:44:24 -0500

    Added comments in ThemingCodelab

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -24,7 +24,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -52,19 +54,25 @@ class MainActivity : ComponentActivity() {
     private val viewModel: ReplyHomeViewModel by viewModels()
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we initialize [ReplyHomeUIState] variable `val uiState` by collecting values from the
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`. Next we call [setContent]
+     * to have it compose its `content` Composable lambda argument into the activity. In that
+     * lambda we initialize [ReplyHomeUIState] variable `val uiState` by collecting values from the
      * [StateFlow] of [ReplyHomeUIState] that is emitted by the [ReplyHomeViewModel.uiState] property
-     * and representing its latest value via [State] in a lifecycle-aware manner. Finally we call
-     * [setContent] to Compose the composable which consists of a [AppTheme] wrapped [Surface] whose
-     * `tonalElevation` argument is 5.dp (When color is ColorScheme.surface, a higher elevation will
-     * result in a darker color in light theme and lighter color in dark theme). The `content` of the
-     * [Surface] is our [ReplyApp] Composable, with its `replyHomeUIState` argument our
-     * [ReplyHomeUIState] variable `uiState`, whose `closeDetailScreen` argument is a lambda that
-     * calls the [ReplyHomeViewModel.closeDetailScreen] method of our [ReplyHomeViewModel] field
-     * [viewModel], and whose `navigateToDetail` argument is a lambda which calls the
-     * [ReplyHomeViewModel.setSelectedEmail] method of our [ReplyHomeViewModel] field [viewModel]
-     * with the [Long] `emailId` passed to the lambda used for the `emailId` argument.
+     * and representing its latest value via a [State] in a lifecycle-aware manner. Then we compose
+     * [AppTheme] to supply default values to [MaterialTheme] composables in its `content` composable
+     * lambda argument. The `content` of the [AppTheme] Composable is a [Box] whose `modifier`
+     * argument is a [Modifier.safeDrawingPadding] to add padding to accommodate the safe drawing
+     * insets. In the [BoxScope] `content` composable lambda argument of the [Box] we compose a
+     * [Surface] whose `tonalElevation` argument is 5.dp (When color is ColorScheme.surface, a
+     * higher elevation will result in a darker color in light theme and lighter color in dark
+     * theme). The `content` of the [Surface] is our [ReplyApp] Composable, with its
+     * `replyHomeUIState` argument our [ReplyHomeUIState] variable `uiState`, whose
+     * `closeDetailScreen` argument is a lambda that calls the [ReplyHomeViewModel.closeDetailScreen]
+     * method of our [ReplyHomeViewModel] field [viewModel], and whose `navigateToDetail` argument
+     * is a lambda which calls the [ReplyHomeViewModel.setSelectedEmail] method of our
+     * [ReplyHomeViewModel] field [viewModel] with the [Long] `emailId` passed to the lambda used
+     * for the `emailId` argument.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use it.
      */
