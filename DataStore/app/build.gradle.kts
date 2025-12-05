48533a5808c3b014b4b9808359140f911da2f888
markgray <mark.t.gray@gmail.com>
2025-12-05 07:16:59 -0500

    Updated gradle dependencies in DataStore

--- DataStore/app/build.gradle.kts
+++ DataStore/app/build.gradle.kts
@@ -53,11 +53,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.12.0")
-    implementation("androidx.compose.ui:ui:1.9.5")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.5")
-    implementation("androidx.compose.material:material:1.9.5")
-    implementation("androidx.compose.runtime:runtime-livedata:1.9.5")
+    implementation("androidx.activity:activity-compose:1.12.1")
+    implementation("androidx.compose.ui:ui:1.10.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0")
+    implementation("androidx.compose.material:material:1.10.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.10.0")
 
     // architecture components
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
@@ -75,9 +75,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.9.5")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.5")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.9.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.10.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.10.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.10.0")
 }
 
 protobuf {
