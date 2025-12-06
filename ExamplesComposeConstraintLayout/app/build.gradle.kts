9c1650c7242387a662dad6cc8e1865795ddf52c1
Mark Gray <mark.t.gray@gmail.com>
2025-12-05 23:39:02 -0500

    Updated gradle dependencies in ExamplesComposeConstraintLayout

--- ExamplesComposeConstraintLayout/app/build.gradle.kts
+++ ExamplesComposeConstraintLayout/app/build.gradle.kts
@@ -54,9 +54,9 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
-    implementation("androidx.activity:activity-compose:1.12.0")
-    implementation("androidx.compose.ui:ui:1.9.5")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.5")
-    implementation("androidx.compose.material:material:1.9.5")
+    implementation("androidx.activity:activity-compose:1.12.1")
+    implementation("androidx.compose.ui:ui:1.10.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0")
+    implementation("androidx.compose.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
 }
