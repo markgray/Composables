40b63f479a3b991a091cdeef73d62a6c8635ebc6
Mark Gray <mark.t.gray@gmail.com>
2025-12-20 03:09:04 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in BasicsCodelab

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.compose.ui:ui:1.10.0")
-    implementation("androidx.activity:activity-compose:1.12.1")
+    implementation("androidx.activity:activity-compose:1.12.2")
     implementation("androidx.compose.material:material:1.10.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.10.0")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
